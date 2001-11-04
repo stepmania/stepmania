342583b99f81c27ed12213b1e998219034a1e53d
Chris Danford <chrisdanford@users.sourceforge.net>
2001-11-04 19:51:21 +0000

    Forgot to add a couple files

---
+++ stepmania/src/ScreenDimensions.h
@@ -0,0 +1,16 @@
+/*
+-----------------------------------------------------------------------------
+ File: ScreenDimensions.h
+
+ Desc: 
+
+ Copyright (c) 2001 Chris Danford.  All rights reserved.
+-----------------------------------------------------------------------------
+*/
+
+
+#define SCREEN_WIDTH		(640)
+#define SCREEN_HEIGHT		(480)
+#define CENTER_X			(SCREEN_WIDTH/2)
+#define CENTER_Y			(SCREEN_HEIGHT/2)
+
