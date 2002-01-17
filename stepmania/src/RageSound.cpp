2b333fab1df2786fd010bcc5af906490cb05e225
Chris Danford <chrisdanford@users.sourceforge.net>
2002-01-17 19:59:06 +0000

    FIXED: 500342 - wave volume changes to 100% on startup

--- stepmania/src/RageSound.cpp
+++ stepmania/src/RageSound.cpp
@@ -30,13 +30,16 @@ RageSound::RageSound( HWND hWnd )
 	if( BASS_GetVersion() != MAKELONG(1,3) )
 		RageError( "BASS version 1.3 DLL could not be loaded.  Verify that Bass.dll exists in the program directory.");
 
-	if( !BASS_Init( -1, 44100, BASS_DEVICE_NOSYNC, m_hWndApp ) )
+	if( !BASS_Init( -1, 44100, BASS_DEVICE_LEAVEVOL, m_hWndApp ) )
 	{
-		MessageBox( NULL, "There was an error while initializing your sound card.\n\n\
-The most likely cause of this problem is that you do not have a sound card\n\
-installed, or that you have not yet installed a driver for your sound card.\n\
-Before running this program again, please verify that your sound card is\n\
-is working in other Windows applications.", "Sound error", MB_ICONSTOP );
+		MessageBox( NULL, 
+					"There was an error while initializing your sound card.\n\n"
+					"The most likely cause of this problem is that you do not have a sound card\n"
+					"installed, or that you have not yet installed a driver for your sound card.\n"
+					"Before running this program again, please verify that your sound card is\n"
+					"is working in other Windows applications.", 
+					"Sound error", 
+					MB_ICONSTOP );
 		RageError( "BASS can't initialize sound device." );
 	}
 
