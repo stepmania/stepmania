48b90d9985b6199718dd1e6ebf245bca9caacd41
Glenn Maynard <gmaynard@users.sourceforge.net>
2002-08-22 23:02:51 +0000

    const safety one two three

--- stepmania/src/StyleDef.h
+++ stepmania/src/StyleDef.h
@@ -63,6 +63,6 @@ public:
 	GameInput StyleInputToGameInput( const StyleInput StyleI ) const;
 	StyleInput GameInputToStyleInput( const GameInput &GameI ) const;
 
-	void GetTransformedNoteDataForStyle( PlayerNumber p, const NoteData* pOriginal, NoteData* pNoteDataOut );
+	void GetTransformedNoteDataForStyle( PlayerNumber p, const NoteData* pOriginal, NoteData* pNoteDataOut ) const;
 };
 
