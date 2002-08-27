7941a1ddf2b392e697092b4a553f87b79c675fb9
Glenn Maynard <gmaynard@users.sourceforge.net>
2002-08-27 23:48:11 +0000

    more crash screen stuff

--- stepmania/src/resource.h
+++ stepmania/src/resource.h
@@ -26,6 +26,7 @@
 #define IDC_CURSOR                      1008
 #define IDC_STATIC_MESSAGE1             1010
 #define IDC_STATIC_MESSAGE2             1011
+#define IDC_BUTTON_CLOSE                1011
 #define IDC_STATIC_MESSAGE3             1013
 #define IDC_ASMBOX                      1133
 #define IDC_REGDUMP                     1283
@@ -39,7 +40,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        123
 #define _APS_NEXT_COMMAND_VALUE         40009
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
