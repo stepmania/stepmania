f21b2c2e814809e7bb321a80bed16d1e9661957b
Glenn Maynard <gmaynard@users.sourceforge.net>
2002-09-07 20:23:36 +0000

    Add DX headers to PCH for now, until we can get them out of the base includes.

--- stepmania/src/StdAfx.h
+++ stepmania/src/StdAfx.h
@@ -20,9 +20,23 @@
 
 #if defined(_MSC_VER) && (_MSC_VER > 1100)
 #pragma warning (disable : 4786) // turn off broken debugger warning
+/* "unreferenced formal parameter"; we *want* that in many cases */
+#pragma warning (disable : 4100)
+/* "case 'aaa' is not a valid value for switch of enum 'bbb'
+ * Actually, this is a valid warning, but we do it all over the
+ * place, eg. with ScreenMessages.  Those should be fixed, but later. XXX */
+#pragma warning (disable : 4063)
 #endif
 
 #include <afxwin.h>         // MFC core and standard components
+
+#include <d3d8.h>
+#include <d3dx8math.h>
+
+#ifndef DIRECTINPUT_VERSION
+#define DIRECTINPUT_VERSION  0x0800
+#endif
+#include <dinput.h>
 
 #include "STDCarray.h"
 
