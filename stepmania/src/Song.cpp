9858e18467a3907285e860c05c1833089cfc3f42
Glenn Maynard <gmaynard@users.sourceforge.net>
2002-09-10 20:55:06 +0000

    also fix cdtitles

--- stepmania/src/Song.cpp
+++ stepmania/src/Song.cpp
@@ -993,10 +993,7 @@ int Song::GetNumTimesPlayed() const
  
 CString Song::GetMusicPath() const
 {
-	// Glenn, did you mean this?  m_DWIPath is empty in my ini, so
-	// the old if statement thinks every song is being loaded from DWIPath.
 	if( PREFSMAN->m_DWIPath!="" && m_sSongDir.Left(PREFSMAN->m_DWIPath.GetLength()) == PREFSMAN->m_DWIPath )
-//	if(m_sSongDir != "" && m_sSongDir.Left(PREFSMAN->m_DWIPath.GetLength()) == PREFSMAN->m_DWIPath)
 		return PREFSMAN->m_DWIPath+"\\"+m_sMusicFile;
 
 	return m_sMusicFile.Left(2) == ".\\"? m_sMusicFile : m_sSongDir+m_sMusicFile;
@@ -1013,7 +1010,7 @@ CString Song::GetBackgroundPath() const
 }
 CString Song::GetCDTitlePath() const
 {
-	if(m_sSongDir != "" && m_sSongDir.Left(PREFSMAN->m_DWIPath.GetLength()) == PREFSMAN->m_DWIPath)
+	if( PREFSMAN->m_DWIPath!="" && m_sSongDir.Left(PREFSMAN->m_DWIPath.GetLength()) == PREFSMAN->m_DWIPath )
 		return PREFSMAN->m_DWIPath+"\\"+m_sCDTitleFile;
 
 	return m_sMusicFile.Left(2) == ".\\" ? m_sCDTitleFile : m_sSongDir+m_sCDTitleFile;
