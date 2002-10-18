5d94e66b8df11a2d4474e6ff4ca5794ddc403764
Glenn Maynard <gmaynard@users.sourceforge.net>
2002-10-18 01:36:19 +0000

    Bring speeds out to two decimal places, so we get .75.

--- stepmania/src/PlayerOptions.cpp
+++ stepmania/src/PlayerOptions.cpp
@@ -56,9 +56,16 @@ CString PlayerOptions::GetString()
 
 	if( m_fArrowScrollSpeed != 1 )
 	{
-		CString s = ssprintf( "%2.1f", m_fArrowScrollSpeed );
-		if( s[s.GetLength()-1] == '0' )
-			s.Delete(s.GetLength()-2, 2);	// delete last 2 chars
+		/* -> 1.00 */
+		CString s = ssprintf( "%2.2f", m_fArrowScrollSpeed );
+		if( s[s.GetLength()-1] == '0' ) {
+			/* -> 1.0 */
+			s.Delete(s.GetLength()-1, 1);	// delete last char
+			if( s[s.GetLength()-1] == '0' ) {
+				/* -> 1 */
+				s.Delete(s.GetLength()-2, 2);	// delete last 2 chars
+			}
+		}
 		sReturn += s + "X, ";
 	}
 
