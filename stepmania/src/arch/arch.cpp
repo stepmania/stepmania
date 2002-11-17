2f91ba10f1d2752524c5cd6b8ece3551a3081776
Glenn Maynard <gmaynard@users.sourceforge.net>
2002-11-17 08:20:34 +0000

    Add simple infrastructure for arch-dependent code.

---
+++ stepmania/src/arch/arch.cpp
@@ -0,0 +1,23 @@
+/*
+ * This file provides functions to create driver objects.
+ */
+
+#include "../stdafx.h"
+
+#include "arch.h"
+
+/* Load default drivers. */
+#include "default/arch.h"
+
+/* Override them with arch-specific drivers, as available. */
+#if defined(WIN32)
+#include "Win32/arch.h"
+#endif
+
+LoadingWindow *MakeLoadingWindow() { return new ARCH_LOADING_WINDOW; }
+
+/*
+ * Copyright (c) 2002 by the person(s) listed below.  All rights reserved.
+ *
+ * Glenn Maynard
+ */
---
+++ stepmania/src/arch/arch.h
@@ -0,0 +1,15 @@
+#ifndef ARCH_H
+#define ARCH_H
+
+/* Include this file if you need to create an instance of a driver object.  */
+class LoadingWindow;
+
+LoadingWindow *MakeLoadingWindow();
+
+#endif
+
+/*
+ * Copyright (c) 2002 by the person(s) listed below.  All rights reserved.
+ *
+ * Glenn Maynard
+ */
