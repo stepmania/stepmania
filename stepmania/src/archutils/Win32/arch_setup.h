bf1efc8cf6ed48cc90a730a0d8bfdb6015362fc3
Glenn Maynard <gmaynard@users.sourceforge.net>
2002-12-17 08:20:55 +0000

    update

--- stepmania/src/archutils/Win32/arch_setup.h
+++ stepmania/src/archutils/Win32/arch_setup.h
@@ -1,7 +1,20 @@
 #ifndef ARCH_SETUP_WINDOWS_H
 #define ARCH_SETUP_WINDOWS_H
 
+/* Fix Windows breakage. */
+
 #define PATH_MAX _MAX_PATH
+
+#include <direct.h> /* has stuff that should be in unistd.h */
+
+#define getcwd _getcwd
+#define wgetcwd _wgetcwd
+#define chdir _chdir
+#define wchdir _wchdir
+#define alloca _alloca
+
+#define stat _stat
+#define lstat _stat
 
 #endif
 
