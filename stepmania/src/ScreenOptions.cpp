5488b0bcb6a53b708121e481ad02a624a3744908
Glenn Maynard <gmaynard@users.sourceforge.net>
2002-12-28 09:08:37 +0000

    old

--- stepmania/src/ScreenOptions.cpp
+++ stepmania/src/ScreenOptions.cpp
@@ -190,12 +190,6 @@ void ScreenOptions::InitOptionsText()
 
 		title.LoadFromFont( THEME->GetPathTo("Fonts","option title") );
 		CString sText = optline.szTitle;
-		// Chris:  To make a line break, use '\n'
-//		sText.Replace( " ", "\n" );
-//		/* A bit of a hack: menus can put ^ to indicate a non-breaking space.  This
-//		 * can go away if tihs gets smarter.  (FOr example, there's no reason to break
-//		 * "how to play" into three lines. */
-//		sText.Replace( "^", " " );
 
 		title.SetText( sText );
 		title.SetXY( LABELS_X, fY );
