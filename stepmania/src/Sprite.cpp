28294496a2b0602cedf42baaa973f5e1c2a3ee99
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-01-09 21:21:08 +0000

    remove old TranslateLocal comment

--- stepmania/src/Sprite.cpp
+++ stepmania/src/Sprite.cpp
@@ -180,18 +180,6 @@ void Sprite::DrawPrimitives()
 	if( m_pTexture  &&  m_pTexture->IsAMovie()  &&  m_pTexture->IsPlaying() )
 		::Sleep( PREFSMAN->m_iMovieDecodeMS );	// let the movie decode a frame
 
-
-	// Offset so that pixels are aligned to texels
-	// Offset by -0.5, -0.5 if 640x480
-	// Offset by -1.0, -1.0 if 320x240
-	// 11/12/2002:  Why in the world is this not needed with the OpenGL renderer?
-	// We're using the exact same orthographic projection transform.  Anyway, things
-	// are perfectly aligned without this TranslateLocal
-//	DISPLAY->TranslateLocal( 
-//		-0.5f*SCREEN_WIDTH/(float)PREFSMAN->m_iDisplayWidth, 
-//		-0.5f*SCREEN_HEIGHT/(float)PREFSMAN->m_iDisplayHeight, 
-//		0 );
-	
 	// use m_temp_* variables to draw the object
 	RectF quadVerticies;
 
