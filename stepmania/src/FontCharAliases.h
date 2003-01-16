7e45953a12e27cd19474f7b20e95f52d26157ea5
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-01-16 20:11:37 +0000

    add data namespaces (these might read from an INI later)

---
+++ stepmania/src/FontCharAliases.h
@@ -0,0 +1,11 @@
+#ifndef FONT_CHAR_ALIASES
+#define FONT_CHAR_ALIASES
+
+#include "RageUtil.h"
+
+namespace FontCharAliases {
+	void ReplaceMarkers( CString &sText );
+	longchar GetChar( CString &codepoint );
+};
+
+#endif
---
+++ stepmania/src/FontCharmaps.h
@@ -0,0 +1,10 @@
+#ifndef FONT_CHARMAPS_H
+#define FONT_CHARMAPS_H
+
+namespace FontCharmaps
+{
+	extern const wchar_t M_SKIP;
+	const wchar_t *get_char_map(CString name);
+};
+
+#endif
