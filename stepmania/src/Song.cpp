691324866463f5ed5317b4d64ac99c048f21ed1e
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-01-19 23:57:33 +0000

    inval cache

--- stepmania/src/Song.cpp
+++ stepmania/src/Song.cpp
@@ -40,7 +40,7 @@
 #include "SDL_image.h"
 
 
-const int FILE_CACHE_VERSION = 104;	// increment this when Song or Notes changes to invalidate cache
+const int FILE_CACHE_VERSION = 105;	// increment this when Song or Notes changes to invalidate cache
 
 
 static int CompareBPMSegments(const BPMSegment &seg1, const BPMSegment &seg2)
