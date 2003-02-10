09b461514ccf51329df0c70e15a22a616e9fa448
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-02-10 01:30:45 +0000

    fix AAAA

--- stepmania/src/StageStats.cpp
+++ stepmania/src/StageStats.cpp
@@ -74,7 +74,8 @@ Grade StageStats::GetGrade( PlayerNumber pn )
 	LOG->Trace( "fPercentDancePoints: %f", fPercentDancePoints  );
 
 	// check for "AAAA"
-	if( iTapNoteScores[pn][TNS_MARVELOUS] > 0 &&
+	if( fPercentDancePoints == 1.00f &&
+		iTapNoteScores[pn][TNS_MARVELOUS] > 0 &&
 		iTapNoteScores[pn][TNS_PERFECT] == 0 &&
 		iTapNoteScores[pn][TNS_GREAT] == 0 &&
 		iTapNoteScores[pn][TNS_GOOD] == 0 &&
