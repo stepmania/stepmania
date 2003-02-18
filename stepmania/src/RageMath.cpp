72088e53e3bd3add17da6cbfed004c16a793c026
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-02-18 07:20:17 +0000

    the matrix multiply was backwards; this turned TranslateLocal into Translate
    this will fix some shadow problems.  the title shadow is something
    odd: it seems that the title text is being shadowed by
    ScreenTitleMenu::ChoicesX instead of ChoicesShadowLength
    for some people?

--- stepmania/src/RageMath.cpp
+++ stepmania/src/RageMath.cpp
@@ -55,22 +55,22 @@ void RageMatrixMultiply( RageMatrix* pOut, const RageMatrix* pA, const RageMatri
     const RageMatrix &b = *pB;
 
 	*pOut = RageMatrix(
-		a.m00*b.m00+a.m01*b.m10+a.m02*b.m20+a.m03*b.m30,
-		a.m00*b.m01+a.m01*b.m11+a.m02*b.m21+a.m03*b.m31,
-		a.m00*b.m02+a.m01*b.m12+a.m02*b.m22+a.m03*b.m32,
-		a.m00*b.m03+a.m01*b.m13+a.m02*b.m23+a.m03*b.m33,
-		a.m10*b.m00+a.m11*b.m10+a.m12*b.m20+a.m13*b.m30,
-		a.m10*b.m01+a.m11*b.m11+a.m12*b.m21+a.m13*b.m31,
-		a.m10*b.m02+a.m11*b.m12+a.m12*b.m22+a.m13*b.m32,
-		a.m10*b.m03+a.m11*b.m13+a.m12*b.m23+a.m13*b.m33,
-		a.m20*b.m00+a.m21*b.m10+a.m22*b.m20+a.m23*b.m30,
-		a.m20*b.m01+a.m21*b.m11+a.m22*b.m21+a.m23*b.m31,
-		a.m20*b.m02+a.m21*b.m12+a.m22*b.m22+a.m23*b.m32,
-		a.m20*b.m03+a.m21*b.m13+a.m22*b.m23+a.m23*b.m33,
-		a.m30*b.m00+a.m31*b.m10+a.m32*b.m20+a.m33*b.m30,
-		a.m30*b.m01+a.m31*b.m11+a.m32*b.m21+a.m33*b.m31,
-		a.m30*b.m02+a.m31*b.m12+a.m32*b.m22+a.m33*b.m32,
-		a.m30*b.m03+a.m31*b.m13+a.m32*b.m23+a.m33*b.m33 
+		b.m00*a.m00+b.m01*a.m10+b.m02*a.m20+b.m03*a.m30,
+		b.m00*a.m01+b.m01*a.m11+b.m02*a.m21+b.m03*a.m31,
+		b.m00*a.m02+b.m01*a.m12+b.m02*a.m22+b.m03*a.m32,
+		b.m00*a.m03+b.m01*a.m13+b.m02*a.m23+b.m03*a.m33,
+		b.m10*a.m00+b.m11*a.m10+b.m12*a.m20+b.m13*a.m30,
+		b.m10*a.m01+b.m11*a.m11+b.m12*a.m21+b.m13*a.m31,
+		b.m10*a.m02+b.m11*a.m12+b.m12*a.m22+b.m13*a.m32,
+		b.m10*a.m03+b.m11*a.m13+b.m12*a.m23+b.m13*a.m33,
+		b.m20*a.m00+b.m21*a.m10+b.m22*a.m20+b.m23*a.m30,
+		b.m20*a.m01+b.m21*a.m11+b.m22*a.m21+b.m23*a.m31,
+		b.m20*a.m02+b.m21*a.m12+b.m22*a.m22+b.m23*a.m32,
+		b.m20*a.m03+b.m21*a.m13+b.m22*a.m23+b.m23*a.m33,
+		b.m30*a.m00+b.m31*a.m10+b.m32*a.m20+b.m33*a.m30,
+		b.m30*a.m01+b.m31*a.m11+b.m32*a.m21+b.m33*a.m31,
+		b.m30*a.m02+b.m31*a.m12+b.m32*a.m22+b.m33*a.m32,
+		b.m30*a.m03+b.m31*a.m13+b.m32*a.m23+b.m33*a.m33 
 	);
 	// phew!
 }
