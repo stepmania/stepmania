d267f60f80e5606b8227a98e5ab54d372a9c8231
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-03-30 20:17:41 +0000

    cache update for the last snapshot

--- stepmania/src/Song.cpp
+++ stepmania/src/Song.cpp
@@ -42,7 +42,7 @@
 #include "SDL_image.h"
 
 
-const int FILE_CACHE_VERSION = 113;	// increment this when Song or Notes changes to invalidate cache
+const int FILE_CACHE_VERSION = 114;	// increment this when Song or Notes changes to invalidate cache
 
 const float DEFAULT_MUSIC_SAMPLE_LENGTH = 12.f;
 
