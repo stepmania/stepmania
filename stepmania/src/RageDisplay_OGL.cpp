c3c96f390ca4102fc1984d5d522ee5aa025e5bfc
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-05-27 08:39:36 +0000

    grumble

--- stepmania/src/RageDisplay_OGL.cpp
+++ stepmania/src/RageDisplay_OGL.cpp
@@ -394,7 +394,7 @@ static void SetupVertices( const RageVertex v[], int iNumVerts )
 		Texture[i*2+1] = v[i].t[1];
 		Normal[i*3+0] = v[i].n[0];
 		Normal[i*3+1] = v[i].n[1];
-		Normal[i*+2] = v[i].n[2];
+		Normal[i*3+2] = v[i].n[2];
 	}
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glVertexPointer(3, GL_FLOAT, 0, Vertex);
