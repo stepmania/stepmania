098a28beb388d4ba081ad924227580c801d02ef5
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-05-31 04:48:38 +0000

    bump cache

--- stepmania/src/Song.cpp
+++ stepmania/src/Song.cpp
@@ -40,7 +40,7 @@
 #include "SDL_image.h"
 
 
-const int FILE_CACHE_VERSION = 118;	// increment this when Song or Notes changes to invalidate cache
+const int FILE_CACHE_VERSION = 119;	// increment this when Song or Notes changes to invalidate cache
 
 const float DEFAULT_MUSIC_SAMPLE_LENGTH = 12.f;
 
