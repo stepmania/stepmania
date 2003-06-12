05e94e427898b71f94d7cf7a9f192215edb1bc29
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-06-12 20:08:55 +0000

    really fix #DISPLAYBPM writing

--- stepmania/src/NotesWriterDWI.cpp
+++ stepmania/src/NotesWriterDWI.cpp
@@ -236,9 +236,9 @@ bool NotesWriterDWI::Write( CString sPath, const Song &out )
 		break;
 	case Song::DISPLAY_SPECIFIED:
 		if( out.m_fDisplayBPMMin == out.m_fDisplayBPMMax )
-			fprintf( fp, "#DISPLAYBPM:%.3f", out.m_fDisplayBPMMin );
+			fprintf( fp, "#DISPLAYBPM:%.3f;\n", out.m_fDisplayBPMMin );
 		else
-			fprintf( fp, "#DISPLAYBPM:%.3f..%.3f", out.m_fDisplayBPMMin, out.m_fDisplayBPMMax );
+			fprintf( fp, "#DISPLAYBPM:%.3f..%.3f;\n", out.m_fDisplayBPMMin, out.m_fDisplayBPMMax );
 		break;
 	case Song::DISPLAY_RANDOM:
 		fprintf( fp, "#DISPLAYBPM:*" );
