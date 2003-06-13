3227d97d5594ddd94cfec06fe531cdfcf6cddbbd
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-06-13 04:31:01 +0000

    fix doubles jukebox

--- stepmania/src/Player.cpp
+++ stepmania/src/Player.cpp
@@ -305,9 +305,9 @@ void Player::Update( float fDeltaTime )
 
 void Player::DrawPrimitives()
 {
-	// don't draw CPU player in doubles.
+	// May have both players in doubles (for battle play); only draw primary player.
 	if( GAMESTATE->GetCurrentStyleDef()->m_StyleType == StyleDef::ONE_PLAYER_TWO_CREDITS  &&
-		GAMESTATE->m_PlayerController[m_PlayerNumber] == PC_CPU )
+		m_PlayerNumber != GAMESTATE->m_MasterPlayerNumber )
 		return;
 
 
