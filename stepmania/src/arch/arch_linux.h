adc0b5abb2cd39109f3fb022b62df8bd1d8f5889
William Reading <billatq@users.sourceforge.net>
2003-06-19 05:49:21 +0000

    Add the SDL audio driver for fallback

--- stepmania/src/arch/arch_linux.h
+++ stepmania/src/arch/arch_linux.h
@@ -5,8 +5,14 @@
 #include "Sound/RageSoundDriver_ALSA9.h"
 /* #endif HAS_ALSA9 */
 #include "Sound/RageSoundDriver_OSS.h"
-
 #endif
+/* 
+ * Sometimes Alsa and OSS fail to load, so
+ * SDL should be a fallback, even though it
+ * doesn't work perfectly.
+ */
+#include "SDL.h"
+#include "Sound/RageSoundDriver_SDL.h"
 
 /*
  * Copyright (c) 2002 by the person(s) listed below.  All rights reserved.
