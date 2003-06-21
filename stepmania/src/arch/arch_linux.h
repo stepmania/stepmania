e8e5f8888726924815b63d19b044aedeb3072c45
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-06-21 23:27:41 +0000

    Never use SDL audio in Linux.

--- stepmania/src/arch/arch_linux.h
+++ stepmania/src/arch/arch_linux.h
@@ -5,15 +5,17 @@
 #include "Sound/RageSoundDriver_ALSA9.h"
 /* #endif HAS_ALSA9 */
 #include "Sound/RageSoundDriver_OSS.h"
-#endif
+
 /* 
  * Sometimes Alsa and OSS fail to load, so
  * SDL should be a fallback, even though it
  * doesn't work perfectly.
  */
-#include "SDL.h"
-#include "Sound/RageSoundDriver_SDL.h"
+/* No, it shouldn't.  If they fail to load, we should receive a bug report
+ * and fix our code, not have it silently fall back on a broken driver. -glenn */
+// #include "Sound/RageSoundDriver_SDL.h"
 
+#endif
 /*
  * Copyright (c) 2002 by the person(s) listed below.  All rights reserved.
  *
