8014bb1f2779d33a99fc80fe504f962dfe06dce3
Sean Burke <hellfyre@users.sourceforge.net>
2003-07-07 00:52:35 +0000

    Revert to cpp and fix a glaring omission in LoadingWindow_Gtk.

--- stepmania/src/arch/LoadingWindow/LoadingWindow_Gtk.cpp
+++ stepmania/src/arch/LoadingWindow/LoadingWindow_Gtk.cpp
@@ -1,5 +1,4 @@
 #include <gtk/gtk.h>
-#include "global.h"
 
 static GtkWidget *label;
 static GtkWidget *window;
@@ -7,8 +6,12 @@ static GtkWidget *window;
 void CreateGtkLoadingWindow() {
   GtkWidget *vbox;
   GtkWidget *loadimage;
+  char **argv;
+  int argc;
 
-  gtk_init(&g_argc,&g_argv);
+  argc = 1;
+  argv[0] = "stepmania";
+  gtk_init(&argc,&argv);
   loadimage = gtk_image_new_from_file("loading.xpm");
   label = gtk_label_new(NULL);
   gtk_label_set_justify(GTK_LABEL(label),GTK_JUSTIFY_CENTER);
@@ -21,12 +24,14 @@ void CreateGtkLoadingWindow() {
   gtk_widget_show(label);
   gtk_widget_show(vbox);
   gtk_widget_show(window);
+  gtk_main();
 }
 
 void DestroyGtkLoadingWindow() {
-  gtk_widget_destroy(window);
+  gtk_main_quit();
 }
 
 void SetGtkLoadingWindowText(const char *s) {
   gtk_label_set_text(GTK_LABEL(label),s);
+  gtk_widget_show(label);
 }
