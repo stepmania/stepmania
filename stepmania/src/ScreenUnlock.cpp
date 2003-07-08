a19e0548a264272f1890cd86f6c6fd67f7e12c5a
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-07-08 23:34:15 +0000

    Fix double-spacing.  I think this happened when the -kb flag was removed.

--- stepmania/src/ScreenUnlock.cpp
+++ stepmania/src/ScreenUnlock.cpp
@@ -1,15 +1,11 @@
 /*
 -----------------------------------------------------------------------------
  Class: ScreenUnlock
-
  Desc: See header.
-
  Copyright (c) 2003 by the person(s) listed below.  All rights reserved.
 	curewater
 -----------------------------------------------------------------------------
 */
-
-
 #include "global.h"
 #include "PrefsManager.h"
 #include "ScreenUnlock.h"
@@ -30,27 +26,20 @@ ScreenUnlock::ScreenUnlock() : ScreenAttract("ScreenUnlock")
 		{
 			// new unlock graphic
 			char filename[30];
-
 			sprintf(filename, "UnlockGraphic%d", i);
 			Unlocks[i].Load( THEME->GetPathToG(filename) );
-
 			sprintf(filename, "Unlock%d", i);
 			PointsUntilNextUnlock.SetXY( 
 				THEME->GetMetricI("ScreenUnlock",ssprintf("Unlock%dX", i)),
 				THEME->GetMetricI("ScreenUnlock",ssprintf("Unlock%dX", i)) );
-			
 			Unlocks[i].SetName( ssprintf("Unlock%d",i) );
-
 			UtilSetXY( Unlocks[i], "ScreenUnlock" );
-		
 			if (!GAMESTATE->m_pUnlockingSys->SongIsLocked
 				(THEME->GetMetric
 				 ("ScreenUnlock", ssprintf
 				  ("Unlock%dSong", i))))
 				this->AddChild(&Unlocks[i]);
-
 		}
-
 		/* Remove the decimal
 		if( sDP.Find(".",1) > 0 )
 		{
@@ -59,9 +48,7 @@ ScreenUnlock::ScreenUnlock() : ScreenAttract("ScreenUnlock")
 		
 		// No negative numbers
 		if( sDP.Left(1) == "-" ) 
-		{ 
 			sDP = "*";
-		};
 		
 		PointsUntilNextUnlock.SetText( sDP );
 		PointsUntilNextUnlock.SetZoom( THEME->GetMetricF("ScreenUnlock","DancePointsZoom") );
