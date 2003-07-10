21adf68857438844f7aaf94b537280784ce7769e
Chris Danford <chrisdanford@users.sourceforge.net>
2003-07-10 03:36:12 +0000

    work on battle stuff

--- stepmania/src/CombinedLifeMeterEnemy.cpp
+++ stepmania/src/CombinedLifeMeterEnemy.cpp
@@ -88,23 +88,15 @@ void CombinedLifeMeterEnemy::Update( float fDelta )
 		m_sprHealthStream.SetGlow( RageColor(1,1,1,0) );
 		m_fLastSeenHealthPercent = GAMESTATE->m_fOpponentHealthPercent;
 
+		m_sprFace.SetState( damage );
+		m_fSecondsUntilReturnToNormalFace = SECONDS_TO_SHOW_FACE;
+
 		if( GAMESTATE->m_fOpponentHealthPercent == 0 )
 		{
 			m_sprFrame.BeginTweening( 0.5f );
 			m_sprFrame.SetDiffuse( RageColor(0.5f,0.5f,0.5f,1) );
 		}
 	}
-
-
-	/*
-	SetFace( Face face )
-
-	if( GAMESTATE->m_fOpponentHealthPercent == 0 )
-		return;
-
-	this->SetState( face );
-	m_fSecondsUntilReturnToNormalFace = SECONDS_TO_SHOW_FACE;
-	*/
 }
 
 
