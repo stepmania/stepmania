bfc22151918055e7ace23c0dfa42491e5d5b4e79
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-07-12 19:45:07 +0000

    The only change in this mess is changing indentation; no actual code is changed.

--- stepmania/src/arch/LoadingWindow/LoadingWindow_Gtk.cpp
+++ stepmania/src/arch/LoadingWindow/LoadingWindow_Gtk.cpp
@@ -4,38 +4,41 @@
 static GtkWidget *label;
 static GtkWidget *window;
 
-void CreateGtkLoadingWindow() {
-  GdkPixmap *loadmap;
-  GtkWidget *vbox;
-  GtkWidget *loadimage;
-  char **argv;
-  int argc;
+void CreateGtkLoadingWindow()
+{
+	GdkPixmap *loadmap;
+	GtkWidget *vbox;
+	GtkWidget *loadimage;
+	char **argv;
+	int argc;
 
-  argc = 1;
-  argv[0] = "stepmania";
-  gtk_init(&argc,&argv);
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_widget_realize(window);
-  loadmap = gdk_pixmap_create_from_xpm_d(window->window,NULL,NULL,loading);
-  loadimage = gtk_image_new_from_pixmap(loadmap,NULL);
-  label = gtk_label_new(NULL);
-  gtk_label_set_justify(GTK_LABEL(label),GTK_JUSTIFY_CENTER);
-  vbox = gtk_vbox_new(FALSE,5);
-  gtk_container_add(GTK_CONTAINER(window),vbox);
-  gtk_box_pack_start(GTK_BOX(vbox),loadimage,FALSE,FALSE,0);
-  gtk_box_pack_end(GTK_BOX(vbox),label,TRUE,TRUE,0);
-  gtk_widget_show(loadimage);
-  gtk_widget_show(label);
-  gtk_widget_show(vbox);
-  gtk_widget_show(window);
-  gtk_main_iteration_do(FALSE);
+	argc = 1;
+	argv[0] = "stepmania";
+	gtk_init(&argc,&argv);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_widget_realize(window);
+	loadmap = gdk_pixmap_create_from_xpm_d(window->window,NULL,NULL,loading);
+	loadimage = gtk_image_new_from_pixmap(loadmap,NULL);
+	label = gtk_label_new(NULL);
+	gtk_label_set_justify(GTK_LABEL(label),GTK_JUSTIFY_CENTER);
+	vbox = gtk_vbox_new(FALSE,5);
+	gtk_container_add(GTK_CONTAINER(window),vbox);
+	gtk_box_pack_start(GTK_BOX(vbox),loadimage,FALSE,FALSE,0);
+	gtk_box_pack_end(GTK_BOX(vbox),label,TRUE,TRUE,0);
+	gtk_widget_show(loadimage);
+	gtk_widget_show(label);
+	gtk_widget_show(vbox);
+	gtk_widget_show(window);
+	gtk_main_iteration_do(FALSE);
 }
 
-void DestroyGtkLoadingWindow() {
+void DestroyGtkLoadingWindow()
+{
 }
 
-void SetGtkLoadingWindowText(const char *s) {
-  gtk_label_set_text(GTK_LABEL(label),s);
-  gtk_widget_show(label);
-  gtk_main_iteration_do(FALSE);
+void SetGtkLoadingWindowText(const char *s)
+{
+	gtk_label_set_text(GTK_LABEL(label),s);
+	gtk_widget_show(label);
+	gtk_main_iteration_do(FALSE);
 }
