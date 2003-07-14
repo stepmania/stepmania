9482754ad60837c0c4c51348f8d482f947047a64
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-07-14 19:35:12 +0000

    Fix clipping problems in ScreenSelectCourse

--- stepmania/src/ScreenSelectCourse.cpp
+++ stepmania/src/ScreenSelectCourse.cpp
@@ -76,6 +76,7 @@ ScreenSelectCourse::ScreenSelectCourse() : Screen("ScreenSelectCourse")
 	this->AddChild( &m_sprExplanation );
 
 	m_Banner.SetXY( BANNER_X, BANNER_Y );
+	m_Banner.ScaleToClipped( BANNER_WIDTH, BANNER_HEIGHT );
 	this->AddChild( &m_Banner );
 
 	m_sprBannerFrame.Load( THEME->GetPathToG("ScreenSelectCourse banner frame") );
@@ -412,7 +413,6 @@ void ScreenSelectCourse::AfterCourseChange()
 				m_textTime.SetText( "xx:xx:xx" );	// The numbers format doesn't have a '?'.  Is there a better solution?
 
 			m_Banner.LoadFromCourse( pCourse );
-			m_Banner.ScaleToClipped( BANNER_WIDTH, BANNER_HEIGHT );
 
 			m_CourseContentsFrame.SetFromCourse( pCourse );
 			m_CourseContentsFrame.TweenInAfterChangedCourse();
