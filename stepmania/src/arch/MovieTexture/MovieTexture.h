0434e88323f5e1011c691c07052c33bcb82c8b2e
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2003-07-16 12:19:53 +0000

    overridden methods should be virtual

--- stepmania/src/arch/MovieTexture/MovieTexture.h
+++ stepmania/src/arch/MovieTexture/MovieTexture.h
@@ -8,7 +8,7 @@ class RageMovieTexture : public RageTexture
 public:
 	RageMovieTexture( RageTextureID ID ): RageTexture(ID) { }
 	virtual ~RageMovieTexture() { }
-	void Update(float fDeltaTime) { }
+	virtual void Update(float fDeltaTime) { }
 
 	virtual void Reload() = 0;
 
