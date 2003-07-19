969585516eb7be12ee9410f897a0b72b3d20e5d5
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-07-19 21:20:34 +0000

    remove odl comment

--- stepmania/src/arch/arch_linux.h
+++ stepmania/src/arch/arch_linux.h
@@ -9,15 +9,6 @@
 
 #include "Sound/RageSoundDriver_OSS.h"
 
-/* 
- * Sometimes Alsa and OSS fail to load, so
- * SDL should be a fallback, even though it
- * doesn't work perfectly.
- */
-/* No, it shouldn't.  If they fail to load, we should receive a bug report
- * and fix our code, not have it silently fall back on a broken driver. -glenn */
-// #include "Sound/RageSoundDriver_SDL.h"
-
 #ifdef HAVE_GTK
 #  include "LoadingWindow/LoadingWindow_Gtk.h"
 #endif
