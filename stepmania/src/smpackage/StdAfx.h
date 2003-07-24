fcf4541e6dcb060c933304641305638f5356ee03
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-07-24 01:11:58 +0000

    disable a warning

--- stepmania/src/smpackage/StdAfx.h
+++ stepmania/src/smpackage/StdAfx.h
@@ -10,6 +10,8 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
+#pragma warning (disable : 4786) // turn off broken debugger warning
+
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <afxwin.h>         // MFC core and standard components
