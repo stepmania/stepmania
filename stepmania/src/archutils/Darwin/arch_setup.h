333c7ae7a174acf3be3c11f3fec228b7e45db6af
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2003-07-28 09:31:31 +0000

    Added defines for Darwin.

---
+++ stepmania/src/archutils/Darwin/arch_setup.h
@@ -0,0 +1,9 @@
+#define DARWIN 1
+#define HAVE_DECL_SIGPWR 0
+#define NEED_POWF
+#define NEED_SQRTF
+#define NEED_SINF
+#define NEED_COSF
+#define NEED_TANF
+#define NEED_ACOSF
+#define VERSION "3.001"
