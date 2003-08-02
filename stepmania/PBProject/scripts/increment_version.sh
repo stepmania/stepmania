e0e79c934817f2b3342be13b4656c86441ec9114
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2003-08-02 13:00:02 +0000

    Used by ProjectBuilder to update build number

---
+++ stepmania/PBProject/scripts/increment_version.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#Stupid ProjectBuild wouldn't allow perl
+
+exec perl -w -e '
+open F, "<version.tst"||die;
+$ver = <F>;
+close F;
+chomp $ver;
+++$ver;
+open F,">version.tst"||die;
+print F "$ver\n";
+close F;
+$date = `date`;
+chomp $date;
+open F, ">ver.cpp"||die;
+print F "unsigned long version_num = $ver;\n";print F "const char *version_time = \"$date\";\n";close F;
+'
+touch ver.cpp
