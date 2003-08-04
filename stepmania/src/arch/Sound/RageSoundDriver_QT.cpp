9c1af5257010379ebdaf881716392c7803d21e3e
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2003-08-04 11:22:33 +0000

    Nearly complete. If I can get the callbacks working, this should work.

--- stepmania/src/arch/Sound/RageSoundDriver_QT.cpp
+++ stepmania/src/arch/Sound/RageSoundDriver_QT.cpp
@@ -19,10 +19,21 @@
 #include <pthread.h>
 #include <sys/types.h>
 
-const unsigned samplesize = 4;
+const unsigned numchannels = 2;
+//const unsigned samplesize = 2 * numchannels;
+const unsigned samplesize = 16 * numchannels;
 const unsigned samples = 4096;
 const unsigned freq = 44100;
-const unsigned buffersize = samples * samplesize;
+//const unsigned buffersize = samples * samplesize;
+const unsigned buffersize = samples * samplesize / 8;
+
+#if defined(DEBUG)
+#define TEST_ERR(err, func) \
+if (__builtin_expect(err, noErr)) \
+    RageException::Throw(#func " failed with error: %d", err)
+#else
+#define TEST_ERR(err, func)
+#endif
 
 typedef struct soundInfo{
     RageSound *snd;
@@ -40,18 +51,22 @@ static vector<soundInfoHdl> sounds;
 static MovieImportComponent miComponent;
 
 RageSound_QT::RageSound_QT() {
-#if 1
+#if 0
     RageException::ThrowNonfatal("Class not finished!");
 #endif
     if (EnterMovies() != noErr)
         RageException::ThrowNonfatal("Could not start movies. Falling back.");
     miComponent = OpenDefaultComponent(MovieImportType, kQTFileTypeWave);
+    ASSERT(miComponent);
 }
 
 RageSound_QT::~RageSound_QT() {
     LockMutex L(SOUNDMAN->lock);
     while (!sounds.empty()) {
         soundInfoHdl sHdl = sounds.back();
+        HLock(Handle(sHdl));
+        OSStatus err = MemError();
+        TEST_ERR(err, HLock);
         soundInfoPtr s = *sHdl;
         if (s->movie[0]) {
             StopMovie(s->movie[0]);
@@ -61,7 +76,12 @@ RageSound_QT::~RageSound_QT() {
             StopMovie(s->movie[1]);
             DisposeMovie(s->movie[1]);
         }
+        HUnlock(Handle(sHdl));
+        err = MemError();
+        TEST_ERR(err, HUnlock);
         DisposeHandle(Handle(sHdl));
+        err = MemError();
+        TEST_ERR(err, DisposeHandle);
         sounds.pop_back();
     }
     if (miComponent)
@@ -70,8 +90,13 @@ RageSound_QT::~RageSound_QT() {
 
 void RageSound_QT::StartMixing(RageSound *snd) {
     LockMutex L(SOUNDMAN->lock);
+    LOG->Trace("StartMixing()");
     soundInfoHdl sHdl = (soundInfoHdl)NewHandle(sizeof(soundInfo));
-    HLockHi(Handle(sHdl)); /* Move to the top of the heap */
+    OSStatus err = MemError();
+    TEST_ERR(err, NewHandle);
+    HLock(Handle(sHdl));
+    err = MemError();
+    TEST_ERR(err, HLock);
     soundInfoPtr s = *sHdl;
 
     s->fillme = 0;
@@ -81,19 +106,30 @@ void RageSound_QT::StartMixing(RageSound *snd) {
     s->lastPos = 0;
     s->snd = snd;
     s->finishedBuffers = -1;
+    HUnlock(Handle(sHdl));
+    err = MemError();
+    TEST_ERR(err, HUnlock);
     sounds.push_back(sHdl);
-    CallBack(NULL, long(s));
+    CallBack(NULL, long(sHdl));
 }
 
 void RageSound_QT::StopMixing(RageSound *snd) {
     LockMutex L(SOUNDMAN->lock);
+    LOG->Trace("StopMixing");
     unsigned size = sounds.size();
 
     for (unsigned i=0; i<size; ++i) {
         soundInfoHdl sHdl = sounds[i];
+        HLock(Handle(sHdl));
+        OSStatus err = MemError();
+        TEST_ERR(err, HLock);
         soundInfoPtr s = *sHdl;
-        if (s->snd != snd)
+        if (s->snd != snd) {
+            HUnlock(Handle(sHdl));
+            err = MemError();
+            TEST_ERR(err, HUnlock);
             continue;
+        }
         if (s->movie[0]) {
             StopMovie(s->movie[0]);
             DisposeMovie(s->movie[0]);
@@ -102,7 +138,14 @@ void RageSound_QT::StopMixing(RageSound *snd) {
             StopMovie(s->movie[1]);
             DisposeMovie(s->movie[1]);
         }
+        sounds.erase(sounds.begin()+i);
+        HUnlock(Handle(sHdl));
+        err = MemError();
+        TEST_ERR(err, HUnlock);
         DisposeHandle(Handle(sHdl));
+        err = MemError();
+        TEST_ERR(err, DisposeHandle);
+        break;
     }
 }
 
@@ -111,56 +154,87 @@ void RageSound_QT::Update(float delta) {
     LockMutex L(SOUNDMAN->lock);
     unsigned size = sounds.size();
     vector<soundInfoHdl> snds = sounds;
+    OSErr err;
 
     for (unsigned i=0; i<size; ++i) {
-        soundInfoPtr s = *(snds[i]);
+        soundInfoHdl sHdl = snds[i];
+        HLock(Handle(sHdl));
+        err = MemError();
+        TEST_ERR(err, HLock);
+        soundInfoPtr s = *sHdl;
         if ((s->movie[0] && !IsMovieDone(s->movie[0])) ||
-            (s->movie[1] && !IsMovieDone(s->movie[1])))
+            (s->movie[1] && !IsMovieDone(s->movie[1]))) {
+            HUnlock(Handle(sHdl));
             continue;
+        }
+        /* StopPlaying() will call StopMixing which will unlock and dispose of the handle */
         s->snd->StopPlaying();
     }
 }
 
 int RageSound_QT::GetPosition(const RageSound *snd) const {
     LockMutex L(SOUNDMAN->lock);
-    soundInfoPtr s;
     unsigned size = sounds.size();
    /* Find the info */
     for (unsigned i=0; i<size; ++i) {
-        s = *(sounds[i]);
-        if (s->snd != snd)
+        soundInfoHdl sHdl = sounds[i];
+        HLock(Handle(sHdl));
+        OSStatus err = MemError();
+        TEST_ERR(err, HLock);
+        soundInfoPtr s = *sHdl;
+        if (s->snd != snd) {
+            HUnlock(Handle(sHdl));
             continue;
-        if (s->finishedBuffers == -1)
+        }
+        if (s->finishedBuffers == -1) {
+            HUnlock(Handle(sHdl));
             return 0; /* It hasn't started yet. */
-        return s->finishedBuffers * samples + GetMovieTime(s->movie[!s->fillme], NULL);
+        }
+        int ret = s->finishedBuffers * samples + GetMovieTime(s->movie[!s->fillme], NULL);
+        HUnlock(Handle(sHdl));
+        LOG->Trace("GetPostion is returning: %d", ret);
+        return ret;
     }
     RageException::Throw("Can't get the position of a sound that isn't playing");
-    return 0; /* unrechable statement */
+    //return 0; /* unrechable statement */
 }
 
 
 void CallBack(QTCallBack cb, long refCon) {
     LockMutex L(SOUNDMAN->lock);
-    soundInfoPtr s = soundInfoPtr(refCon);
+    LOG->Trace("CallBack");
+    soundInfoHdl sHdl = soundInfoHdl(refCon);
+    HLock(Handle(sHdl));
+    OSStatus err = MemError();
+    TEST_ERR(err, HLock);
+    soundInfoPtr s = *sHdl;
 
     s->finishedBuffers++;
     if (!cb)
         GetData(s);
     long playme = !s->fillme;
-    SetMovieVolume(s->movie[playme], kFullVolume);
+    ASSERT(s->movie[playme]);
     GoToBeginningOfMovie(s->movie[playme]);
     StartMovie(s->movie[playme]);
+    err = GetMoviesError();
+    TEST_ERR(err, StartMovie);
     if (s->stopPos == 0) {
         /* Not stopping */
         GetData(s);
         QTCallBack callBack = NewCallBack(GetMovieTimeBase(s->movie[playme]), callBackAtExtremes);
-        CallMeWhen(callBack, CallBack, long(s), 0, 0, 0);
+        CallMeWhen(callBack, CallBack, long(sHdl), 0, 0, 0);
     }
+    HUnlock(Handle(sHdl));
+    err = MemError();
+    TEST_ERR(err, HUnlock);
     DisposeCallBack(cb);
+    
 }
 
 inline void GetData(soundInfoPtr s) {
-    static long flags = newMovieActive;
+    LOG->Trace("GetData");
+    /* attempt one failed! */
+#if 0
     Handle hdl, dataRef = NULL;
     Track targetTrack = NULL;
     TimeValue addedDuration = 0;
@@ -169,7 +243,7 @@ inline void GetData(soundInfoPtr s) {
     ComponentResult result;    
     Movie &movie = s->movie[s->fillme];
     char buffer[buffersize];
-    int got = s->snd->GetPCM(buffer, buffersize, s->lastPos);
+    unsigned got = s->snd->GetPCM(buffer, buffersize, s->lastPos);
 
     if (got < buffersize) {
         s->stopPos = got / samplesize + s->lastPos;
@@ -178,8 +252,8 @@ inline void GetData(soundInfoPtr s) {
     }
     s->lastPos += samples;
 
-    /* I really don' understand this part. Oh well. */
-    hdl = NewHandle(got);
+    /* I really don't understand this part. Oh well. */
+    hdl = NewHandleClear(got);
     HLock(hdl);
     BlockMove(buffer, *hdl, got);
     err = PtrToHand(&hdl, &dataRef, sizeof(Handle)); /* um... */
@@ -190,13 +264,88 @@ inline void GetData(soundInfoPtr s) {
     if (movie)
         DisposeMovie(movie);
     
-    movie = NewMovie(flags);
+    movie = NewMovie(0);
     SetMovieTimeScale(movie, freq);
     if (GetMoviesError() != noErr)
         RageException::Throw("Couldn't create new movie");
     result = MovieImportDataRef(miComponent, dataRef, HandleDataHandlerSubType,
                                 movie, NULL, &targetTrack, NULL, &addedDuration,
                                 movieImportCreateTrack, &outFlags);
+    TEST_ERR(result, MovieImportDataRef);
+    DisposeHandle(dataRef);
     s->fillme = !s->fillme;
+#else
+    char buffer[buffersize];
+    unsigned got = s->snd->GetPCM(buffer, buffersize, s->lastPos);
+
+    if (got < buffersize) {
+        s->stopPos = got / samplesize + s->lastPos;
+        if (got == 0)
+            return;
+    }
+    s->lastPos += samples;
+    
+    OSErr err = noErr;
+    if (s->movie[s->fillme])
+        DisposeMovie(s->movie[s->fillme]);
+
+    s->movie[s->fillme] = NewMovie(newMovieActive);
+    err = GetMoviesError();
+    TEST_ERR(err, NewMovie);
+    SetMovieTimeScale(s->movie[s->fillme], freq);
+    err = GetMoviesError();
+    TEST_ERR(err, SetMovieTimeScale);
+
+    /* Put the sound data into a handle */
+    Handle sndHdl;
+
+    err = PtrToHand(buffer, &sndHdl, got);
+    TEST_ERR(err, PtrToHand);
+
+    
+    /* Create the SoundDescription */
+    SoundDescriptionHandle sndDescHdl = (SoundDescriptionHandle)NewHandleClear(sizeof(SoundDescription));
+    
+    err = MemError();
+    TEST_ERR(err, NewHandle);
+    {
+        SoundDescriptionPtr sndDescPtr = *sndDescHdl;
+        sndDescPtr->descSize = sizeof(SoundDescription);
+        sndDescPtr->dataFormat = k16BitBigEndianFormat;
+        sndDescPtr->numChannels = numchannels;
+        sndDescPtr->sampleSize = samplesize / numchannels;
+        sndDescPtr->sampleRate = freq << 16;
+    }
+
+    /* Add data to media and then add media to track */
+    Handle dataRef;
+
+    PtrToHand(&sndHdl, &dataRef, sizeof(Handle));
+    LOG->Trace("Create new Track");
+    Track track = NewMovieTrack(s->movie[s->fillme], 0, 0, kFullVolume);
+    err = GetMoviesError();
+    TEST_ERR(err, NewMovieTrack);
+    ASSERT(track);
+
+    LOG->Trace("Create new Media");
+    Media media = NewTrackMedia(track, SoundMediaType, freq, dataRef, HandleDataHandlerSubType);
+    err = GetMoviesError();
+    TEST_ERR(err, NewTrackMedia);
+    ASSERT(media);
+
+    LOG->Trace("BeginMediaEdits");
+    err = BeginMediaEdits(media);
+    TEST_ERR(err, BeginMediaEdits);
+    err = AddMediaSample(media, sndHdl, 0, got, 1, (SampleDescriptionHandle)sndDescHdl,
+                         samples, 0, NULL);
+    TEST_ERR(err, AddMediaSample);
+    err = EndMediaEdits(media);
+    TEST_ERR(err, EndMediaEdits);
+    err = InsertMediaIntoTrack(track, 0, 0, GetMediaDuration(media), 0x00010000);
+    TEST_ERR(err, InsertMediaIntoTrack);
+    if (sndDescHdl)
+        DisposeHandle(Handle(sndDescHdl));
+    s->fillme = !s->fillme;
+#endif
 }
 
--- stepmania/src/arch/Sound/RageSoundDriver_QT.h
+++ stepmania/src/arch/Sound/RageSoundDriver_QT.h
@@ -16,31 +16,23 @@
 * is included in arch.cpp along w/ (eventually) this.
 * How do I do this without using phony namespaces?
 * --Steve
-*/
+*
 namespace QT {
 #include <QuickTime/QuickTime.h>
-}
-#include "RageSound.h"
+}*/
 #include "RageSoundDriver.h"
 
 class RageSound_QT: public RageSoundDriver {
-private:
-    QT::ComponentInstance soundOutput;
-	float latency;
-
-	bool CanPlayMultiChannels();
-
 protected:
 	virtual void StartMixing(RageSound *snd);
 	virtual void StopMixing(RageSound *snd);
 	virtual int GetPosition(const RageSound *snd) const;
 	virtual void Update (float delta);
-	virtual float GetPlayLatency() const { return latency; }
+	virtual float GetPlayLatency() const { return 0; }
 
 public:
 	RageSound_QT();
 	virtual ~RageSound_QT();
-	static void GetData(QT::SndChannel *chan, QT::SndCommand *cmd_passed);
 };
 
 #endif /* RAGE_SOUND_QT */
