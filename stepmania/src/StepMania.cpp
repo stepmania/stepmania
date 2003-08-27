bccafc6bec6e9e399814772ec6d74a5c0f1b09c1
Ben Anderson <bashmaster@users.sourceforge.net>
2003-08-27 01:30:32 +0000

    Changed the screenshot key to F11 so that at least it works in Linux. Feel free to think of a better solution.

--- stepmania/src/StepMania.cpp
+++ stepmania/src/StepMania.cpp
@@ -694,7 +694,7 @@ bool HandleGlobalInputs( DeviceInput DeviceI, InputEventType type, GameInput Gam
 #ifndef DARWIN
 	/* XXX: Er, this doesn't work; Windows traps this key and takes a screenshot
 	 * into the clipboard.  I don't want to disable that, though; it's useful. */
-	if(DeviceI == DeviceInput(DEVICE_KEYBOARD, SDLK_SYSREQ))
+	if(DeviceI == DeviceInput(DEVICE_KEYBOARD, SDLK_F11))
 #else
 	if(DeviceI == DeviceInput(DEVICE_KEYBOARD, SDLK_KP_MULTIPLY))
 #endif
