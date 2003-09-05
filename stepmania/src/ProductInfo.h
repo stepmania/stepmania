27dad54b95706b1b7541eff46564f0a3fe3eafcc
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-09-05 22:57:19 +0000

    Some people have been distributing CVS snapshots without changing the identification, resulting in crash dumps coming in identifying themselves as alpha 3 or alpha 4 and being something else entirely.  Let's leave the strings in CVS set to something else.

--- stepmania/src/ProductInfo.h
+++ stepmania/src/ProductInfo.h
@@ -2,5 +2,6 @@
 // Don't forget to also change ProductInfo.nsinc!
 
 #define PRODUCT_NAME "StepMania"
-#define PRODUCT_VER "3.9 alpha 4b"
+#define PRODUCT_VER "3.9 random build"
+// #define PRODUCT_VER "3.9 alpha 4b"
 #define PRODUCT_NAME_VER PRODUCT_NAME " " PRODUCT_VER
