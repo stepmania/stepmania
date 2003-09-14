1dba3cd96da5725479aa5878aaf7fe1d982e7cda
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-09-14 01:49:03 +0000

    allow setting up to 15-feet in the editor, if you really want to

--- stepmania/src/ScreenEdit.cpp
+++ stepmania/src/ScreenEdit.cpp
@@ -168,7 +168,7 @@ Menu g_EditNotesStatistics
 (
 	"Statistics",
 	MenuRow( "Difficulty",	true, 0, "BEGINNER","EASY","MEDIUM","HARD","CHALLENGE" ),
-	MenuRow( "Meter",		true, 0, "1","2","3","4","5","6","7","8","9","10","11" ),
+	MenuRow( "Meter",		true, 0, "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15" ),
 	MenuRow( "Description",	true ),
 	MenuRow( "Tap Steps",	false ),
 	MenuRow( "Hold Steps",	false ),
