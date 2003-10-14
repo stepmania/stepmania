4c5117867042a1268360f6bc7e38701416e3d165
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-10-14 01:33:14 +0000

    um, no, not that file

--- stepmania/src/ProductInfo.h
+++ stepmania/src/ProductInfo.h
@@ -2,6 +2,6 @@
 // Don't forget to also change ProductInfo.nsinc!
 
 #define PRODUCT_NAME "StepMania"
-//#define PRODUCT_VER "3.9 random build"
-#define PRODUCT_VER "3.9 alpha 7"
+#define PRODUCT_VER "3.9 random build"
+//#define PRODUCT_VER "3.9 alpha 7"
 #define PRODUCT_NAME_VER PRODUCT_NAME " " PRODUCT_VER
