04e28b05d0614847902075437c08620386ba2261
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-10-16 09:07:04 +0000

    support g_bAutoRestart

--- stepmania/src/arch/ArchHooks/ArchHooks_Win32.cpp
+++ stepmania/src/arch/ArchHooks/ArchHooks_Win32.cpp
@@ -9,6 +9,7 @@
 #include "archutils/win32/crash.h"
 #include "archutils/win32/DebugInfoHunt.h"
 #include "archutils/win32/GotoURL.h"
+#include "archutils/win32/RestartProgram.h"
 #include "ProductInfo.h"
 
 ArchHooks_Win32::ArchHooks_Win32()
@@ -131,30 +132,10 @@ static BOOL CALLBACK ErrorWndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lP
 			GotoURL( "http://sourceforge.net/tracker/?func=add&group_id=37892&atid=421366" );
 			break;
 		case IDC_BUTTON_RESTART:
-			{
-				/* Clear the startup mutex, since we're starting a new
-				 * instance before ending ourself. */
-				TCHAR szFullAppPath[MAX_PATH];
-				GetModuleFileName(NULL, szFullAppPath, MAX_PATH);
+			Win32RestartProgram();
+			/* not reached */
+			ASSERT( 0 );
 
-				// Launch StepMania
-				PROCESS_INFORMATION pi;
-				STARTUPINFO	si;
-				ZeroMemory( &si, sizeof(si) );
-
-				CreateProcess(
-					NULL,		// pointer to name of executable module
-					szFullAppPath,		// pointer to command line string
-					NULL,  // process security attributes
-					NULL,   // thread security attributes
-					false,  // handle inheritance flag
-					0, // creation flags
-					NULL,  // pointer to new environment block
-					NULL,   // pointer to current directory name
-					&si,  // pointer to STARTUPINFO
-					&pi  // pointer to PROCESS_INFORMATION
-				);
-			}
 			EndDialog( hWnd, 0 );
 			break;
 
@@ -168,6 +149,9 @@ static BOOL CALLBACK ErrorWndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lP
 
 void ArchHooks_Win32::MessageBoxError( CString error )
 {
+	if( g_bAutoRestart )
+		Win32RestartProgram();
+
 	g_sErrorString = error;
  	// throw up a pretty error dialog
 	AppInstance handle;
