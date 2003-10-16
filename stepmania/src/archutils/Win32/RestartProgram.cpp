c3bc5440d5670189952f7bfd4449bd197c56195e
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-10-16 08:56:58 +0000

    add RestartProgram

---
+++ stepmania/src/archutils/Win32/RestartProgram.cpp
@@ -0,0 +1,29 @@
+#include "global.h"
+#include "RestartProgram.h"
+
+void Win32RestartProgram()
+{
+	TCHAR szFullAppPath[MAX_PATH];
+	GetModuleFileName(NULL, szFullAppPath, MAX_PATH);
+
+	// Launch StepMania
+	PROCESS_INFORMATION pi;
+	STARTUPINFO	si;
+	ZeroMemory( &si, sizeof(si) );
+	CreateProcess(
+		NULL,		// pointer to name of executable module
+		szFullAppPath,		// pointer to command line string
+		NULL,  // process security attributes
+		NULL,   // thread security attributes
+		false,  // handle inheritance flag
+		0, // creation flags
+		NULL,  // pointer to new environment block
+		NULL,   // pointer to current directory name
+		&si,  // pointer to STARTUPINFO
+		&pi  // pointer to PROCESS_INFORMATION
+	);
+
+	ExitProcess( 0 );
+
+	/* not reached */
+}
---
+++ stepmania/src/archutils/Win32/RestartProgram.h
@@ -0,0 +1,6 @@
+#ifndef RESTART_PROGRAM_H
+#define RESTART_PROGRAM_H
+
+void Win32RestartProgram();
+
+#endif
