0a8d3010cfcca2c86147e2cb0695ce2497fff9da
Glenn Maynard <gmaynard@users.sourceforge.net>
2003-10-20 20:47:13 +0000

    cleanup

--- stepmania/autogen.sh
+++ stepmania/autogen.sh
@@ -70,15 +70,3 @@ $AUTOCONF
 $AUTOHEADER
 $AUTOMAKE $AUTOMAKE_OPTIONS
 
-#the other configure files from cvs are not executable. Make them executable.
-chmod a+x $srcdir/src/SDL-1.2.5/configure $srcdir/src/SDL_sound-1.0.0/configure 2>/dev/null
-
-#make -f Makefile.am srcdir=. acinclude.m4
-
-#if test -z "$*"; then
-#  echo "**Warning**: I am going to run \`configure' with no arguments."
-#  echo "If you wish to pass any to it, please specify them on the"
-#  echo \`$0\'" command line."
-#  echo
-#fi
-
