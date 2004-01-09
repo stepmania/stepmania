1292180789e2d8d38396c3e6e8bdbccb17b10186
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-01-09 23:20:16 +0000

    dur

--- stepmania/src/ScreenOptions.cpp
+++ stepmania/src/ScreenOptions.cpp
@@ -635,7 +635,6 @@ void ScreenOptions::UpdateEnabledDisabled()
 			const float DiffuseAlpha = row.m_bHidden? 0.0f:1.0f;
 			if( row.m_textItems[0]->GetDestY() != row.m_fY ||
 			    row.m_textItems[0]->DestTweenState().diffuse[0][3] != DiffuseAlpha )
-			if( row.m_textItems[0]->GetDestY() != row.m_fY )
 			{
 				row.m_textItems[0]->StopTweening();
 				row.m_textItems[0]->BeginTweening( 0.3f );
