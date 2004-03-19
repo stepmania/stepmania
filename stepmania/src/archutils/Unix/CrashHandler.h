4c1d80c810566310fc8d29126d37dc44038615b4
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2004-03-19 08:26:39 +0000

    Add missing includes.

--- stepmania/src/archutils/Unix/CrashHandler.h
+++ stepmania/src/archutils/Unix/CrashHandler.h
@@ -2,6 +2,7 @@
 #define CRASH_HANDLER_H
 
 #include <signal.h>
+#include <ucontext.h>
 
 void ForceCrashHandler( const char *reason );
 void CrashSignalHandler( int signal, siginfo_t *si, const ucontext_t *uc );
@@ -9,6 +10,7 @@ void CrashHandlerHandleArgs( int argc, char* argv[] );
 void InitializeCrashHandler();
 	
 #if defined(DARWIN)
+#include <MachineExceptions.h>
 OSStatus CrashExceptionHandler( ExceptionInformation *e );
 #endif
 
