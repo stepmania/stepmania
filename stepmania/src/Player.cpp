8b1d60a558eebe0d4a50da81e12a266bd9d9a9db
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-03-28 08:49:57 +0000

    add debugging

--- stepmania/src/Player.cpp
+++ stepmania/src/Player.cpp
@@ -883,6 +883,9 @@ void PlayerMinus::Step( int col, RageTimer tm )
 		if( score==TNS_MARVELOUS  &&  !GAMESTATE->ShowMarvelous())
 			score = TNS_PERFECT;
 
+		LOG->Trace("XXX: %i at %f, %f ago, lbu %f ago, music at %f, step was at %f, off by %f (sc %f)",
+			score, fStepSeconds, fStepSeconds, GAMESTATE->m_LastBeatUpdate.Ago(),
+			fCurrentMusicSeconds, fMusicSeconds, fNoteOffset, fScaledSecondsFromPerfect );
 //		LOG->Trace("Note offset: %f (fSecondsFromPerfect = %f), Score: %i", fNoteOffset, fSecondsFromPerfect, score);
 		
 		SetTapNoteScore(col, iIndexOverlappingNote, score);
