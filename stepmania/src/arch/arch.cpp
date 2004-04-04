e37502d5c4825bd5f0d16d2732c0558d0dc7044e
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-04-04 00:58:53 +0000

    use arch_platform.h

--- stepmania/src/arch/arch.cpp
+++ stepmania/src/arch/arch.cpp
@@ -8,20 +8,7 @@
 
 #include "PrefsManager.h"
 #include "arch.h"
-
-/* Load default drivers. */
-#include "arch_default.h"
-
-/* Override them with arch-specific drivers, as available. */
-#if defined(LINUX)
-#include "arch_linux.h"
-#elif defined(DARWIN)
-#include "arch_darwin.h"
-#elif defined(_XBOX)
-#include "arch_xbox.h"
-#elif defined(_WINDOWS)
-#include "arch_Win32.h"
-#endif
+#include "arch_platform.h"
 
 LoadingWindow *MakeLoadingWindow()
 {
