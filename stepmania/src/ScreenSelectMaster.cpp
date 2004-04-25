cb9efe946db98bdc9328f49f37564fc9666f39db
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-04-25 22:37:54 +0000

    fix ChangeSelection in !SharedPreviewAndCursor mode

--- stepmania/src/ScreenSelectMaster.cpp
+++ stepmania/src/ScreenSelectMaster.cpp
@@ -480,15 +480,16 @@ bool ScreenSelectMaster::ChangeSelection( PlayerNumber pn, int iNewChoice )
 
 	for( int p=0; p<NUM_PLAYERS; p++ )
 	{
+		const int iOldChoice = m_iChoice[p];
+
+		if( !bMoveAll && p!=pn )
+			continue;	// skip
+
 		/* Set the new m_iChoice even for disabled players, since a player might
 		 * join on a SHARED_PREVIEW_AND_CURSOR after the cursor has been moved. */
-		const int iOldChoice = m_iChoice[p];
 		m_iChoice[p] = iNewChoice;
 
 		if( !GAMESTATE->IsHumanPlayer(p) )
-			continue;	// skip
-
-		if( !bMoveAll && p!=pn )
 			continue;	// skip
 
 		if( SHARED_PREVIEW_AND_CURSOR )
