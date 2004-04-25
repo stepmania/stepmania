f03f1b527bc0b9ae43134d5e3092669e25cb01a3
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-04-25 08:40:14 +0000

    remove code that's been commented out for eons

--- stepmania/src/ScreenStage.h
+++ stepmania/src/ScreenStage.h
@@ -28,33 +28,6 @@ public:
 	virtual void MenuBack( PlayerNumber pn );
 
 private:
-
-
-	// Ez2 and Pump Stage screens should be re-implemented as BGAnimations
-
-/*
-	// Common Elements
-	Sprite			m_sprNumbers[4];	// up to 3 numbers and suffix
-	Sprite			m_sprStage;			// "Stage", "Final Stage", etc.
-	// A frame for m_sprNumbers and m_sprStage, so they can be
-	// manipulated as a unit.
-	ActorFrame		m_frameStage;
-
-	// Ez2 Elements
-
-	Sprite			m_sprbg[3];
-	Sprite			m_sprbgxtra;
-	Sprite			m_sprScrollingBlobs[2][20];
-	BitmapText		m_ez2ukm[2];
-	BitmapText		m_stagename;
-	BitmapText		m_stagedesc[2];
-
-	// Pump Elements
-
-	Sprite m_sprSongBackground;
-
-	Quad			m_quadMask;		// write this into ZBuffer as a mask
-*/
 	Transition	m_In, m_Out, m_Back;
 	BGAnimation				m_Background;
 	BGAnimation				m_Overlay; // overlays all elements except bitmaptexts
