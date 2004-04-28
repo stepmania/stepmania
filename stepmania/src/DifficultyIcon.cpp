4044ec407378667a3092a8ca451de3b7ff673218
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-04-28 23:36:18 +0000

    we give a warning about this earlier, so let's not die because of it here

--- stepmania/src/DifficultyIcon.cpp
+++ stepmania/src/DifficultyIcon.cpp
@@ -64,7 +64,7 @@ void DifficultyIcon::SetFromDifficulty( PlayerNumber pn, Difficulty dc )
 	{
 	case NUM_DIFFICULTIES:		SetState( dc );			break;
 	case NUM_DIFFICULTIES*2:	SetState( dc*2+pn );	break;
-	default:		FAIL_M( ssprintf( "%s: %i", this->GetID().c_str(), GetNumStates()) );
+	default:					m_bBlank = true;		break;
 	}	
 }
 
