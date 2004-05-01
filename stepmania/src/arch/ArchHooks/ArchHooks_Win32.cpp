53fd27b88536958c5e7a9ce968614080106a950c
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-05-01 01:44:07 +0000

    disable "drive not ready", etc. dialogs

--- stepmania/src/arch/ArchHooks/ArchHooks_Win32.cpp
+++ stepmania/src/arch/ArchHooks/ArchHooks_Win32.cpp
@@ -17,6 +17,10 @@ ArchHooks_Win32::ArchHooks_Win32()
 {
 	SetUnhandledExceptionFilter(CrashHandler);
 	TimeCritMutex = new RageMutex("TimeCritMutex");
+
+	/* Disable critical errors, and handle them internally.  We never want the
+	 * "drive not ready", etc. dialogs to pop up. */
+	SetErrorMode( SetErrorMode(0) | SEM_FAILCRITICALERRORS );
 }
 
 ArchHooks_Win32::~ArchHooks_Win32()
