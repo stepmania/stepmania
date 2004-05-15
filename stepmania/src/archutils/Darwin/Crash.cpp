e8e33efa877fbe855b78d07270d57bba018668b8
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-05-15 20:13:19 +0000

    license update

--- stepmania/src/archutils/Darwin/Crash.cpp
+++ stepmania/src/archutils/Darwin/Crash.cpp
@@ -1,12 +1,3 @@
-/*
- *  Crash.cpp
- *  stepmania
- *
- *  Created by Steve Checkoway on Thu Jul 31 2003.
- *  Copyright (c) 2003 Steve Checkoway. All rights reserved.
- *
- */
-
 #include "StdString.h"
 #include "Crash.h"
 #include "archutils/Unix/CrashHandler.h"
@@ -50,3 +41,28 @@ void InformUserOfCrash( const CString &sPath )
     CFRelease(Cancel);
     CFRelease(error);
 }
+
+/*
+ * (c) 2003-2004 Steve Checkoway
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
