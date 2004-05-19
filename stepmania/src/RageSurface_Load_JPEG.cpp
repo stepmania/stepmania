17d8c4389624b4fae859d093b5c51a8382f510b0
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-05-19 02:26:34 +0000

    jpeg reading fix

--- stepmania/src/RageSurface_Load_JPEG.cpp
+++ stepmania/src/RageSurface_Load_JPEG.cpp
@@ -94,6 +94,7 @@ void RageFile_JPEG_skip_input_data( j_decompress_ptr cinfo, long num_bytes )
 	int in_buffer = min( (long) src->pub.bytes_in_buffer, num_bytes );
 	src->pub.next_input_byte += in_buffer;
 	src->pub.bytes_in_buffer -= in_buffer;
+	num_bytes -= in_buffer;
 
 	if( num_bytes )
 		src->file->SeekCur( num_bytes );
