f77715125641ad5193676ae6ba6dd0d3c9f5fd31
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-05-25 04:26:46 +0000

    license update; removed old "license" to avoid confusion

--- stepmania/src/ezsockets.cpp
+++ stepmania/src/ezsockets.cpp
@@ -3,10 +3,6 @@
    Designed by Josh Allen and Charles
    Lohr. Socket programming methods based
    on Charles Lohr's EZW progam.
-   You may freely destribute this code
-   if this message is retained.
-   This code is distributed on an as-is
-   basis with no gaurentees whatsoever.
 
   Modified by Charles Lohr for use with 
    Windows-Based OSes.
@@ -423,3 +419,28 @@ ostream& operator <<(ostream &os, EzSockets &obj)
 	os<<readString;
 	return os;
 }
+
+/*
+ * (c) 2003-2004 Charles Lohr, Josh Allen
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
