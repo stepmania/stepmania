5ef80799c9df66e374b67244a98b2c9b4bc2366c
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-05-29 23:15:18 +0000

    move some Windows build junk out of the top directory cleanups while moving things: StepMania.ICO -> StepMania.ico, StepMania.RC -> WindowsResources.rc, resource.h -> WindowsResources.h, error.bmp -> DialogCrashHeader.bmp, error2.bmp -> DialogErrorHeader.bmp

---
+++ stepmania/src/archutils/Win32/WindowsResources.h
@@ -0,0 +1,52 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by StepMania.RC
+//
+#define IDC_CRASH_SAVE                  16
+#define IDD_ERROR_DIALOG                111
+#define IDD_LOADING_DIALOG              116
+#define BITMAP_LOADING                  117
+#define BITMAP_CRASH                    121
+#define BITMAP_ERROR                    129
+#define IDD_OK                          129
+#define IDD_DRIVER                      130
+#define IDD_DISASM_CRASH                199
+#define IDC_SHOCKWAVEFLASH1             1000
+#define IDC_BUTTON_RESTART              1001
+#define IDM_TOGGLEFULLSCREEN            1002
+#define IDC_BUTTON_REPORT               1002
+#define IDM_CHANGERESOLUTION            1003
+#define IDC_BUTTON_VIEW_LOG             1003
+#define IDM_CHANGEDETAIL                1003
+#define IDM_CHANGEDISPLAYCOLOR          1004
+#define IDM_CHANGETEXTURECOLOR          1005
+#define IDC_EDIT_ERROR                  1005
+#define IDM_TOGGLESTATISTICS            1006
+#define IDI_ICON                        1007
+#define IDC_CURSOR                      1008
+#define IDC_STATIC_MESSAGE1             1010
+#define IDC_STATIC_MESSAGE2             1011
+#define IDC_BUTTON_CLOSE                1011
+#define IDC_VIEW_LOG                    1012
+#define IDC_STATIC_MESSAGE3             1013
+#define IDC_PROGRESS1                   1014
+#define IDC_HUSH                        1016
+#define IDC_MESSAGE                     1017
+#define IDC_CHECK2                      1019
+#define IDC_SPLASH                      1020
+#define IDC_ASMBOX                      1133
+#define IDC_REGDUMP                     1283
+#define IDC_STATIC_BOMBREASON           1284
+#define IDC_CALL_STACK                  1310
+#define IDM_EXIT                        40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
