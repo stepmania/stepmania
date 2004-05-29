f5e3a42d887f5eb723aa339fae976eee419a7dac
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-05-29 20:57:48 +0000

    cleanup, license update

--- stepmania/src/CatalogXml.cpp
+++ stepmania/src/CatalogXml.cpp
@@ -1,15 +1,4 @@
 #include "global.h"
-/*
------------------------------------------------------------------------------
- Class: CatalogXml
-
- Desc: See header.
-
- Copyright (c) 2001-2002 by the person(s) listed below.  All rights reserved.
-	Chris Danford
------------------------------------------------------------------------------
-*/
-
 #include "CatalogXml.h"
 #include "SongManager.h"
 #include "RageLog.h"
@@ -17,6 +6,7 @@
 #include "Steps.h"
 #include "XmlFile.h"
 #include "Course.h"
+#include "Profile.h"
 
 const CString CATALOG_XML	= "Catalog.xml";
 
@@ -93,4 +83,31 @@ void SaveCatalogXml( CString sDir )
 	}
 
 	xml.SaveToFile(fn);
+
+	LOG->Trace( "Done." );
 }
+
+/*
+ * (c) 2004 Chris Danford
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
