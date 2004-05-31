549cd7c83f42470f8a567ea6979e02e792e71488
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-05-31 21:35:31 +0000

    license updates

--- stepmania/src/ScoreKeeper.h
+++ stepmania/src/ScoreKeeper.h
@@ -1,22 +1,15 @@
+/* ScoreKeeper - Abstract class to handle scorekeeping, stat-taking, etc. */
+
 #ifndef SCOREKEEPER_H
-#define SCOREKEEPER_H 1
+#define SCOREKEEPER_H
+
 /*
------------------------------------------------------------------------------
- Class: ScoreKeeper
-
- Abstract class to handle scorekeeping, stat-taking, etc.
-
- Stat handling is in here because that can differ between games, too; for
- example, some games count double taps as a single note in scoring and
- some count per-tap.
-
- Results are injected directly into GameState.
-
- Copyright (c) 2001-2002 by the person(s) listed below.  All rights reserved.
-	Chris Danford
-	Glenn Maynard
------------------------------------------------------------------------------
-*/
+ * Stat handling is in here because that can differ between games, too; for
+ * example, some games count double taps as a single note in scoring and
+ * some count per-tap.
+ *
+ * Results are injected directly into GameState.
+ */
 
 #include "Actor.h"
 #include "PlayerNumber.h"
@@ -51,3 +44,28 @@ public:
 };
 
 #endif
+
+/*
+ * (c) 2001-2004 Chris Danford, Glenn Maynard
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
--- stepmania/src/StyleDef.h
+++ stepmania/src/StyleDef.h
@@ -1,25 +1,7 @@
+/* StyleDef - A data structure that holds the definition for one of a Game's styles. */
+
 #ifndef STYLEDEF_H
 #define STYLEDEF_H
-/*
------------------------------------------------------------------------------
- Class: StyleDef
-
- Desc: A data structure that holds the definition for one of a Game's styles.
-	Styles define a set of columns for each player, and information about those
-	columns, like what Instruments are used play those columns and what track 
-	to use to populate the column's notes.
-	A "track" is the term used to descibe a particular vertical sting of note 
-	in NoteData.
-	A "column" is the term used to describe the vertical string of notes that 
-	a player sees on the screen while they're playing.  Column notes are 
-	picked from a track, but columns and tracks don't have a 1-to-1 
-	correspondance.  For example, dance-versus has 8 columns but only 4 tracks
-	because two players place from the same set of 4 tracks.
-
- Copyright (c) 2001-2002 by the person(s) listed below.  All rights reserved.
-	Chris Danford
------------------------------------------------------------------------------
-*/
 
 #include "StyleInput.h"
 #include "GameInput.h"
@@ -82,3 +64,28 @@ public:
 
 
 #endif
+
+/*
+ * (c) 2001-2002 Chris Danford
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
