7dfd61158778dc1e69695ccbc5c4c6ce962acd11
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-05-31 21:55:14 +0000

    license updates

--- stepmania/src/NotesLoaderBMS.cpp
+++ stepmania/src/NotesLoaderBMS.cpp
@@ -1,5 +1,4 @@
 #include "global.h"
-
 #include "NotesLoaderBMS.h"
 #include "NotesLoader.h"
 #include "NoteData.h"
@@ -10,6 +9,8 @@
 #include "RageException.h"
 #include "RageFile.h"
 #include "StepsUtil.h"
+#include "song.h"
+#include "Steps.h"
 
 /*	BMS encoding:     tap-hold
 	4&8panel:   Player1     Player2
@@ -736,3 +737,28 @@ void BMSLoader::SlideDuplicateDifficulties( Song &p )
 		}
 	}
 }
+
+/*
+ * (c) 2001-2004 Chris Danford, Glenn Maynard
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
--- stepmania/src/NotesLoaderDWI.h
+++ stepmania/src/NotesLoaderDWI.h
@@ -1,20 +1,16 @@
+/* DWILoader - reads a Song from a .DWI file. */
+
 #ifndef NOTES_LOADER_DWI_H
 #define NOTES_LOADER_DWI_H
 
-#include "song.h"
-#include "Steps.h"
 #include "GameInput.h"
 #include "NotesLoader.h"
 
-#include "song.h"
-#include "Steps.h"
+class Song;
+class Steps;
 
-/* Return NA if no files in the directory can be loaded by
- * this loader, OK on success, ERROR if an applicable file was found
- * but there was a fatal error loading.  (ERROR not used yet--we
- * always throw.)
- */
-class DWILoader: public NotesLoader {
+class DWILoader: public NotesLoader
+{
 	void DWIcharToNote( char c, GameController i, DanceNote &note1Out, DanceNote &note2Out );
 	void DWIcharToNoteCol( char c, GameController i, int &col1Out, int &col2Out );
 
@@ -36,3 +32,28 @@ public:
 };
 
 #endif
+
+/*
+ * (c) 2001-2004 Chris Danford, Glenn Maynard
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
