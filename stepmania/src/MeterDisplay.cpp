12bf8f87fda8c7c813493615c7c4c5ec13f250c4
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-06-01 00:53:06 +0000

    license updates

--- stepmania/src/MeterDisplay.cpp
+++ stepmania/src/MeterDisplay.cpp
@@ -1,14 +1,4 @@
 #include "global.h"
-/*
------------------------------------------------------------------------------
- File: MeterDisplay.h
-
- Desc: The song's MeterDisplay displayed in SelectSong.
-
- Copyright (c) 2001-2002 by the person(s) listed below.  All rights reserved.
------------------------------------------------------------------------------
-*/
-
 #include "MeterDisplay.h"
 
 
@@ -39,3 +29,28 @@ void MeterDisplay::SetPercent( float fPercent )
 
 	m_sprTip->SetX( SCALE(fPercent, 0.f, 1.f, -m_fStreamWidth/2, m_fStreamWidth/2) );
 }
+
+/*
+ * (c) 2003-2004 Chris Danford
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
