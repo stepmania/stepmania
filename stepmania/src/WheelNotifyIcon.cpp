48b341fe51c30ebb1d535f3079ad5748ed1c0c28
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-06-01 00:57:14 +0000

    license update DrawPrimitives() should generally be const: draw, don't change don't forcibly change the diffuse color; simply don't draw

--- stepmania/src/WheelNotifyIcon.cpp
+++ stepmania/src/WheelNotifyIcon.cpp
@@ -63,22 +63,48 @@ void WheelNotifyIcon::SetFlags( Flags flags )
 
 
 	m_vIconsToShow.resize( min(m_vIconsToShow.size(),2u) );	// crop to most important 2
-
-	if( m_vIconsToShow.size() == 0 )
-		Sprite::SetDiffuse( RageColor(1,1,1,0) );
-	else
-		Sprite::SetDiffuse( RageColor(1,1,1,1) );
 }
 
-void WheelNotifyIcon::DrawPrimitives()
+bool WheelNotifyIcon::EarlyAbortDraw()
 {
-		float fSecondFraction = fmodf(RageTimer::GetTimeSinceStart(), 1 );
+	if( m_vIconsToShow.empty() )
+		return true;
+	return Sprite::EarlyAbortDraw();
+}
+
+void WheelNotifyIcon::Update( float fDeltaTime )
+{
+	float fSecondFraction = fmodf(RageTimer::GetTimeSinceStart(), 1 );
 	if( m_vIconsToShow.size() > 0 )
 	{
 		int index = (int)(fSecondFraction*m_vIconsToShow.size());
 		Sprite::SetState( m_vIconsToShow[index] );
 	}
 
-	Sprite::DrawPrimitives();
+	Sprite::Update( fDeltaTime );
 }
 
+/*
+ * (c) 2001-2004 Chris Danford
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
--- stepmania/src/WheelNotifyIcon.h
+++ stepmania/src/WheelNotifyIcon.h
@@ -1,20 +1,9 @@
-#ifndef WheelNotifyIcon_H
-#define WheelNotifyIcon_H
-/*
------------------------------------------------------------------------------
- Class: WheelNotifyIcon
+/* WheelNotifyIcon - A little graphic to the left of the song's text banner in the MusicWheel. */
 
- Desc: A little graphic to the left of the song's text banner in the MusicWheel.
-
- Copyright (c) 2001-2002 by the person(s) listed below.  All rights reserved.
-	Chris Danford
------------------------------------------------------------------------------
-*/
-
+#ifndef WHEEL_NOTIFY_ICON_H
+#define WHEEL_NOTIFY_ICON_H
 
 #include "Sprite.h"
-
-
 
 class WheelNotifyIcon : public Sprite
 {
@@ -32,8 +21,9 @@ public:
 
 	void SetFlags( Flags flags );
 
-	virtual void DrawPrimitives();
-	
+	virtual void Update( float fDeltaTime );
+	virtual bool EarlyAbortDraw();
+
 protected:
 	enum Icons { training=0, best1, best2, best3, edits, long_ver, marathon, empty };
 
@@ -41,3 +31,28 @@ protected:
 };
 
 #endif
+
+/*
+ * (c) 2001-2004 Chris Danford
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
