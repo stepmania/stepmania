3e036b7bf8ef4353cd3b8321947ab7c5057f02b7
Chris Danford <chrisdanford@users.sourceforge.net>
2004-06-06 23:26:43 +0000

    formatting cleanup

--- stepmania/src/arch/MemoryCard/MemoryCardDriver.h
+++ stepmania/src/arch/MemoryCard/MemoryCardDriver.h
@@ -4,10 +4,10 @@
 struct UsbStorageDevice
 {
 	UsbStorageDevice() { MakeBlank(); }
-
+	
 	void MakeBlank()
 	{
-	  // -1 means "don't know"
+		// -1 means "don't know"
 		iBus = -1;
 		iPort = -1;
 		iLevel = -1;
@@ -22,13 +22,13 @@ struct UsbStorageDevice
 	int iLevel;
 	CString sSerial;
 	int iScsiIndex;
-  CString sScsiDevice;
+	CString sScsiDevice;
 	CString	sOsMountDir;	// WITHOUT trailing slash
 	CString	sName;	// Name in the profile on the memory card.  
-					// This is passed here because it's read in the mounting thread.
-
+	// This is passed here because it's read in the mounting thread.
+	
 	bool IsBlank() { return sOsMountDir.empty(); }
-
+	
 	bool operator==(const UsbStorageDevice& other) const
 	{
 #define COMPARE(x) if( x != other.x ) return false;
@@ -40,10 +40,10 @@ struct UsbStorageDevice
 		return true;
 #undef COMPARE
 	}
-  bool operator!=(const UsbStorageDevice& other) const
-  {
-    return !operator==(other);
-  }
+	bool operator!=(const UsbStorageDevice& other) const
+	{
+		return !operator==(other);
+	}
 };
 
 class MemoryCardDriver
@@ -64,27 +64,27 @@ public:
 #endif
 
 /*
- * (c) 2003-2004 Chris Danford
- * All rights reserved.
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, and/or sell copies of the Software, and to permit persons to
- * whom the Software is furnished to do so, provided that the above
- * copyright notice(s) and this permission notice appear in all copies of
- * the Software and that both the above copyright notice(s) and this
- * permission notice appear in supporting documentation.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
- * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
- * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
- * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
- * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
- */
+* (c) 2003-2004 Chris Danford
+* All rights reserved.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, and/or sell copies of the Software, and to permit persons to
+* whom the Software is furnished to do so, provided that the above
+* copyright notice(s) and this permission notice appear in all copies of
+* the Software and that both the above copyright notice(s) and this
+* permission notice appear in supporting documentation.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+* THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+* INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+* OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+* OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+* OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+* PERFORMANCE OF THIS SOFTWARE.
+*/
 
