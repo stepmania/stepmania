95d10d712398e0d777916a70311719f5cb26f5cc
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-06-11 05:11:40 +0000

    license update

--- stepmania/src/arch/arch_darwin.h
+++ stepmania/src/arch/arch_darwin.h
@@ -1,18 +1,35 @@
 #ifndef ARCH_DARWIN_H
 #define ARCH_DARWIN_H
 
-/*
- *  arch_darwin.h
- *  stepmania
- *
- *  Created by Steve Checkoway on Fri Jun 13 2003.
- *  Copyright (c) 2003 Steve Checkoway. All rights reserved.
- *
- */
-
 #include "Sound/RageSoundDriver_CA.h"
 #include "Sound/RageSoundDriver_QT1.h"
 #include "LoadingWindow/LoadingWindow_Cocoa.h"
 #include "ArchHooks/ArchHooks_darwin.h"
 
 #endif
+
+/*
+ * (c) 2003 Steve Checkoway
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
