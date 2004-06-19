688b20abffcf546a659c63181acd59b9ce658a6d
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-06-19 02:21:37 +0000

    simplify

--- stepmania/src/ScreenLogo.cpp
+++ stepmania/src/ScreenLogo.cpp
@@ -1,22 +1,18 @@
 #include "global.h"
 #include "ScreenLogo.h"
+#include "ActorUtil.h"
 #include "ThemeManager.h"
 #include "GameState.h"
 #include "GameDef.h"
-#include "RageLog.h"
-#include "SongManager.h"
-
-#define LOGO_ON_COMMAND			THEME->GetMetric("ScreenLogo","LogoOnCommand")
-
 
 ScreenLogo::ScreenLogo( CString sName ) : ScreenAttract( sName )
 {
+	m_sprLogo.SetName( "Logo" );
 	m_sprLogo.Load( THEME->GetPathToG(ssprintf("ScreenLogo %s",GAMESTATE->GetCurrentGameDef()->m_szName)) );
-	m_sprLogo.Command( LOGO_ON_COMMAND );
+	ON_COMMAND( m_sprLogo );
 	this->AddChild( &m_sprLogo );
 
-	this->MoveToTail( &m_In );	// put it in the back so it covers up the stuff we just added
-	this->MoveToTail( &m_Out );	// put it in the back so it covers up the stuff we just added
+	this->SortByDrawOrder();
 }
 
 /*
