7b196e492efe0eeaff9c9d69df0c68ef9a9a06cc
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-07-14 21:08:59 +0000

    fix warning

--- stepmania/src/RageDisplay.cpp
+++ stepmania/src/RageDisplay.cpp
@@ -589,7 +589,7 @@ RageSurface *RageDisplay::CreateSurfaceFromPixfmt( PixelFormat pixfmt,
 RageDisplay::PixelFormat RageDisplay::FindPixelFormat( 
 	int bpp, int Rmask, int Gmask, int Bmask, int Amask, bool realtime )
 {
-	PixelFormatDesc tmp = { bpp, Rmask, Gmask, Bmask, Amask };
+	PixelFormatDesc tmp = { bpp, { Rmask, Gmask, Bmask, Amask } };
 
 	for(int pixfmt = 0; pixfmt < NUM_PIX_FORMATS; ++pixfmt)
 	{
