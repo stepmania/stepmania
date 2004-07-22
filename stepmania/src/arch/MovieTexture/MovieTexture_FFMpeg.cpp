f8cc7beb11f2091767b7caa4fd8265f5d9dd11a3
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-07-22 23:04:44 +0000

    fix spurious warnings

--- stepmania/src/arch/MovieTexture/MovieTexture_FFMpeg.cpp
+++ stepmania/src/arch/MovieTexture/MovieTexture_FFMpeg.cpp
@@ -845,7 +845,7 @@ void MovieTexture_FFMpeg::DecoderThread()
 	/* Windows likes to boost priority when processes come out of a wait state.  We don't
 	 * want that, since it'll result in us having a small priority boost after each movie
 	 * frame, resulting in skips in the gameplay thread. */
-	if( !SetThreadPriorityBoost(GetCurrentThread(), TRUE) && GetLastError() != ERROR_NOT_SUPPORTED )
+	if( !SetThreadPriorityBoost(GetCurrentThread(), TRUE) && GetLastError() != ERROR_CALL_NOT_IMPLEMENTED )
 		LOG->Warn( werr_ssprintf(GetLastError(), "SetThreadPriorityBoost failed") );
 #endif
 
