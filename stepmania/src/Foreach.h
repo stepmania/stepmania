ab479af3d3c6d224cb2fdb38e516daf61ab72125
Chris Danford <chrisdanford@users.sourceforge.net>
2004-07-23 01:23:42 +0000

    force proper order of operations

--- stepmania/src/Foreach.h
+++ stepmania/src/Foreach.h
@@ -2,8 +2,8 @@
 #define Foreach_H
 
 
-#define FOREACH( elemType, vect, var ) 			for( vector<elemType>::iterator var = vect.begin(); var != vect.end(); var++ )
-#define FOREACH_CONST( elemType, vect, var ) 	for( vector<elemType>::const_iterator var = vect.begin(); var != vect.end(); var++ )
+#define FOREACH( elemType, vect, var ) 			for( vector<elemType>::iterator var = (vect).begin(); var != (vect).end(); var++ )
+#define FOREACH_CONST( elemType, vect, var ) 	for( vector<elemType>::const_iterator var = (vect).begin(); var != (vect).end(); var++ )
 
 
 #endif
