5d0b1af78f3ffe5ff513886f79485b87ad57d8a6
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-08-30 19:50:13 +0000

    check for valid texture format

--- stepmania/src/arch/MovieTexture/MovieTexture_Null.cpp
+++ stepmania/src/arch/MovieTexture/MovieTexture_Null.cpp
@@ -28,6 +28,9 @@ MovieTexture_Null::MovieTexture_Null(RageTextureID ID) : RageMovieTexture(ID)
     CreateFrameRects();
 
     RageDisplay::PixelFormat pixfmt = RageDisplay::FMT_RGBA4;
+    if( !DISPLAY->SupportsTextureFormat(pixfmt) )
+	    pixfmt = RageDisplay::FMT_RGBA8;
+    ASSERT( DISPLAY->SupportsTextureFormat(pixfmt) );
 
     const RageDisplay::PixelFormatDesc *pfd = DISPLAY->GetPixelFormatDesc( pixfmt );
     RageSurface *img = CreateSurface( size, size, pfd->bpp,
