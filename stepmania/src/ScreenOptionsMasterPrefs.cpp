20dcf02ad1e15baa65bb70b8b82016c752e362db
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-09-05 06:00:25 +0000

    "0%" -> "SILENT"; remove extra "10%"

--- stepmania/src/ScreenOptionsMasterPrefs.cpp
+++ stepmania/src/ScreenOptionsMasterPrefs.cpp
@@ -517,7 +517,7 @@ static const ConfOption g_ConfOptions[] =
 	/* Sound options */
 	ConfOption( "Resampling\nQuality",	ResamplingQuality,	"FAST","NORMAL","HIGH QUALITY" ),
 	ConfOption( "Attract\nSound Frequency",	AttractSoundFrequency,	"NEVER","ALWAYS","2 TIMES","3 TIMES","4 TIMES","5 TIMES" ),
-	ConfOption( "Sound\nVolume",		SoundVolume,		"0%","10%","20%","30%","40%","50%","60%","70%","80%","90%","100%","10%" ),
+	ConfOption( "Sound\nVolume",		SoundVolume,		"SILENT","10%","20%","30%","40%","50%","60%","70%","80%","90%","100%" ),
 	ConfOption( "", NULL )	// end marker
 };
 
