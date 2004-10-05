30a1b81014e597b836ac97d31db1bf7909df1c94
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2004-10-05 10:32:50 +0000

    update

--- stepmania/PBProject/scripts/increment_version.sh
+++ stepmania/PBProject/scripts/increment_version.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#Stupid ProjectBuild wouldn't allow perl
-
 perl -w -e '
 open F, "<version.tst"||die;
 $ver = <F>;
@@ -14,6 +12,8 @@ close F;
 $date = `date`;
 chomp $date;
 open F, ">ver.cpp"||die;
-print F "unsigned long version_num = $ver;\n";print F "const char *version_time = \"$date\";\n";close F;
+print F "unsigned long version_num = $ver;\n";
+print F "const char *version_time = \"$date\";\n";
+close F;
 '
 touch ver.cpp
