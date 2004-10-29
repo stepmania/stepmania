dc8dfdad6dfaf3c5f7f289d3689cd58f68d1a944
Glenn Maynard <gmaynard@users.sourceforge.net>
2004-10-29 04:39:00 +0000

    rc2

--- stepmania/src/ProductInfo.h
+++ stepmania/src/ProductInfo.h
@@ -2,5 +2,5 @@
 
 #define PRODUCT_NAME "StepMania"
 #define PRODUCT_VER "3.9 random build"
-//#define PRODUCT_VER "3.9 rc1"
+//#define PRODUCT_VER "3.9 rc2"
 #define PRODUCT_NAME_VER PRODUCT_NAME " " PRODUCT_VER
