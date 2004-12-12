73e5f1092fff2d685205444650a92fe00190530d
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2004-12-12 09:58:14 +0000

    Unused variable.

--- stepmania/src/RageUtil.cpp
+++ stepmania/src/RageUtil.cpp
@@ -523,7 +523,6 @@ void CRC32( unsigned int &iCRC, const void *pVoidBuffer, size_t iSize )
 
 	iCRC ^= 0xFFFFFFFF;
 
-	unsigned crc = 0;
 	const char *pBuffer = (const char *) pVoidBuffer;
 	for( unsigned i = 0; i < iSize; ++i )
 		iCRC = (iCRC >> 8) ^ tab[(iCRC ^ pBuffer[i]) & 0xFF];
