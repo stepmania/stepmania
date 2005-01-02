f60254ba0e46889fd8753840bff45d7264bd56e6
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-01-02 23:04:25 +0000

    don't waste time zeroing RageVColor by default

--- stepmania/src/RageTypes.h
+++ stepmania/src/RageTypes.h
@@ -194,7 +194,7 @@ class RageVColor
 public:
 	uint8_t b,g,r,a;	// specific ordering required by Direct3D
 
-	RageVColor(): b(0), g(0), r(0), a(0) { }
+	RageVColor() { }
 	RageVColor(const RageColor &rc) { *this = rc; }
 	RageVColor &operator= (const RageColor &rc) {
 		r = FTOC(rc.r); g = FTOC(rc.g); b = FTOC(rc.b); a = FTOC(rc.a);
