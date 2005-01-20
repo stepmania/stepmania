6493010e49f58d8a43ae8810870afaad5e7aa5f3
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-01-20 00:37:33 +0000

    Use a Lua script to select the evaluation screen, instead of a ScreenBranch.
    This approach has several advantages.  It's more flexible; full conditionals
    can be used.  It mixes with things like the "bGoingToPlayTheSameMusic"
    logic in ScreenAttract much better; it gets the real real next screen, not
    the branch.  It also mixes with screen preloading, such as used by ScreenStage.
    
    (I don't know if grouping functions that are used like branches makes sense;
    I'm not sure how Lua will be used in general, so this stuff might get reorganized
    later.)

---
+++ stepmania/Themes/default/Scripts/Branches.lua
@@ -0,0 +1,10 @@
+function SelectEvaluationScreen()
+	if IsNetConnected() then return "ScreenNetEvaluation" end
+	Mode = PlayModeName()
+	if( Mode == "Regular" ) then return "ScreenEvaluationStage" end
+	if( Mode == "Nonstop" ) then return "ScreenEvaluationNonstop" end
+	if( Mode == "Oni" ) then return "ScreenEvaluationOni" end
+	if( Mode == "Endless" ) then return "ScreenEvaluationEndless" end
+	if( Mode == "Rave" ) then return "ScreenEvaluationRave" end
+	if( Mode == "Battle" ) then return "ScreenEvaluationBattle" end
+end
