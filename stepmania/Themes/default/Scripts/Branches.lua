6b2e1e6d131025ee75d488c9287e951921f42816
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-01-20 18:29:20 +0000

    more branch conversions

--- stepmania/Themes/default/Scripts/Branches.lua
+++ stepmania/Themes/default/Scripts/Branches.lua
@@ -1,3 +1,22 @@
+function SongSelectionScreen()
+	if PlayModeName() == "Nonstop" then return "ScreenSelectCourseNonstop" end
+	if PlayModeName() == "Oni" then return "ScreenSelectCourseOni" end
+	if PlayModeName() == "Endless" then return "ScreenSelectCourseEndless" end
+	if IsNetSMOnline() then return LoginScreen() end
+	if IsNetConnected() then return "ScreenNetSelectMusic" end
+	return "ScreenSelectMusic"
+end
+
+function LoginScreen()
+	if not IsSMOnlineLoggedIn(1) and IsPlayerEnabled(1) then
+		return "ScreenSMOnlineLogin1"
+	elseif not IsSMOnlineLoggedIn(2) and IsPlayerEnabled(2) then
+		return "ScreenSMOnlineLogin2"
+	else
+		return "ScreenNetRoom"
+	end
+end
+
 function SelectGameplayScreen()
 	if IsExtraStage() or IsExtraStage2() then return "ScreenGameplay" end
 	return "ScreenGameplay"
@@ -13,3 +32,14 @@ function SelectEvaluationScreen()
 	if( Mode == "Rave" ) then return "ScreenEvaluationRave" end
 	if( Mode == "Battle" ) then return "ScreenEvaluationBattle" end
 end
+
+function SelectFirstOptionsScreen()
+	if PlayModeName() == "Rave" then return ScreenRaveOptions end
+	return "ScreenPlayerOptions"
+end
+
+function SelectEndingScreen()
+	if GetBestFinalGrade() >= Grade("AAA") then return "ScreenMusicScroll" end
+	return "ScreenCredits"
+end	
+
