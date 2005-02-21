1b7d1943e359cbe0f989bebb076f82f21410ef25
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-02-21 06:39:03 +0000

    lua GetTotalNumSongsPlayed

--- stepmania/src/Profile.cpp
+++ stepmania/src/Profile.cpp
@@ -1693,6 +1693,8 @@ public:
 	static int SetGoalSeconds( T* p, lua_State *L )			{ p->m_iGoalSeconds = IArg(1); return 0; }
 	static int GetCaloriesBurnedToday( T* p, lua_State *L )	{ lua_pushnumber(L, p->GetCaloriesBurnedToday() ); return 1; }
 	static int GetSaved( T* p, lua_State *L )				{ p->m_SavedLuaData.PushSelf(L); return 1; }
+	static int GetTotalNumSongsPlayed( T* p, lua_State *L )	{ lua_pushnumber(L, p->GetTotalNumSongsPlayed() ); return 1; }
+	
 
 	static void Register(lua_State *L)
 	{
@@ -1706,6 +1708,7 @@ public:
 		ADD_METHOD( SetGoalSeconds )
 		ADD_METHOD( GetCaloriesBurnedToday )
 		ADD_METHOD( GetSaved )
+		ADD_METHOD( GetTotalNumSongsPlayed )
 		Luna<T>::Register( L );
 	}
 };
