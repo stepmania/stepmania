5a63fb3cd3b2cfbb2097635a0cfceddbfbe14e64
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-02-26 20:44:16 +0000

    add GetMeter

--- stepmania/src/Steps.cpp
+++ stepmania/src/Steps.cpp
@@ -339,12 +339,14 @@ public:
 	static int GetStepsType( T* p, lua_State *L )	{ lua_pushnumber(L, p->m_StepsType ); return 1; }
 	static int GetDifficulty( T* p, lua_State *L )	{ lua_pushnumber(L, p->GetDifficulty() ); return 1; }
 	static int GetDescription( T* p, lua_State *L )	{ lua_pushstring(L, p->GetDescription() ); return 1; }
+	static int GetMeter( T* p, lua_State *L )		{ lua_pushnumber(L, p->GetMeter() ); return 1; }
 
 	static void Register(lua_State *L)
 	{
 		ADD_METHOD( GetStepsType )
 		ADD_METHOD( GetDifficulty )
 		ADD_METHOD( GetDescription )
+		ADD_METHOD( GetMeter )
 		Luna<T>::Register( L );
 	}
 };
