1788322c4b0f78708d0b5365d8b492d31efc3a78
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-02-27 21:55:30 +0000

    fix assert

--- stepmania/src/SongManager.cpp
+++ stepmania/src/SongManager.cpp
@@ -931,7 +931,7 @@ void SongManager::GetExtraStageInfo( bool bExtra2, const Style *sd,
 	}
 
 	ASSERT_M( sGroup != "", ssprintf("%p '%s' '%s'",
-		GAMESTATE->m_pCurSong,
+		GAMESTATE->m_pCurSong.Get(),
 		GAMESTATE->m_pCurSong? GAMESTATE->m_pCurSong->GetSongDir().c_str():"",
 		GAMESTATE->m_pCurSong? GAMESTATE->m_pCurSong->m_sGroupName.c_str():"") );
 
