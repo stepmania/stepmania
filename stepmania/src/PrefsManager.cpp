19a75abbb2908929df1ec533a6dab7a2f3c796b5
Chris Danford <chrisdanford@users.sourceforge.net>
2005-04-03 08:12:45 +0000

    cleanup

--- stepmania/src/PrefsManager.cpp
+++ stepmania/src/PrefsManager.cpp
@@ -201,7 +201,7 @@ void PrefsManager::Init()
 	m_bDancePointsForOni = false;
 	m_bPercentageScoring = false;
 	m_fMinPercentageForMachineSongHighScore = 0.5f;
-	m_fMinPercentageForMachineCourseHighScore = 0.001f;	// don't save course scores with 0 percentage
+	m_fMinPercentageForMachineCourseHighScore = 0.0001f;	// don't save course scores with 0 percentage
 	m_bDisqualification = false;
 	m_bShowLyrics = true;
 	m_bAutogenSteps = true;
