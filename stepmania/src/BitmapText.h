cc9842e5a42e77b4a22a21291ae4951dcb04ea26
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-05-04 21:36:22 +0000

    gettext -> GetText to work around linux namespace pollution.  (Lua methods that are useful in commands need to be lowercase, since we lowercase on parsing, but others don't have to.)

--- stepmania/src/BitmapText.h
+++ stepmania/src/BitmapText.h
@@ -19,7 +19,7 @@ public:
 	static int maxwidth( T* p, lua_State *L )			{ p->SetMaxWidth( FArg(1) ); return 0; }
 	static int maxheight( T* p, lua_State *L )			{ p->SetMaxHeight( FArg(1) ); return 0; }
 	static int settext( T* p, lua_State *L )			{ p->SetText( SArg(1) ); return 0; }
-	static int gettext( T* p, lua_State *L )			{ lua_pushstring( L, p->GetText() ); return 1; }
+	static int GetText( T* p, lua_State *L )			{ lua_pushstring( L, p->GetText() ); return 1; }
 
 	static void Register(lua_State *L) 
 	{
@@ -27,7 +27,7 @@ public:
 		ADD_METHOD( maxwidth )
 		ADD_METHOD( maxheight )
 		ADD_METHOD( settext )
-		ADD_METHOD( gettext )
+		ADD_METHOD( GetText )
 		LunaActor<T>::Register( L );
 	}
 };
