a73938547b5e6d0191b1bc7066cf145f49bd1487
Thad Ward <coderjoe@users.sourceforge.net>
2005-05-12 20:14:03 +0000

    bump for release.

--- stepmania/src/ProductInfo.h
+++ stepmania/src/ProductInfo.h
@@ -2,7 +2,7 @@
 
 #define PRODUCT_NAME "StepMania"
 #define PRODUCT_VER "3.9 random build"
-//#define PRODUCT_VER "3.9 rc2a"
+//#define PRODUCT_VER "3.9 rc3"
 #define PRODUCT_NAME_VER PRODUCT_NAME " " PRODUCT_VER
 
 /*
