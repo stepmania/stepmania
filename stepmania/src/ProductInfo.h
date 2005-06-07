e46350693c318538448c801aea1bb25deb0a0197
Ben Anderson <bashmaster@users.sourceforge.net>
2005-06-07 13:31:39 +0000

    Going into version 4.0. Let's not do CVS snapshot releases this time and skip straight to alpha 1 when we start releasing from trunk again. And \'random build\' just plain sounds unprofessional.

--- stepmania/src/ProductInfo.h
+++ stepmania/src/ProductInfo.h
@@ -6,8 +6,8 @@
 // Don't forget to also change ProductInfo.inc!
 
 #define PRODUCT_NAME "StepMania"
-#define PRODUCT_VER "3.9 random build"
-//#define PRODUCT_VER "3.9 alpha 23"
+#define PRODUCT_VER "4.0 CVS"
+//#define PRODUCT_VER "4.0 alpha 1"
 #define PRODUCT_NAME_VER PRODUCT_NAME " " PRODUCT_VER
 
 #endif
