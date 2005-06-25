ae70c1581845c56a17265b4c5d45dc3e0d5bd31d
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-06-25 02:03:37 +0000

    remove title menu special case (just one of several)

--- stepmania/src/ScreenTitleMenu.cpp
+++ stepmania/src/ScreenTitleMenu.cpp
@@ -31,15 +31,6 @@ ScreenTitleMenu::ScreenTitleMenu( CString sScreenName ) :
 {
 	LOG->Trace( "ScreenTitleMenu::ScreenTitleMenu()" );
 
-	// Don't show screen title menu (says "Press Start") 
-	// if there are 0 credits and inserted and CoinMode is pay.
-	if( GAMESTATE->GetCoinMode() == COIN_MODE_PAY  &&
-		GAMESTATE->m_iCoins < PREFSMAN->m_iCoinsPerCredit )
-	{
-		SCREENMAN->SetNewScreen( INITIAL_SCREEN );
-		return;
-	}
-
 	/* XXX We really need two common calls: 1, something run when exiting from gameplay
 	 * (to do this reset), and 2, something run when entering gameplay, to apply default
 	 * options.  Having special cases in attract screens and the title menu to reset
