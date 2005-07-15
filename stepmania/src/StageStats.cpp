7afc69b2fdfc8fd8d68deee9e05ab352cecb9e80
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-07-15 04:48:36 +0000

    OnePassed binding

--- stepmania/src/StageStats.cpp
+++ stepmania/src/StageStats.cpp
@@ -112,12 +112,14 @@ public:
 
 	static int GetPlayerStageStats( T* p, lua_State *L )	{ p->m_player[IArg(1)].PushSelf(L); return 1; }
 	static int GetGameplaySeconds( T* p, lua_State *L )		{ lua_pushnumber(L, p->fGameplaySeconds); return 1; }
+	static int OnePassed( T* p, lua_State *L )				{ lua_pushboolean(L, p->OnePassed()); return 1; }
 	static int AllFailed( T* p, lua_State *L )				{ lua_pushboolean(L, p->AllFailed()); return 1; }
 
 	static void Register(lua_State *L)
 	{
 		ADD_METHOD( GetPlayerStageStats )
 		ADD_METHOD( GetGameplaySeconds )
+		ADD_METHOD( OnePassed )
 		ADD_METHOD( AllFailed )
 		Luna<T>::Register( L );
 	}
