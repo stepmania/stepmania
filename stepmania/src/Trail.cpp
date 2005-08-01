dbaec61958e2ed7564079145515a5f6d03cb3f64
Chris Danford <chrisdanford@users.sourceforge.net>
2005-08-01 05:11:30 +0000

    lua method rename: GetDifficulty -> GetCourseDifficulty

--- stepmania/src/Trail.cpp
+++ stepmania/src/Trail.cpp
@@ -191,11 +191,14 @@ class LunaTrail: public Luna<Trail>
 public:
 	LunaTrail() { LUA->Register( Register ); }
 
-	static int GetDifficulty( T* p, lua_State *L )	{ lua_pushnumber(L, p->m_CourseDifficulty ); return 1; }
+	static int GetCourseDifficulty( T* p, lua_State *L )	{ lua_pushnumber(L, p->m_CourseDifficulty ); return 1; }
+	static int GetStepsType( T* p, lua_State *L )	{ lua_pushnumber(L, p->m_StepsType ); return 1; }
 
 	static void Register(lua_State *L)
 	{
-		ADD_METHOD( GetDifficulty )
+		ADD_METHOD( GetCourseDifficulty )
+		ADD_METHOD( GetStepsType )
+
 		Luna<T>::Register( L );
 	}
 };
