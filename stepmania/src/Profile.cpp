8e9aff2d21bccc5fc62640aef7ab2c2517e3e673
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-08-05 16:21:00 +0000

    compile fix

--- stepmania/src/Profile.cpp
+++ stepmania/src/Profile.cpp
@@ -1810,7 +1810,7 @@ StepsType Profile::GetLastPlayedStepsType() const
 {
 	if( m_vRecentStepsScores.empty() )
 		return STEPS_TYPE_INVALID;
-	return m_vRecentStepsScores.back().stepsID.GetStepsType();
+	return m_vRecentStepsScores.back().stepsID->GetStepsType();
 }
 
 const Profile::HighScoresForASong *Profile::GetHighScoresForASong( const SongID& songID ) const
