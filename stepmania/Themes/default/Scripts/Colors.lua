05a25c0b687152e612204924c8a545e9376c7896
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-08-31 04:02:41 +0000

    PlayerColor returns rgba

--- stepmania/Themes/default/Scripts/Colors.lua
+++ stepmania/Themes/default/Scripts/Colors.lua
@@ -1,6 +1,6 @@
 function PlayerColor( pn )
-	if pn == PLAYER_1 then return "0.4,1.0,0.8,1" end	-- sea green
-	if pn == PLAYER_2 then return "1.0,0.5,0.2,1" end	-- orange
+	if pn == PLAYER_1 then return color("0.4,1.0,0.8,1") end	-- sea green
+	if pn == PLAYER_2 then return color("1.0,0.5,0.2,1") end	-- orange
 	return "1,1,1,1"
 end
 
