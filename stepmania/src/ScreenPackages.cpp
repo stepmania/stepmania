eafaf7fbb44c8fc4480dcd1b4507cb75c1af4c7b
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-09-03 18:19:25 +0000

    cleanup (avoid multiple *)

--- stepmania/src/ScreenPackages.cpp
+++ stepmania/src/ScreenPackages.cpp
@@ -281,7 +281,8 @@ void ScreenPackages::TweenOffScreen( )
 
 void ScreenPackages::RefreshPackages()
 {
-	GetDirListing( "Packages/*.*zip", m_Packages, false, false );
+	GetDirListing( "Packages/*.zip", m_Packages, false, false );
+	GetDirListing( "Packages/*.smzip", m_Packages, false, false );
 	
 	if ( m_iPackagesPos < 0 )
 		m_iPackagesPos = 0;
