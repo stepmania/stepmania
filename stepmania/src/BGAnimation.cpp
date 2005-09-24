3a185cc977377e1aa62834fae10de106b7a447d6
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-09-24 23:43:06 +0000

    yuck.  ActorScroller doesn't need UseScroller, but BGAnimation does.  Move the hack into BGAnimation, which is on its way out anyway.

--- stepmania/src/BGAnimation.cpp
+++ stepmania/src/BGAnimation.cpp
@@ -149,8 +149,12 @@ void BGAnimation::LoadFromAniDir( const CString &_sAniDir )
 
 void BGAnimation::LoadFromNode( const CString& sDir, const XNode* pNode )
 {
-	ActorScrollerAutoDeleteChildren::LoadFromNode( sDir, pNode );
-
+	bool bUseScroller = false;
+	pNode->GetAttrValue( "UseScroller", bUseScroller );
+	if( bUseScroller )
+		ActorScrollerAutoDeleteChildren::LoadFromNode( sDir, pNode );
+	else
+		ActorFrame::LoadFromNode( sDir, pNode );
 
 	/* Backwards-compatibility: if a "LengthSeconds" value is present, create a dummy
 	 * actor that sleeps for the given length of time.  This will extend GetTweenTimeLeft. */
