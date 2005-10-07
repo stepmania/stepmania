591d9e4aca602d8da04688d9c2794cd3618b725e
Chris Danford <chrisdanford@users.sourceforge.net>
2005-10-07 07:12:35 +0000

    fix error

--- stepmania/src/RageSoundManager.cpp
+++ stepmania/src/RageSoundManager.cpp
@@ -331,7 +331,7 @@ void RageSoundManager::PlayOnce( CString sPath )
 
 void RageSoundManager::SetMixVolume( float fMixVol )
 {
-	ASSERT_M( fMixVol >= 0 && fVolume <= 1, ssprintf("%f",fMixVol) );
+	ASSERT_M( fMixVol >= 0 && fMixVol <= 1, ssprintf("%f",fMixVol) );
 
 	g_SoundManMutex.Lock(); /* lock for access to m_fMixVolume */
 	m_fMixVolume = fMixVol;
