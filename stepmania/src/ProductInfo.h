44228131a23f2b2b1a20f1752e82928b447bdb1e
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-10-27 23:42:54 +0000

    3.9 rc4 -> 3.9

--- stepmania/src/ProductInfo.h
+++ stepmania/src/ProductInfo.h
@@ -1,8 +1,8 @@
 // Don't forget to also change ProductInfo.inc!
 
 #define PRODUCT_NAME "StepMania"
-#define PRODUCT_VER "3.9 random build"
-//#define PRODUCT_VER "3.9 rc4"
+//#define PRODUCT_VER "3.9 random build"
+#define PRODUCT_VER "3.9"
 #define PRODUCT_NAME_VER PRODUCT_NAME " " PRODUCT_VER
 
 /*
