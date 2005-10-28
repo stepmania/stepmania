c4195979f367c0e7c0a6224a294c761fcd7849a1
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2005-10-28 08:07:53 +0000

    Clean up a bit, remove useless code. Some work needs to be done to fix up the menu bar situation.

--- stepmania/PBProject/SDLMain.m
+++ stepmania/PBProject/SDLMain.m
@@ -5,22 +5,21 @@
     Feel free to customize this file to suit your needs
 */
 
+#import <Cocoa/Cocoa.h>
 #import <SDL.h>
-#import "SDLMain.h"
 #import <sys/param.h> /* for MAXPATHLEN */
 #import <unistd.h>
 
 static int    gArgc;
 static char  **gArgv;
-static BOOL   gFinderLaunch;
-
-/* An internal Apple class used to setup Apple menus */
-@interface NSAppleMenuController : NSObject {}
-- (void)controlMenu:(NSMenu *)aMenu;
-@end
 
 @interface SDLApplication : NSApplication
 @end
+
+@interface SDLMain : NSObject {}
+- (void) startGame:(id)obj;
+@end
+
 
 @implementation SDLApplication
 /* Invoked from the Quit menu item */
@@ -47,102 +46,64 @@ static BOOL   gFinderLaunch;
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	/* Hand off to main application code */
-    exit(SDL_main (gArgc, gArgv));
+    exit( SDL_main (gArgc, gArgv) );
 	[pool release]; // not really needed, but shuts gcc up.
 }
 
-/* Set the working directory to the .app's parent directory */
-- (void) setupWorkingDirectory:(BOOL)shouldChdir
+/* Called when the internal event loop has just started running */
+- (void) applicationDidFinishLaunching: (NSNotification *) note
 {
-
-    if (shouldChdir)
-    {
-        char parentdir[MAXPATHLEN];
-        char *c;
-
-        strncpy ( parentdir, gArgv[0], sizeof(parentdir) );
-        c = (char*) parentdir;
-
-        while (*c != '\0')     /* go to end */
-               c++;
-
-        while (*c != '/')      /* back up to parent */
-               c--;
-
-        *c++ = '\0';           /* cut off last part (binary name) */
-
-        assert ( chdir (parentdir) == 0 );   /* chdir to the binary app's parent */
-        assert ( chdir ("../../../") == 0 ); /* chdir to the .app's parent */
-    }
+#if 1
+	[NSThread detachNewThreadSelector:@selector(startGame:) toTarget:self withObject:nil];
+#else
+	[self startGame:nil];
+#endif
 }
+@end
 
 
-void setupAppleMenu(void)
-{
-    /* warning: this code is very odd */
-    NSAppleMenuController *appleMenuController;
-    NSMenu *appleMenu;
-    NSMenuItem *appleMenuItem;
-
-    appleMenuController = [[NSAppleMenuController alloc] init];
-    appleMenu = [[NSMenu alloc] initWithTitle:@""];
-    appleMenuItem = [[NSMenuItem alloc] initWithTitle:@"" action:nil keyEquivalent:@""];
-    
-    [appleMenuItem setSubmenu:appleMenu];
-
-    /* yes, we do need to add it and then remove it --
-       if you don't add it, it doesn't get displayed
-       if you don't remove it, you have an extra, titleless item in the menubar
-       when you remove it, it appears to stick around
-       very, very odd */
-    [[NSApp mainMenu] addItem:appleMenuItem];
-    [appleMenuController controlMenu:appleMenu];
-    [[NSApp mainMenu] removeItem:appleMenuItem];
-    [appleMenu release];
-    [appleMenuItem release];
-}
-
-/* Create a window menu */
-void setupWindowMenu(void)
+static void setupMenus( void )
 {
     NSMenu		*windowMenu;
     NSMenuItem	*windowMenuItem;
     NSMenuItem	*menuItem;
-
-
+	NSMenu *mainMenu = [NSApp mainMenu];
+	
+	printf("%d\n", [mainMenu numberOfItems]);
+	
     windowMenu = [[NSMenu alloc] initWithTitle:@"Window"];
     
     /* "Minimize" item */
-    menuItem = [[NSMenuItem alloc] initWithTitle:@"Minimize" action:@selector(performMiniaturize:) keyEquivalent:@"m"];
+    menuItem = [[NSMenuItem alloc] initWithTitle:@"Minimize" action:@selector(performMiniaturize:)
+								   keyEquivalent:@"m"];
     [windowMenu addItem:menuItem];
     [menuItem release];
     
     /* Put menu into the menubar */
     windowMenuItem = [[NSMenuItem alloc] initWithTitle:@"Window" action:nil keyEquivalent:@""];
     [windowMenuItem setSubmenu:windowMenu];
-    [[NSApp mainMenu] addItem:windowMenuItem];
+    [mainMenu addItem:windowMenuItem];
     
     /* Tell the application object that this is now the window menu */
     [NSApp setWindowsMenu:windowMenu];
-
+	
     /* Finally give up our references to the objects */
     [windowMenu release];
     [windowMenuItem release];
 }
 
 /* Replacement for NSApplicationMain */
-void CustomApplicationMain (int argc, char **argv)
+static void CustomApplicationMain( int argc, char **argv )
 {
     NSAutoreleasePool	*pool = [[NSAutoreleasePool alloc] init];
     SDLMain				*sdlMain;
-
+	
     /* Ensure the application object is initialised */
     [SDLApplication sharedApplication];
-    
+	
+	//[NSApp setMainMenu:[[NSMenu alloc] initWithTitle:@""]];
     /* Set up the menubar */
-    [NSApp setMainMenu:[[NSMenu alloc] init]];
-    setupAppleMenu();
-    setupWindowMenu();
+    setupMenus();
     
     /* Create SDLMain and make it the app delegate */
     sdlMain = [[SDLMain alloc] init];
@@ -155,70 +116,13 @@ void CustomApplicationMain (int argc, char **argv)
     [pool release];
 }
 
-
-/* Called when the internal event loop has just started running */
-- (void) applicationDidFinishLaunching: (NSNotification *) note
-{
-    /* Set the working directory to the .app's parent directory */
-    [self setupWorkingDirectory:gFinderLaunch];
-
-#if 1
-	[NSThread detachNewThreadSelector:@selector(startGame:) toTarget:self withObject:nil];
-#else
-	[self startGame:nil];
-#endif
-}
-@end
-
-
-@implementation NSString (ReplaceSubString)
-
-- (NSString *)stringByReplacingRange:(NSRange)aRange with:(NSString *)aString
-{
-    unsigned int bufferSize;
-    unsigned int selfLen = [self length];
-    unsigned int aStringLen = [aString length];
-    unichar *buffer;
-    NSRange localRange;
-    NSString *result;
-
-    bufferSize = selfLen + aStringLen - aRange.length;
-    buffer = (unichar *)NSAllocateMemoryPages(bufferSize*sizeof(unichar));
-    
-    /* Get first part into buffer */
-    localRange.location = 0;
-    localRange.length = aRange.location;
-    [self getCharacters:buffer range:localRange];
-    
-    /* Get middle part into buffer */
-    localRange.location = 0;
-    localRange.length = aStringLen;
-    [aString getCharacters:(buffer+aRange.location) range:localRange];
-     
-    /* Get last part into buffer */
-    localRange.location = aRange.location + aRange.length;
-    localRange.length = selfLen - localRange.location;
-    [self getCharacters:(buffer+aRange.location+aStringLen) range:localRange];
-    
-    /* Build output string */
-    result = [NSString stringWithCharacters:buffer length:bufferSize];
-    
-    NSDeallocateMemoryPages(buffer, bufferSize);
-    
-    return result;
-}
-
-@end
-
-
-
 #ifdef main
 #  undef main
 #endif
 
 
 /* Main entry point to executable - should *not* be SDL_main! */
-int main (int argc, char **argv)
+int main( int argc, char **argv )
 {
 
     /* Copy the arguments into a global variable */
@@ -226,15 +130,9 @@ int main (int argc, char **argv)
     
     /* This is passed if we are launched by double-clicking */
     if ( argc >= 2 && strncmp (argv[1], "-psn", 4) == 0 )
-	{
         gArgc = 1;
-		gFinderLaunch = YES;
-    }
 	else
-	{
         gArgc = argc;
-		gFinderLaunch = NO;
-    }
     gArgv = (char**) malloc (sizeof(*gArgv) * (gArgc+1));
     assert (gArgv != NULL);
     for (i = 0; i < gArgc; i++)
@@ -242,6 +140,6 @@ int main (int argc, char **argv)
     gArgv[i] = NULL;
 
     [SDLApplication poseAsClass:[NSApplication class]];
-    CustomApplicationMain (argc, argv);
+    CustomApplicationMain( argc, argv );
     return 0;
 }
