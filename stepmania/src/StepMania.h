8142decec5ebc2cf008f0797578020e9e90b6254
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-11-23 02:21:41 +0000

    fix search/replace

--- stepmania/src/StepMania.h
+++ stepmania/src/StepMania.h
@@ -16,7 +16,7 @@ void ChangeCurrentGame( const Game* g );
 void FocusChanged( bool bHasFocus );
 bool AppHasFocus();
 
-// If successful, return filename of screenshot in sDir, else return CString()
+// If successful, return filename of screenshot in sDir, else return ""
 CString SaveScreenshot( CString sDir, bool bSaveCompressed, bool bMakeSignature, int iIndex = -1 );
 
 void InsertCoin( int iNum = 1, const RageTimer *pTime = NULL );
