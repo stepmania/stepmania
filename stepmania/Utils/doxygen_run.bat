6131eb41152760c0786cab393c3d3fb4bc8d304a
Chris Danford <chrisdanford@users.sourceforge.net>
2005-12-01 05:37:19 +0000

    no message

--- stepmania/Utils/doxygen_run.bat
+++ stepmania/Utils/doxygen_run.bat
@@ -1,10 +1,8 @@
-SET PATH=%PATH%;c:\stepmania\stepmania\utils
-
 del /q ..\stepmania.chm
 del /s /q temp
 rmdir /s /q temp
 
-doxygen.exe doxygen_config
+doxygen\doxygen.exe doxygen_config
 
 rem forfiles -ptemp -s -m*.png -c"pngcrushinplace.bat 0x22@FILE0x22"
 
