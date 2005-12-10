05072579ae4303a73794e513ba3b9bd7a62db965
Glenn Maynard <gmaynard@users.sourceforge.net>
2005-12-10 18:46:09 +0000

    fix compile

--- stepmania/src/tests/test_misc.cpp
+++ stepmania/src/tests/test_misc.cpp
@@ -53,7 +53,7 @@ void test_init()
 	HOOKS = MakeArchHooks();
 
 	FILEMAN = new RageFileManager( argv0 );
-	FILEMAN->Mount( "dir", InitialWorkingDirectory, "/" );
+	FILEMAN->Mount( "dir", RageFileManagerUtil::sInitialWorkingDirectory, "/" );
 	FILEMAN->Mount( g_Driver, g_Root, "/" );
 
 	LOG = new RageLog();
