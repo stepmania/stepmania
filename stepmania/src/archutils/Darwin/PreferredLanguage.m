e21934cbdf55f562f41962b82bf1f4f01742a1ab
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2006-01-15 00:00:49 +0000

    Cleanup.

--- stepmania/src/archutils/Darwin/PreferredLanguage.m
+++ stepmania/src/archutils/Darwin/PreferredLanguage.m
@@ -1,13 +1,13 @@
 char *GetPreferredLanguage()
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    NSUserDefaults *def = [NSUserDefaults standardUserDefaults];
-    NSArray *languages = [def objectForKey:@"AppleLanguages"];
+	NSUserDefaults *def = [NSUserDefaults standardUserDefaults];
+	NSArray *languages = [def objectForKey:@"AppleLanguages"];
 	const char *lang = [[languages objectAtIndex:0] UTF8String];
 	char *ret = (char *)malloc( strlen(lang) + 1 );
-
+	
 	strcpy( ret, lang );
-    [pool release];
+	[pool release];
 	return ret;
 }
 
