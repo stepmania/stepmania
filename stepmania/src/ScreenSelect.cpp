6ec1d47d6c3f32c8585db7ab8f8139ceef6d3cce
Chris Danford <chrisdanford@users.sourceforge.net>
2006-01-29 00:25:33 +0000

    tab cleanup

--- stepmania/src/ScreenSelect.cpp
+++ stepmania/src/ScreenSelect.cpp
@@ -11,13 +11,13 @@
 #include "Command.h"
 #include "InputEventPlus.h"
 
-#define CHOICE_NAMES			THEME->GetMetric (m_sName,"ChoiceNames")
-#define CHOICE( s )				THEME->GetMetricM(m_sName,ssprintf("Choice%s",s.c_str()))
-#define CODE_NAMES				THEME->GetMetric (m_sName,"CodeNames")
-#define CODE( s )				THEME->GetMetric (m_sName,ssprintf("Code%s",s.c_str()))
-#define CODE_ACTION( s )		THEME->GetMetricM(m_sName,ssprintf("Code%sAction",s.c_str()))
-#define IDLE_TIMEOUT_SCREEN		THEME->GetMetric (m_sName,"IdleTimeoutScreen")
-#define UPDATE_ON_MESSAGE		THEME->GetMetric (m_sName,"UpdateOnMessage")
+#define CHOICE_NAMES		THEME->GetMetric (m_sName,"ChoiceNames")
+#define CHOICE( s )		THEME->GetMetricM(m_sName,ssprintf("Choice%s",s.c_str()))
+#define CODE_NAMES		THEME->GetMetric (m_sName,"CodeNames")
+#define CODE( s )		THEME->GetMetric (m_sName,ssprintf("Code%s",s.c_str()))
+#define CODE_ACTION( s )	THEME->GetMetricM(m_sName,ssprintf("Code%sAction",s.c_str()))
+#define IDLE_TIMEOUT_SCREEN	THEME->GetMetric (m_sName,"IdleTimeoutScreen")
+#define UPDATE_ON_MESSAGE	THEME->GetMetric (m_sName,"UpdateOnMessage")
 
 void ScreenSelect::Init()
 {
