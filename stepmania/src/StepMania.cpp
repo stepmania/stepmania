171dc2684509a59c9b9a858642071b1604de8f6a
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-02-24 03:15:25 +0000

    Fix RageException::Throw during concurrent loading crashes.

--- stepmania/src/StepMania.cpp
+++ stepmania/src/StepMania.cpp
@@ -277,6 +277,12 @@ void ShutdownGame()
 
 static void HandleException( const RString &sError )
 {
+	if( SCREENMAN && SCREENMAN->IsConcurrentlyLoading() )
+	{
+		LOG->Trace( "HandleException: FinishConcurrentRendering" );
+		GameLoop::FinishConcurrentRendering();
+	}
+
 	if( g_bAutoRestart )
 		HOOKS->RestartProgram();
 
