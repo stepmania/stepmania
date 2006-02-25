1514f2f5d4d87b6f06921e52c401293a84dcc8cc
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-02-25 06:07:40 +0000

    remove old checkpoints

--- stepmania/src/Player.cpp
+++ stepmania/src/Player.cpp
@@ -546,9 +546,7 @@ void Player::Update( float fDeltaTime )
 		// TODO: Remove use of PlayerNumber.
 		PlayerNumber pn = m_pPlayerState->m_PlayerNumber;
 
-		CHECKPOINT_M( ssprintf("P%i, %i %i", pn, col, iNumCols) );
 		const StyleInput StyleI( pn, col );
-		CHECKPOINT_M( ssprintf("P%i %i", StyleI.player, StyleI.col) );
 		bool bIsHoldingButton = INPUTMAPPER->IsButtonDown( StyleI );
 		// TODO: Make this work for non-human-controlled players
 		if( bIsHoldingButton && !GAMESTATE->m_bDemonstrationOrJukebox && m_pPlayerState->m_PlayerController==PC_HUMAN )
