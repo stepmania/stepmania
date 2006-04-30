1501fbfbf51d64e186276cda5dad0eef223afe66
Chris Danford <chrisdanford@users.sourceforge.net>
2006-04-30 07:34:47 +0000

    bump cache version

--- stepmania/src/Song.cpp
+++ stepmania/src/Song.cpp
@@ -37,7 +37,7 @@
 #include <set>
 #include <float.h>
 
-const int FILE_CACHE_VERSION = 144;	// increment this to invalidate cache
+const int FILE_CACHE_VERSION = 145;	// increment this to invalidate cache
 
 const float DEFAULT_MUSIC_SAMPLE_LENGTH = 12.f;
 
