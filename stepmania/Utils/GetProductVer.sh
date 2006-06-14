c6a49f62aa84e4221924b3450bfd861bde4c4407
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2006-06-14 00:34:18 +0000

    Fix script.

--- stepmania/Utils/GetProductVer.sh
+++ stepmania/Utils/GetProductVer.sh
@@ -11,8 +11,8 @@ if ! test -e $1; then
 	exit 1
 fi
 
-PRODUCT=`cat $1 | grep -w '^.define PRODUCT_NAME' | sed -e 's/.*"\(.*\)"/\1/'`
-VER=`cat $1 | grep -w '^.define PRODUCT_VER' | sed -e 's/.*"\(.*\)"/\1/'`
+PRODUCT=`cat $1 | grep -w '^.define PRODUCT_FAMILY_BARE' | sed -re 's/.*_BARE +(.*)/\1/'`
+VER=`cat $1 | grep -w '^.define PRODUCT_VER_BARE' | sed -re 's/.*_BARE +(.*)/\1/'`
 
 # "3.9 alpha 1" -> "3.9 alpha1"
 VER=`echo "$VER" | sed -e 's/alpha /alpha/'`
