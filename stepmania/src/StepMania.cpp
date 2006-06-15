38a7ff109eda6ba88eb42eace1f7485de17e195a
Ben Anderson <bashmaster@users.sourceforge.net>
2006-06-15 02:21:47 +0000

    NSIS is only used on Windows.

--- stepmania/src/StepMania.cpp
+++ stepmania/src/StepMania.cpp
@@ -1115,10 +1115,15 @@ int main(int argc, char* argv[])
 		NSMAN->DisplayStartupStatus();	// If we're using networking show what happened
 
 	/* Run the main loop. */
+#if defined(WIN32)
 	if( GetCommandlineArgument("ExportNsisStrings") )
+		// XXX Nullsoft Scriptable Installation System?
 		ExportNsisStrings::Do();
 	else
 		GameLoop::RunGameLoop();
+#else
+	GameLoop::RunGameLoop();
+#endif
 
 	/* If we ended mid-game, finish up. */
 	GAMESTATE->EndGame();
