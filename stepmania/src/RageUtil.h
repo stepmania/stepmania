f0ba8c3322070129a7af4f413ac8b6ddd54141a9
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-07-25 04:51:17 +0000

    "ARRAY_SIZE" is also stomped on by Windows headers.
    Add "ARRAYLEN"; remove ARRAYSIZE after transition.

--- stepmania/src/RageUtil.h
+++ stepmania/src/RageUtil.h
@@ -14,7 +14,8 @@ class RageFileDriver;
 #define ZERO(x)	memset(&(x), 0, sizeof(x))
 #define COPY(a,b) do { ASSERT(sizeof(a)==sizeof(b)); memcpy(&(a), &(b), sizeof(a)); } while( false )
 
-#define ARRAYSIZE(a) (sizeof(a) / sizeof((a)[0]))
+#define ARRAYLEN(a) (sizeof(a) / sizeof((a)[0]))
+#define ARRAYSIZE(a) (sizeof(a) / sizeof((a)[0])) // deprecated
 
 /* Common harmless mismatches.  All min(T,T) and max(T,T) cases are handled
  * by the generic template we get from <algorithm>. */
