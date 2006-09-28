082290e7464962089d57265b3b27b3ad478c889f
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2006-09-28 06:27:57 +0000

    Unused.

--- stepmania/src/Grade.cpp
+++ stepmania/src/Grade.cpp
@@ -7,29 +7,6 @@
 #include "LuaManager.h"
 #include "LuaFunctions.h"
 
-static const char *GradeNames[] = {
-	"Tier01",
-	"Tier02",
-	"Tier03",
-	"Tier04",
-	"Tier05",
-	"Tier06",
-	"Tier07",
-	"Tier08",
-	"Tier09",
-	"Tier10",
-	"Tier11",
-	"Tier12",
-	"Tier13",
-	"Tier14",
-	"Tier15",
-	"Tier16",
-	"Tier17",
-	"Tier18",
-	"Tier19",
-	"Tier20",
-	"Failed",
-};
 LuaXType( Grade );
 
 RString GradeToLocalizedString( Grade g )
