4d2feecc35333c3df2e4e28615041dd916e806fd
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-10-02 22:42:24 +0000

    fix compile

--- stepmania/src/XmlFile.h
+++ stepmania/src/XmlFile.h
@@ -44,7 +44,7 @@ public:
 	XNodes	m_childs;	// child node
 	XAttrs	m_attrs;	// attributes
 
-	void SetName( const RString &sName ) { m_Value.m_sName = sName; }
+	void SetName( const RString &sName ) { m_sName = sName; }
 	RString GetName() const { return m_sName; }
 	RString GetValue() const { return m_sValue; }
 
