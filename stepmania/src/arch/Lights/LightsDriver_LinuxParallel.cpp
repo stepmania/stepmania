a9cae2253da7e891e3b062a3fdf212a2b9b04f24
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-10-02 23:11:17 +0000

    fix compile

--- stepmania/src/arch/Lights/LightsDriver_LinuxParallel.cpp
+++ stepmania/src/arch/Lights/LightsDriver_LinuxParallel.cpp
@@ -51,7 +51,9 @@ void LightsDriver_LinuxParallel::Set( const LightsState *ls )
 	}
 	s += "\n";
 
-	int iNumGameButtonsToShow = INPUTMAPPER->GetInputScheme()->GetNumGameplayButtons();
+	int iNumGameButtonsToShow = INPUTMAPPER->GetInputScheme()->ButtonNameToIndex( "Start" );
+	if( iNumGameButtonsToShow == GameButton_Invalid )
+		iNumGameButtonsToShow = INPUTMAPPER->GetInputScheme()->m_iButtonsPerController;
 	FOREACH_GameController( gc )
 	{
 		s += ssprintf("Controller%d Bits: ",gc+1);
