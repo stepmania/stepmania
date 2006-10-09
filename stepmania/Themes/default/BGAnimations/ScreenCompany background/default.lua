0c74193c2c1d4b4c2dc4ec6b754e89f1cacf77b1
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-10-09 07:32:39 +0000

    use Def

--- stepmania/Themes/default/BGAnimations/ScreenCompany background/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenCompany background/default.lua
@@ -1,22 +1,19 @@
 local t =
-{
-	Type = "ActorFrame",
-	children =
-	{
-		{
+Def.ActorFrame {
+	children = Def.Actor {
+		Def.Layer {
 			File="white.png",
 			OnCommand="x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;stretchto,SCREEN_LEFT,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM"
 		},
-		{
+		Def.Layer {
 			File="shadow (32bpp).png",
 			OnCommand="x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;diffuse,0,0,0,1;addx,+20;addy,6;linear,8;addx,-40"
 		},
-		{
+		Def.Layer {
 			File="stepmania (dither).png",
 			OnCommand="x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y"
 		}
 	}
-		
 }
 
 return t
