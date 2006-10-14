596eb4d881f7fb5e7e5529b58b801c8701621d9d
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-10-14 23:33:25 +0000

    add LUA_REGISTER_NAMESPACE, for simple non-singleton bindings

--- stepmania/src/LuaBinding.h
+++ stepmania/src/LuaBinding.h
@@ -163,6 +163,11 @@ public:
 #define ADD_METHOD( method_name ) \
 	AddMethod( #method_name, method_name )
 
+#define LUA_REGISTER_NAMESPACE( T ) \
+	void Register##T( lua_State *L ) { luaL_register( L, #T, T##Table ); lua_pop( L, 1 ); } \
+	REGISTER_WITH_LUA_FUNCTION( Register##T );
+#define LIST_METHOD( method_name ) \
+	{ #method_name, method_name }
 
 #endif
 
