116db99a92bf9c8051e7cf532b3cb84f9fc1ca02
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-10-15 05:51:23 +0000

    The "Def.Type { }" syntax is for defining actors by type, like ActorUtil::Create.  Most of the time, use LoadActor, which is for loading files by filename, like ActorUtil::MakeActor.
    (If you're creating "a Model with these parameters", use Def.Model.
    If you're just loading "this other filename, whatever it is",
    use LoadActor.)

--- stepmania/Themes/default/BGAnimations/ScreenCompany background/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenCompany background/default.lua
@@ -1,15 +1,13 @@
 local t =
 Def.ActorFrame {
-	children = Def.Actor {
-		Def.Layer {
-			File="white.png",
+	children = {
+		LoadActor("white.png")() .. {
 			OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;stretchto,SCREEN_LEFT,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM)
 		},
-		Def.Layer {
-			File="shadow (32bpp).png",
+		LoadActor("shadow (32bpp).png")() .. {
 			OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;diffuse,0,0,0,1;addx,20;addy,6;linear,8;addx,-40)
 		},
-		Def.Layer {
+		LoadActor("stepmania (dither).png")() .. {
 			File="stepmania (dither).png",
 			OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y),
 		}
