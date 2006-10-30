796aa48ba2cffd18fad07c6ac76687f5c58fac24
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2006-10-30 09:23:58 +0000

    Fix life meter.

--- stepmania/Themes/default/BGAnimations/ScreenEvaluationStage overlay/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenEvaluationStage overlay/default.lua
@@ -1,10 +1,18 @@
+local p1x = THEME:GetMetric( 'ScreenEvaluationStage', 'BonusFrameP1X' )
+local p1y = THEME:GetMetric( 'ScreenEvaluationStage', 'BonusFrameP1Y' )
+local p2x = THEME:GetMetric( 'ScreenEvaluationStage', 'BonusFrameP2X' )
+local p2y = THEME:GetMetric( 'ScreenEvaluationStage', 'BonusFrameP2Y' )
+p1y = p1y + 75 -- It starts 75 pixels down from the top of the Bonus frame.
+p2y = p2y + 75
+
+Trace( "(x,y) = (" .. p1x .. "," .. p1y .. ")" )
 return Def.ActorFrame {
 	children = {
 		LoadActor("life graph")(PLAYER_1) .. {
-			InitCommand = cmd(x,SCREEN_LEFT+96;y,SCREEN_CENTER_Y+91),
+			InitCommand = cmd(x,p1x;y,p1y),
 		},
 		LoadActor("life graph")(PLAYER_2) .. {
-			InitCommand = cmd(x,SCREEN_LEFT-96;y,SCREEN_CENTER_Y+91),
+			InitCommand = cmd(x,p2x;y,p2y),
 		},
 	}
 }
