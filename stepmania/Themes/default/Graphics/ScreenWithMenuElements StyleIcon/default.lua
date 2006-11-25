a49aa8d636c1faf96b9696ad11d75696f422d274
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-11-25 11:02:16 +0000

    fix code (this is still wrong; it's using the StyleType and should be using the style name)

--- stepmania/Themes/default/Graphics/ScreenWithMenuElements StyleIcon/default.lua
+++ stepmania/Themes/default/Graphics/ScreenWithMenuElements StyleIcon/default.lua
@@ -1,8 +1,9 @@
 local style = GAMESTATE:GetCurrentStyle();
 if not style then return Def.Actor {}; end
 
-local s = StyleTypeToString( style:GetStyleType() );
+local s = style:GetStyleType();
 local Reverse = PlayerNumber:Reverse();
+s = string.gsub(s, "StyleType_", "");
 
 local t = LoadActor("_icon " .. s)() ..  {
 	InitCommand = cmd(pause;setstate,Reverse[GAMESTATE:GetMasterPlayerNumber()]);
