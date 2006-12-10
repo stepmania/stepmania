77d3971fd49c9e799dfee950b836d06837d601ed
Chris Danford <chrisdanford@users.sourceforge.net>
2006-12-10 08:18:46 +0000

    virtual on overridden virtuals for clarity

--- stepmania/src/HoldJudgment.h
+++ stepmania/src/HoldJudgment.h
@@ -16,7 +16,7 @@ public:
 
 	void SetHoldJudgment( HoldNoteScore hns );
 	void LoadFromMultiPlayer( MultiPlayer mp );
-	void HandleMessage( const Message &msg );
+	virtual void HandleMessage( const Message &msg );
 
 	//
 	// Lua
