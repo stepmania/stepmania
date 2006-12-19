a8a945352c18f476a92c49592c4daf18be925820
Glenn Maynard <gmaynard@users.sourceforge.net>
2006-12-19 01:55:49 +0000

    update LoadActor don't load the def repeatedly

--- stepmania/Themes/default/BGAnimations/_frame 3x1/default.lua
+++ stepmania/Themes/default/BGAnimations/_frame 3x1/default.lua
@@ -1,7 +1,9 @@
 local File, Width = ...
 local FullFile = THEME:GetPathB('','_frame files 3x1/'..File )
+local Frame = LoadActor( FullFile )
+
 return Def.ActorFrame { children = {
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,0;pause;horizalign,right;x,-Width/2) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,1;pause;zoomtowidth,Width) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,2;pause;horizalign,left;x,Width/2) };
+	Frame .. { InitCommand=cmd(setstate,0;pause;horizalign,right;x,-Width/2) };
+	Frame .. { InitCommand=cmd(setstate,1;pause;zoomtowidth,Width) };
+	Frame .. { InitCommand=cmd(setstate,2;pause;horizalign,left;x,Width/2) };
 }}
--- stepmania/Themes/default/BGAnimations/_frame 3x3/default.lua
+++ stepmania/Themes/default/BGAnimations/_frame 3x3/default.lua
@@ -1,13 +1,14 @@
 local File, Width, Height = ...
 local FullFile = THEME:GetPathB('','_frame files 3x3/'..File )
+local Frame = LoadActor( FullFile )
 return Def.ActorFrame { children = {
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,0;pause;horizalign,right;vertalign,bottom;x,-Width/2;y,-Height/2) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,1;pause;zoomtowidth,Width;vertalign,bottom;zoomtowidth,Width;y,-Height/2) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,2;pause;horizalign,left;vertalign,bottom;x,Width/2;y,-Height/2) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,3;pause;horizalign,right;x,-Width/2;zoomtoheight,Height) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,4;pause;zoomtowidth,Width;zoomtoheight,Height) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,5;pause;horizalign,left;x,Width/2;zoomtoheight,Height) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,6;pause;horizalign,right;vertalign,top;x,-Width/2;y,Height/2) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,7;pause;zoomtowidth,Width;vertalign,top;zoomtowidth,Width;y,Height/2) };
-	LoadActor( FullFile )() .. { InitCommand=cmd(setstate,8;pause;horizalign,left;vertalign,top;x,Width/2;y,Height/2) };
-}}
+	Frame .. { InitCommand=cmd(setstate,0;pause;horizalign,right;vertalign,bottom;x,-Width/2;y,-Height/2) };
+	Frame .. { InitCommand=cmd(setstate,1;pause;zoomtowidth,Width;vertalign,bottom;zoomtowidth,Width;y,-Height/2) };
+	Frame .. { InitCommand=cmd(setstate,2;pause;horizalign,left;vertalign,bottom;x,Width/2;y,-Height/2) };
+	Frame .. { InitCommand=cmd(setstate,3;pause;horizalign,right;x,-Width/2;zoomtoheight,Height) };
+	Frame .. { InitCommand=cmd(setstate,4;pause;zoomtowidth,Width;zoomtoheight,Height) };
+	Frame .. { InitCommand=cmd(setstate,5;pause;horizalign,left;x,Width/2;zoomtoheight,Height) };
+	Frame .. { InitCommand=cmd(setstate,6;pause;horizalign,right;vertalign,top;x,-Width/2;y,Height/2) };
+	Frame .. { InitCommand=cmd(setstate,7;pause;zoomtowidth,Width;vertalign,top;zoomtowidth,Width;y,Height/2) };
+	Frame .. { InitCommand=cmd(setstate,8;pause;horizalign,left;vertalign,top;x,Width/2;y,Height/2) };
+}
