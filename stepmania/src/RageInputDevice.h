4dbbfa4feb4db49df4b9a930fa66eba9ededaa73
Glenn Maynard <gmaynard@users.sourceforge.net>
2007-01-15 03:05:16 +0000

    float

--- stepmania/src/RageInputDevice.h
+++ stepmania/src/RageInputDevice.h
@@ -312,9 +312,9 @@ public:
 	RageTimer ts;
 
 	DeviceInput(): device(InputDevice_Invalid), button(DeviceButton_Invalid), level(0), bDown(false), ts(RageZeroTimer) { }
-	DeviceInput( InputDevice d, DeviceButton b, float l=0 ): device(d), button(b), level(l), bDown(l > 0.5), ts(RageZeroTimer) { }
+	DeviceInput( InputDevice d, DeviceButton b, float l=0 ): device(d), button(b), level(l), bDown(l > 0.5f), ts(RageZeroTimer) { }
 	DeviceInput( InputDevice d, DeviceButton b, float l, const RageTimer &t ):
-		device(d), button(b), level(l), bDown(level > 0.5), ts(t) { }
+		device(d), button(b), level(l), bDown(level > 0.5f), ts(t) { }
 
 	bool operator==( const DeviceInput &other ) const
 	{ 
