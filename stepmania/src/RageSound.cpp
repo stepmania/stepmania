2407d46dbf783a97a695288667bc46e7c8f57a99
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-01-20 06:55:23 +0000

    Fix compile.

--- stepmania/src/RageSound.cpp
+++ stepmania/src/RageSound.cpp
@@ -155,7 +155,7 @@ public:
 	int GetLength() const { return 0; }
 	int GetLength_Fast() const { return 0; }
 	int SetPosition( int iFrame )  { return 1; }
-	int Read( char *buf, int iFrames ) { return RageSoundReader::END_OF_FILE; }
+	int Read( int16_t *pBuf, int iFrames ) { return RageSoundReader::END_OF_FILE; }
 	RageSoundReader *Copy() const { return new RageSoundReader_Silence; }
 	int GetSampleRate() const { return 44100; }
 	unsigned GetNumChannels() const { return 1; }
