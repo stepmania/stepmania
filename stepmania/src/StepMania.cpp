e17ccc6bdb86cd3656df930342719d8fc30e74cb
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-02-04 08:17:22 +0000

    Allow quitting before creating the main window.

--- stepmania/src/StepMania.cpp
+++ stepmania/src/StepMania.cpp
@@ -1056,6 +1056,14 @@ int main(int argc, char* argv[])
 
 	SAFE_DELETE( pLoadingWindow );		// destroy this before init'ing Display
 
+	/* If the user has tried to quit during the loading, do it before creating the
+	 * main window. This prevents going to full screen just to quit. */
+	if( ArchHooks::UserQuit() )
+	{
+		ShutdownGame();
+		return 0;
+	}
+	
 	StartDisplay();
 
 	StoreActualGraphicOptions();
