113a4f0eca33feec82059207de5b7573e499c1cd
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-02-04 02:04:30 +0000

    extern const as per c++98 7.1.5.2.

--- stepmania/PBProject/scripts/increment_version.pl
+++ stepmania/PBProject/scripts/increment_version.pl
@@ -3,15 +3,13 @@
 $ver = 0;
 if( open FH, '<ver.cpp' )
 {
-	$_ = <FH>;
-	/version_num = (\d+);/ and $ver = $1+1;
+	<FH> =~ /version_num = (\d+);/ and $ver = $1 + 1;
 	close FH;
 }
 chop( $date = `date` );
 open FH, '>ver.cpp' or die $!;
-# Cannot use const here since gcc will strip it out...
 print FH <<"EOF";
-unsigned long version_num = $ver;
-const char *version_time = "$date";
+extern const unsigned long version_num = $ver;
+extern const char *const version_time = "$date";
 EOF
 close FH;
