80140f2168126da77887b53b713d2f557adde741
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-02-11 01:30:51 +0000

    atomic writes.

--- stepmania/PBProject/scripts/increment_version.pl
+++ stepmania/PBProject/scripts/increment_version.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+use File::Copy;
 
 $ver = 0;
 if( open FH, '<ver.cpp' )
@@ -7,9 +8,10 @@ if( open FH, '<ver.cpp' )
 	close FH;
 }
 chop( $date = `date` );
-open FH, '>ver.cpp' or die $!;
+open FH, '>.ver.tmp' or die $!;
 print FH <<"EOF";
 extern const unsigned long version_num = $ver;
 extern const char *const version_time = "$date";
 EOF
 close FH;
+move '.ver.tmp', 'ver.cpp';
