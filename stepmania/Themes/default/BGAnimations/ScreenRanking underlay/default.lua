0bb594d32f1a420456245372bafbfa0ec5f7ea81
Glenn Maynard <gmaynard@users.sourceforge.net>
2007-02-16 07:52:05 +0000

    use LoadFont

--- stepmania/Themes/default/BGAnimations/ScreenRanking underlay/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenRanking underlay/default.lua
@@ -1,5 +1,4 @@
-return Def.BitmapText {
-	Font = "blaster",
+return LoadFont(blaster) .. {
 	Text = "Best Ranking",
 	InitCommand = cmd(horizalign,'HorizAlign_Left';x,SCREEN_LEFT+48;y,SCREEN_TOP+32;shadowlength,0)
 }
