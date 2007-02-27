ea27bbf7a9c8f5f665d9836e40fcf63e6dcf1878
Glenn Maynard <gmaynard@users.sourceforge.net>
2007-02-27 01:13:53 +0000

    lua

---
+++ stepmania/Themes/default/BGAnimations/ScreenEvaluationStage out.lua
@@ -0,0 +1,10 @@
+local children = {
+	LoadActor( THEME:GetPathS("", "_swoosh") ) .. { StartTransitioningCommand=cmd(play); };
+	LoadActor("_moveon") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
+		OnCommand=cmd(diffusealpha,0;sleep,1;linear,0.3;diffusealpha,1;sleep,.5);
+	};
+};
+
+return Def.ActorFrame { children=children };
+
