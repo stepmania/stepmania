dd05b42bf29eae0e51344305f07ddfb049aae5dd
Glenn Maynard <gmaynard@users.sourceforge.net>
2007-03-16 09:33:31 +0000

    add IsFillInMarker

--- stepmania/src/HighScore.cpp
+++ stepmania/src/HighScore.cpp
@@ -397,12 +397,21 @@ public:
 	static int GetName( T* p, lua_State *L )			{ lua_pushstring(L, p->GetName() ); return 1; }
 	static int GetScore( T* p, lua_State *L )			{ lua_pushnumber(L, p->GetScore() ); return 1; }
 	static int GetPercentDP( T* p, lua_State *L )			{ lua_pushnumber(L, p->GetPercentDP() ); return 1; }
+	static int IsFillInMarker( T* p, lua_State *L )
+	{
+		bool bIsFillInMarker = false;
+		FOREACH_PlayerNumber( pn )
+			bIsFillInMarker |= p->GetName() == RANKING_TO_FILL_IN_MARKER[pn];
+		lua_pushboolean( L, bIsFillInMarker );
+		return 1;
+	}
 
 	LunaHighScore()
 	{
 		ADD_METHOD( GetName );
 		ADD_METHOD( GetScore );
 		ADD_METHOD( GetPercentDP );
+		ADD_METHOD( IsFillInMarker );
 	}
 };
 
