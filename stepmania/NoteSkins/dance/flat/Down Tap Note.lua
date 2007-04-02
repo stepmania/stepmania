67f9ccce87e9a50a9d50905f0c9676c21bc562ac
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-04-02 00:20:56 +0000

    Add flat note skin.

---
+++ stepmania/NoteSkins/dance/flat/Down Tap Note.lua
@@ -0,0 +1,11 @@
+return Def.Sprite {
+	Texture=NOTESKIN:GetPath('_down','tap note');
+	Frame0000=56;
+	Delay0000=0.1;
+	Frame0001=57;
+	Delay0001=0.1;
+	Frame0002=58;
+	Delay0002=0.1;
+	Frame0003=59;
+	Delay0003=0.1;
+};
