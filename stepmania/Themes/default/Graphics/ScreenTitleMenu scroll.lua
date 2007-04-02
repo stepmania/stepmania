3608aaa0c5f87fbc9815a4c3d504901b04b5d9cd
AJ Kelly <aj187@ajworld.net>
2007-04-02 16:54:44 +0000

    if we're centering the menu, let's center the bitmaptext objects. also, xml -> lua

---
+++ stepmania/Themes/default/Graphics/ScreenTitleMenu scroll.lua
@@ -0,0 +1,11 @@
+local children = {
+	LoadFont("", "blaster") ..{
+		Text=THEME:GetString( 'ScreenTitleMenu', ThisGameCommand:GetText() );
+		InitCommand=cmd(horizalign,center;shadowlength,0);
+		GainFocusCommand=cmd(stoptweening;diffuseshift;effectperiod,0.5;effectcolor1,0.5,1,0.5,1;effectcolor2,0.25,0.5,0.25,1;);
+		LoseFocusCommand=cmd(stoptweening;stopeffect);
+		DisabledCommand=cmd(diffuse,0.5,0.5,0.5,1);
+	};
+};
+
+return Def.ActorFrame { children = children };
