e501603b52e7fe008f16bef9cf699455c57a0264
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-04-03 03:12:47 +0000

    Do the scaling/croping in Init, rather than each time through On.

--- stepmania/Themes/default/BGAnimations/ScreenLogo background/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenLogo background/default.lua
@@ -1,4 +1,3 @@
 return LoadActor( "bg.png" ) .. {
-	InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
-	OnCommand=cmd(scale_or_crop_background);
+	InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;scale_or_crop_background);
 }
