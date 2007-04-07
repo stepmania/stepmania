11db60a74c39be81abf5c9456f3b587da822a074
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-04-07 13:07:35 +0000

    Implement ScreenMusicScroll's functionality in the overlay. It is not quite the same, but it does more or less the same thing without using thousands of actors (one per loaded song!).

---
+++ stepmania/Themes/default/BGAnimations/ScreenMusicScroll overlay.lua
@@ -0,0 +1,25 @@
+local num = THEME:GetMetric( "ScreenMusicScroll", "TimerSeconds" )
+local fontPath = THEME:GetPathF( "ScreenMusicScroll", "titles" )
+local children = {}
+for i=1,num do
+	local song = SONGMAN:GetRandomSong()
+	local color = SONGMAN:GetSongColor( song )
+	local t = Def.BitmapText {
+		_Level = 1;
+		File = fontPath;
+		Text = song:GetDisplayFullTitle();
+		OnCommand = cmd(zoom,0.7;diffuse,color);
+	}
+		
+	table.insert( children, WrapInActorFrame({t}) )
+end
+
+return Def.ActorScroller {
+	NumItemsToDraw = 6;
+	SecondsPerItem = 1;
+	TransformFunction = function( self, offset, itemIndex, numItems )
+		self:y( offset*80 )
+	end;
+	BeginCommand = cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;scrollwithpadding,3,3);
+	children = children;
+}
