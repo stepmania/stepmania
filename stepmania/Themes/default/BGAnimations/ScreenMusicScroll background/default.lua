e2542ad5fafe2023d31914a07b6b82643f8cba98
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-04-07 22:33:53 +0000

    Add text using Lua rather than baked in.

--- stepmania/Themes/default/BGAnimations/ScreenMusicScroll background/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenMusicScroll background/default.lua
@@ -1,3 +1,10 @@
-return LoadActor( "_music scroll background" ) .. {
-	InitCommand = cmd(scale_or_crop_background),
+local children  = {
+	LoadActor( "_music scroll background" ) .. {
+		InitCommand = cmd(scale_or_crop_background);
+	};
+	LoadFont( "ScreenMusicScroll", "thanks" ) .. {
+		Text = "Thank you for playing!";
+		InitCommand = cmd(align,0,1;x,SCREEN_LEFT+20;y,SCREEN_BOTTOM-40;zoom,.8)
+	};
 }
+return WrapInActorFrame( children )
