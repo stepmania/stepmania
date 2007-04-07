d909e8c6a95f92311c8076d17d1f34231a851fbb
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-04-07 12:48:07 +0000

    cleanup.

--- stepmania/Themes/default/BGAnimations/ScreenCredits overlay/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenCredits overlay/default.lua
@@ -5,12 +5,6 @@ local children = {
 	LoadActor( "credits" ) .. {
 		InitCommand = cmd(x,SCREEN_CENTER_X+160;y,SCREEN_CENTER_Y);
 	};
---	Def.Quad {
---		InitCommand = cmd(diffuse,0,0,0,0;setsize,SCREEN_WIDTH,60;x,SCREEN_CENTER_X;y,SCREEN_TOP+30;diffusebottomedge,0,0,0,0);
---	};
---	Def.Quad {
---		InitCommand = cmd(diffuse,0,0,0,0;setsize,SCREEN_WIDTH,60;x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-30;diffusetopedge,0,0,0,0);
---	}
 }
 
 return WrapInActorFrame( children )
