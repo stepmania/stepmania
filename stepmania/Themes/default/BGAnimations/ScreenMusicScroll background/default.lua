f7577ffa78ad8a38786b9cfa931bdb973b93fd2d
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-04-07 11:33:14 +0000

    stretchto -> scale_or_crop_background. This is a cool looking background but the baked in text looks bad. Is there any chance we could get this widescreen or at least without the text?

--- stepmania/Themes/default/BGAnimations/ScreenMusicScroll background/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenMusicScroll background/default.lua
@@ -1,3 +1,3 @@
 return LoadActor( "_music scroll background" ) .. {
-	InitCommand = cmd(stretchto,SCREEN_LEFT,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM),
+	InitCommand = cmd(scale_or_crop_background),
 }
