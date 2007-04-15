4821e7ba06ade61855e88735879b28e4a0bb3b10
AJ Kelly <aj187@ajworld.net>
2007-04-15 22:24:23 +0000

    fix arrows appearing under score but over score frame

---
+++ stepmania/Themes/default/BGAnimations/ScreenGameplay overlay/default.lua
@@ -0,0 +1,18 @@
+local children = { };
+
+if GAMESTATE:IsExtraStage() then
+	children[#children+1] = LoadActor("_extra score frame") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X+0;y,SCREEN_CENTER_Y-208);
+		OnCommand=cmd(addy,-100;sleep,0.5;linear,1;addy,100);
+		OffCommand=cmd(linear,1;addy,-100);
+	};
+else
+	local sFile = GAMESTATE:GetPlayMode() == "PlayMode_Oni" and "_oni score frame" or "_score frame";
+	children[#children+1] = LoadActor(sFile) .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X+0;y,SCREEN_CENTER_Y+208);
+		OnCommand=cmd(addy,100;linear,0.5;addy,-100);
+		OffCommand=cmd(linear,0.5;addy,100);
+	};
+end
+
+return Def.ActorFrame { children = children };
