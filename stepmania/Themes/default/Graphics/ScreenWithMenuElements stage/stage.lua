a341d98591bc3ea60e3b66e511e3c1a2724e3829
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-05-05 23:21:58 +0000

    Simplify.

--- stepmania/Themes/default/Graphics/ScreenWithMenuElements stage/stage.lua
+++ stepmania/Themes/default/Graphics/ScreenWithMenuElements stage/stage.lua
@@ -8,7 +8,7 @@ for _, s in ipairs(Stage) do
 			SetCommand=function(self, params)
 				self:visible( params.StageToShow == s );
 				if params.StageNumber then
-					self:settext( string.format("%i", params.StageNumber+1) );
+					self:settext( tostring(params.StageNumber+1) );
 				end
 			end;
 		}
