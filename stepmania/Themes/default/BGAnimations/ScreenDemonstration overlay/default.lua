0b9d338daafa8cf474dc3c9eb34b4c0be599a97d
AJ Kelly <aj187@ajworld.net>
2007-05-07 17:27:28 +0000

    XML -> Lua

---
+++ stepmania/Themes/default/BGAnimations/ScreenDemonstration overlay/default.lua
@@ -0,0 +1,11 @@
+local children =
+{
+	LoadActor("../_black.png") .. {
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH;zoomtoheight,120;diffusealpha,0.7);
+	};
+	LoadActor("demonstration.png") .. {
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;diffuseblink;effectperiod,1);
+	};
+};
+
+return Def.ActorFrame{ children=children };
---
+++ stepmania/Themes/default/BGAnimations/ScreenGameOver background/default.lua
@@ -0,0 +1,9 @@
+local children =
+{
+	LoadActor("gameover.png") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;diffusealpha,0);
+		OnCommand=cmd(linear,0.3;diffusealpha,0.9;sleep,5);
+	};
+};
+
+return Def.ActorFrame{ children=children };
