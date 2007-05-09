538327e7f3fbce416f7edf18eebbe87cf26419d1
Glenn Maynard <gmaynard@users.sourceforge.net>
2007-05-09 04:43:47 +0000

    add ScreenClassExists

--- stepmania/src/ScreenManager.cpp
+++ stepmania/src/ScreenManager.cpp
@@ -857,6 +857,7 @@ public:
 	}
 	static int SystemMessage( T* p, lua_State *L )		{ p->SystemMessage( SArg(1) ); return 0; }
 	static int ScreenIsPrepped( T* p, lua_State *L )	{ lua_pushboolean( L, ScreenManagerUtil::ScreenIsPrepped( SArg(1) ) ); return 1; }
+	static int ScreenClassExists( T* p, lua_State *L )	{ lua_pushboolean( L, g_pmapRegistrees->find( SArg(1) ) != g_pmapRegistrees->end() ); return 1; }
 
 	LunaScreenManager()
 	{
@@ -864,6 +865,7 @@ public:
 		ADD_METHOD( GetTopScreen );
 		ADD_METHOD( SystemMessage );
 		ADD_METHOD( ScreenIsPrepped );
+		ADD_METHOD( ScreenClassExists );
 	}
 };
 
