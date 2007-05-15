0ae69c2351af4def2e2d1e1e01d5da6b2230eff7
AJ Kelly <aj187@ajworld.net>
2007-05-15 20:07:34 +0000

    change StyleIcon position as not to be covered by the music wheel

--- stepmania/Themes/default/BGAnimations/ScreenWithMenuElements underlay.lua
+++ stepmania/Themes/default/BGAnimations/ScreenWithMenuElements underlay.lua
@@ -3,8 +3,8 @@ if not THEME:GetMetric(Var "LoadingScreen", "StyleIcon") then
 end
 
 local t = LoadActor( THEME:GetPathG(Var "LoadingScreen", "StyleIcon") ) .. {
-	BeginCommand = cmd(x,SCREEN_CENTER_X+817;y,SCREEN_TOP+26);
-	OnCommand = cmd(draworder,97;spring,0.5;addx,-SCREEN_WIDTH);
-	OffCommand = cmd(bouncebegin,0.5;addx,SCREEN_WIDTH);
+	BeginCommand = cmd(x,SCREEN_CENTER_X+8;y,SCREEN_TOP-11);
+	OnCommand = cmd(draworder,200;linear,0.25;addy,33);
+	OffCommand = cmd(linear,0.25;addy,-44);
 };
 return t;
