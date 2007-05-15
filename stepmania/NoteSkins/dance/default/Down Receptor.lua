0af7590ed342c80b504361f9d9951219a6c1ee89
Chris Danford <chrisdanford@users.sourceforge.net>
2007-05-15 22:15:36 +0000

    style cleanup

--- stepmania/NoteSkins/dance/default/Down Receptor.lua
+++ stepmania/NoteSkins/dance/default/Down Receptor.lua
@@ -1,3 +1,2 @@
-return LoadActor( NOTESKIN:GetPath("", "_Tap Receptor"),
-	NOTESKIN:LoadActor( Var "Button", "Go Receptor" ) );
+return LoadActor( NOTESKIN:GetPath("", "_Tap Receptor"), NOTESKIN:LoadActor( Var "Button", "Go Receptor" ) );
 
