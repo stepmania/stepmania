b5aecac4624fbc4579c45dee7dae472e2a9ff90d
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-05-19 22:20:04 +0000

    revert.

--- stepmania/Themes/default/BGAnimations/ScreenSelectMusic underlay/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenSelectMusic underlay/default.lua
@@ -2,14 +2,12 @@ local OnCommand = cmd(diffusealpha,0;linear,0.5;diffusealpha,1)
 local OffCommand = cmd(linear,0.5;diffusealpha,0)
 local t = Def.ActorFrame {
 	LoadActor( '_difficulty frame p1' ) .. {
-		Condition = GAMESTATE:IsHumanPlayer( PLAYER_1 );
-		InitCommand = cmd(x,SCREEN_CENTER_X-275;y,SCREEN_CENTER_Y-15);
+		InitCommand = cmd(player,PLAYER_1;x,SCREEN_CENTER_X-275;y,SCREEN_CENTER_Y-15);
 		OnCommand = OnCommand;
 		OffCommand = OffCommand;
 	};
 	LoadActor( '_difficulty frame p2' ) .. {
-		Condition = GAMESTATE:IsHumanPlayer( PLAYER_2 );
-		InitCommand = cmd(x,SCREEN_CENTER_X-82;y,SCREEN_CENTER_Y-15);
+		InitCommand = cmd(player,PLAYER_2;x,SCREEN_CENTER_X-82;y,SCREEN_CENTER_Y-15);
 		OnCommand = OnCommand;
 		OffCommand = OffCommand;
 	};
