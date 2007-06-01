609f5e151e901ff0d4874e566c20529738f37d33
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-06-01 21:40:51 +0000

    Fix lua compile without committing the compiled files.

--- stepmania/Themes/default/Graphics/ScreenSelectProfile scroll choiceProfile.lua
+++ stepmania/Themes/default/Graphics/ScreenSelectProfile scroll choiceProfile.lua
@@ -6,13 +6,13 @@ return Def.ActorFrame {
 	};
 	LoadFont( "Common normal" ) .. {
 		Text=PROFILEMAN:GetLocalProfile( Var("GameCommand"):GetProfileID() ):GetDisplayName();
-		OnCommand=cmd(diffusealpha,0;linear,0.3;diffusealpha,1):
+		OnCommand=cmd(diffusealpha,0;linear,0.3;diffusealpha,1);
 		OffCommand=cmd(linear,0.3;diffusealpha,0);
 		InitCommand=cmd(halign,0;shadowlength,2;x,50);
 		GainFocusCommand=cmd(stoptweening;diffuseshift;effectperiod,0.5;effectcolor1,1,0.5,0.5,1;effectcolor2,0.5,0.25,0.25,1;linear,0;zoom,1.0);
-		LoseFocusCommand="stoptweening;stopeffect;linear,0;zoom,0.9"
-		EnabledCommand=""
-		DisabledCommand="diffuse,0.5,0.5,0.5,1"
+		LoseFocusCommand="stoptweening;stopeffect;linear,0;zoom,0.9";
+		EnabledCommand="";
+		DisabledCommand="diffuse,0.5,0.5,0.5,1";
 	};
 	LoadFont( "Common normal" ) .. {
 		Text=PROFILEMAN:GetLocalProfile( Var("GameCommand"):GetProfileID() ):GetNumTotalSongsPlayed() .. " song(s) played";
