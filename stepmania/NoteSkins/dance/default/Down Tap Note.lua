c67987bac0f8397c1db0b0c734c1b3f75257f01b
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-07-29 04:01:56 +0000

    Simplify.

--- stepmania/NoteSkins/dance/default/Down Tap Note.lua
+++ stepmania/NoteSkins/dance/default/Down Tap Note.lua
@@ -1,12 +1,4 @@
-local t = Def.Sprite {
-	Texture=NOTESKIN:GetPath('_down','tap note');
-	Frame0000=0;
-	Delay0000=0.1;
-	Frame0001=1;
-	Delay0001=0.1;
-	Frame0002=2;
-	Delay0002=0.1;
-	Frame0003=3;
-	Delay0003=0.1;
+return Def.Sprite {
+	Texture=NOTESKIN:GetPath( '_down', 'tap note' );
+	Frames = Sprite.LinearFrames( 4, 1 );
 };
-return t;
