2956390080b2014e8fc3a5570ccc7d457682f012
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-08-11 23:17:38 +0000

    Unbake text from graphic. Someone else might want to change the text a bit.

--- stepmania/Themes/default/BGAnimations/ScreenGameplay overlay/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenGameplay overlay/default.lua
@@ -1,19 +1,25 @@
-local t = Def.ActorFrame {
+local font = LoadFont( "blaster" );
+local frame = LoadActor( "_score frame" );
+local text = GAMESTATE:GetPlayMode() == 'PlayMode_Oni' and "time" or "score";
+return Def.ActorFrame {
+	Def.ActorFrame {
+		frame;
+		font .. {
+			Text = "player 1 " .. text;
+			InitCommand = cmd(addy,14;addx,-20;zoom,0.4;shadowlength,0);
+		};
+		InitCommand = cmd(x,SCREEN_LEFT-100;y,SCREEN_CENTER_Y+202);
+		OnCommand = cmd(linear,1;x,SCREEN_CENTER_X-208);
+		OffCommand = cmd(linear,1;x,SCREEN_LEFT-100)
+	};
+	Def.ActorFrame {
+		frame;
+		font .. {
+			Text = "player 2 " .. text;
+			InitCommand = cmd(addy,14;addx,20;zoom,0.4;shadowlength,0);
+		};
+		InitCommand = cmd(x,SCREEN_RIGHT+100;y,SCREEN_CENTER_Y+202);
+		OnCommand = cmd(linear,1;x,SCREEN_CENTER_X+210);
+		OffCommand = cmd(linear,1;x,SCREEN_RIGHT+100)
+	}
 };
-
-if GAMESTATE:IsExtraStage() then
-	t[#t+1] = LoadActor("_extra score frame") .. {
-		InitCommand=cmd(x,SCREEN_CENTER_X+0;y,SCREEN_CENTER_Y-208);
-		OnCommand=cmd(addy,-100;sleep,0.5;linear,1;addy,100);
-		OffCommand=cmd(linear,1;addy,-100);
-	};
-else
-	local sFile = GAMESTATE:GetPlayMode() == "PlayMode_Oni" and "_oni score frame" or "_score frame";
-	t[#t+1] = LoadActor(sFile) .. {
-		InitCommand=cmd(x,SCREEN_CENTER_X+0;y,SCREEN_CENTER_Y+208);
-		OnCommand=cmd(addy,100;linear,0.5;addy,-100);
-		OffCommand=cmd(linear,0.5;addy,100);
-	};
-end
-
-return t;
