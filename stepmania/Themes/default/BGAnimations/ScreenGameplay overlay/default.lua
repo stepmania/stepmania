3588ccd54bdd5a312293d9f81a0468bb59d44493
AJ Kelly <aj187@ajworld.net>
2007-08-11 23:46:06 +0000

    change font and use some conditional magic to show the frames for players that are joined

--- stepmania/Themes/default/BGAnimations/ScreenGameplay overlay/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenGameplay overlay/default.lua
@@ -1,4 +1,4 @@
-local font = LoadFont( "blaster" );
+local font = LoadFont( "_zeroesthree" );
 local frame = LoadActor( "_score frame" );
 local text = GAMESTATE:GetPlayMode() == 'PlayMode_Oni' and "time" or "score";
 return Def.ActorFrame {
@@ -6,20 +6,22 @@ return Def.ActorFrame {
 		frame;
 		font .. {
 			Text = "player 1 " .. text;
-			InitCommand = cmd(addy,14;addx,-20;zoom,0.4;shadowlength,0);
+			InitCommand = cmd(horizalign,left;addy,16;addx,-80;zoom,0.75;shadowlength,0);
 		};
 		InitCommand = cmd(x,SCREEN_LEFT-100;y,SCREEN_CENTER_Y+202);
 		OnCommand = cmd(linear,1;x,SCREEN_CENTER_X-208);
-		OffCommand = cmd(linear,1;x,SCREEN_LEFT-100)
+		OffCommand = cmd(linear,1;x,SCREEN_LEFT-100);
+		Condition=GAMESTATE:IsSideJoined(PLAYER_1) == true;
 	};
 	Def.ActorFrame {
 		frame;
 		font .. {
 			Text = "player 2 " .. text;
-			InitCommand = cmd(addy,14;addx,20;zoom,0.4;shadowlength,0);
+			InitCommand = cmd(horizalign,right;addy,16;addx,80;zoom,0.75;shadowlength,0);
 		};
 		InitCommand = cmd(x,SCREEN_RIGHT+100;y,SCREEN_CENTER_Y+202);
 		OnCommand = cmd(linear,1;x,SCREEN_CENTER_X+210);
-		OffCommand = cmd(linear,1;x,SCREEN_RIGHT+100)
+		OffCommand = cmd(linear,1;x,SCREEN_RIGHT+100);
+		Condition=GAMESTATE:IsSideJoined(PLAYER_2) == true;
 	}
 };
