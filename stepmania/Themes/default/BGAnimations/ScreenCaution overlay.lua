c9f2b2b418e16379638b0718bfa262e87c881b42
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-08-12 03:51:54 +0000

    Allow localization.

--- stepmania/Themes/default/BGAnimations/ScreenCaution overlay.lua
+++ stepmania/Themes/default/BGAnimations/ScreenCaution overlay.lua
@@ -1,12 +1,12 @@
 local t = Def.ActorFrame {
 	LoadFont("blaster") .. {
-		Text="CAUTION!";
+		Text=THEME:GetString( "ScreenCaution", "Caution" );
 		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+60;diffuse,1,0,0,1;zoom,1.5;diffusebottomedge,0.25,0,0,1;shadowlength,0);
 	};
 	
 	LoadFont("_zeroesthree") .. {
-		Text="For safety purposes, play with the lights on\nand keep the dance platform a safe distance\nfrom your TV or monitor as well as any other\nfurniture at all times.\nDo not allow pets, children, or other spectators\non the dance platform during gameplay.\nBe aware of the platform\'s position at all times.";
-		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;horizalign,center;shadowlength,0);
+		Text=THEME:GetString( "ScreenCaution", "CautionText" );
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;horizalign,center;shadowlength,0;wrapwidthpixels,SCREEN_WIDTH-80);
 	};
 };
 
