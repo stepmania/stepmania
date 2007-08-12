40ad9a166c883e922695db56a6b22a3a53e49768
Glenn Maynard <gmaynard@users.sourceforge.net>
2007-08-12 21:59:10 +0000

    A common problem we have is being able to manipulate major objects, like songs and courses: it's difficult to guarantee that all references to them are updated if they're deleted or reallocated, which is a brittleness that leads to obscure bugs.
    A general fix for this is to avoid storing pointers to objects, and
    to use IDs instead; for example, SongID to reference a song (which
    tags by directory).  This leads to an efficiency problem: searching
    a map<RString> is much more expensive.  We need a robust way to
    cache the results.
    
    Implement CachedObject<T>, CachedObjectPointer<T>.  This implements
    caching pointers to objects, negative caching, clearing cache when objects
    are deleted, and clearing negative cache when objects are created.

---
+++ stepmania/src/RageUtil_CachedObject.cpp
@@ -0,0 +1,74 @@
+#include "global.h"
+#include "RageUtil_CachedObject.h"
+#include "RageThreads.h"
+
+#if 0
+/* Example use: */
+struct Object
+{
+	CachedObject<Object> m_CachedObject;
+
+	Object() { }
+};
+
+struct User
+{
+	CachedObjectPointer<Object> cache;
+
+	Object *foobar()
+	{
+		Object *p;
+		if( !cache.Get(&p) )
+		{
+			p = NULL;
+			cache.Set(p);
+		}
+		return p;
+	}
+};
+
+CACHED_REGISTER_CLASS(Object);
+
+void test()
+{
+	Object p;
+	User gar;
+	gar.foobar();
+}
+#endif
+
+static RageMutex m_Mutex("CachedObjects");
+void CachedObjectHelpers::Lock()
+{
+	m_Mutex.Lock();
+}
+
+void CachedObjectHelpers::Unlock()
+{
+	m_Mutex.Unlock();
+}
+
+/*
+ * (c) 2007 Glenn Maynard
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
---
+++ stepmania/src/RageUtil_CachedObject.h
@@ -0,0 +1,199 @@
+/* CachedObject - cached object pointers with automatic invalidation. */
+
+#ifndef RAGE_UTIL_CACHED_OBJECT_H
+#define RAGE_UTIL_CACHED_OBJECT_H
+
+#include "Foreach.h"
+
+template<typename T>
+class CachedObjectPointer;
+
+namespace CachedObjectHelpers
+{
+	void Lock();
+	void Unlock();
+}
+
+template<typename T>
+class CachedObject
+{
+public:
+	CachedObject()
+	{
+		m_pObject = NULL;
+
+		/* A new object is being constructed, so invalidate negative caching. */
+		ClearCacheNegative();
+	}
+
+	CachedObject( const CachedObject &cpy )
+	{
+		m_pObject = NULL;
+		ClearCacheNegative();
+	}
+
+	~CachedObject()
+	{
+		if( m_pObject != NULL )
+			ClearCacheSpecific( m_pObject );
+	}
+
+	CachedObject &operator=( const CachedObject &rhs ) { return *this; }
+
+	/* Clear all cached entries for this type. */
+	static void ClearCacheAll()
+	{
+		CachedObjectHelpers::Lock();
+		FOREACH( ObjectPointer *, m_apObjectPointers, p )
+		{
+			(*p)->m_pCache = NULL;
+			(*p)->m_bCacheIsSet = false;
+		}
+		CachedObjectHelpers::Unlock();
+	}
+
+	/* Clear all cached entries pointing to a specific object. */
+	static void ClearCacheSpecific( const T *pObject )
+	{
+		CachedObjectHelpers::Lock();
+		FOREACH( ObjectPointer *, m_apObjectPointers, p )
+		{
+			if( (*p)->m_pCache == pObject )
+			{
+				(*p)->m_pCache = NULL;
+				(*p)->m_bCacheIsSet = false;
+			}
+		}
+		CachedObjectHelpers::Unlock();
+	}
+
+	/* Clear all negative cached entries of this type. */
+	static void ClearCacheNegative()
+	{
+		CachedObjectHelpers::Lock();
+		FOREACH( ObjectPointer *, m_apObjectPointers, p )
+		{
+			if( (*p)->m_pCache == NULL )
+				(*p)->m_bCacheIsSet = false;
+		}
+		CachedObjectHelpers::Unlock();
+	}
+
+private:
+	typedef CachedObjectPointer<T> ObjectPointer;
+	friend class ObjectPointer;
+
+	static void Register( ObjectPointer *p )
+	{
+		m_apObjectPointers.push_back( p );
+	}
+
+	static void Unregister( ObjectPointer *p )
+	{
+		vector<ObjectPointer *>::iterator it = find( m_apObjectPointers.begin(), m_apObjectPointers.end(), p );
+		ASSERT( it != m_apObjectPointers.end() );
+		m_apObjectPointers.erase( it );
+	}
+
+	/* This points to the actual T this object is contained in.  This is set
+	 * the first time CachedObjectPointer::Set() is called for this object.
+	 * That's more convenient than setting it ourselves; we don't need to
+	 * do anything special in T's copy ctor.  This works because there's no
+	 * need to clear cache for an object before any CachedObjectPointers have
+	 * ever been set for it. */
+	const T *m_pObject;
+	static vector<ObjectPointer *> m_apObjectPointers;
+};
+
+template<typename T>
+class CachedObjectPointer
+{
+public:
+	typedef CachedObject<T> Object;
+
+	CachedObjectPointer()
+	{
+		m_bCacheIsSet = false;
+		Object::Register( this );
+	}
+
+	CachedObjectPointer( const CachedObjectPointer &cpy )
+	{
+		CachedObjectHelpers::Lock();
+		m_pCache = cpy.m_pCache;
+		m_bCacheIsSet = cpy.m_bCacheIsSet;
+		Object::Register( this );
+		CachedObjectHelpers::Unlock();
+	}
+
+	~CachedObjectPointer()
+	{
+		Object::Unregister( this );
+	}
+
+	bool Get( T **pRet ) const
+	{
+		CachedObjectHelpers::Lock();
+		if( !m_bCacheIsSet )
+		{
+			CachedObjectHelpers::Unlock();
+			return false;
+		}
+		*pRet = m_pCache;
+		CachedObjectHelpers::Unlock();
+		return true;
+	}
+
+	void Set( T *p )
+	{
+		CachedObjectHelpers::Lock();
+		m_pCache = p;
+		m_bCacheIsSet = true;
+		if( p != NULL )
+			p->m_CachedObject.m_pObject = p;
+		CachedObjectHelpers::Unlock();
+	}
+
+	void Unset()
+	{
+		CachedObjectHelpers::Lock();
+		m_pCache = NULL;
+		m_bCacheIsSet = false;
+		CachedObjectHelpers::Unlock();
+	}
+
+private:
+	friend class Object;
+
+	T *m_pCache;
+	bool m_bCacheIsSet;
+};
+
+#define CACHED_REGISTER_CLASS(T) template<> vector<CachedObject<T>::ObjectPointer *> CachedObject<T>::m_apObjectPointers;
+
+#endif
+
+/*
+ * (c) 2007 Glenn Maynard
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
