af0211c6a9af6cf00b3111e213c095ae47938e9b
Steve Checkoway <stevecheckoway@users.sourceforge.net>
2007-08-18 02:08:37 +0000

    This should be checking if the player is enabled, not if the font is not null.

--- stepmania/src/ScreenNetEvaluation.cpp
+++ stepmania/src/ScreenNetEvaluation.cpp
@@ -193,7 +193,7 @@ void ScreenNetEvaluation::UpdateStats()
 	for( int j=0; j<NETNUMTAPSCORES; ++j )
 	{
 		int iNumDigits = (j==JudgeLine_MaxCombo)? MAX_COMBO_NUM_DIGITS:4;
-		if( m_textJudgeNumbers[j][m_pActivePlayer].m_pFont != NULL )
+		if( GAMESTATE->IsPlayerEnabled(m_pActivePlayer) ) // XXX: Why would this not be the case? -- Steve
 			m_textJudgeNumbers[j][m_pActivePlayer].SetText( ssprintf("%*d", iNumDigits, NSMAN->m_EvalPlayerData[m_iCurrentPlayer].tapScores[j]) );
 	}
 
