6af9a963871ddbb9acfb75ba04ac7c30ecd97d6c
Chris Danford <chrisdanford@users.sourceforge.net>
2008-03-11 09:59:15 +0000

    rough new theme: title, gameplay

--- stepmania/Themes/default/BGAnimations/ScreenGameplay overlay/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenGameplay overlay/default.lua
@@ -1,6 +1,6 @@
-local font = LoadFont( "_zeroesthree" );
+local font = LoadFont( "common normal" );
 local frame = LoadActor( "_score frame" );
-local text = GAMESTATE:GetPlayMode() == 'PlayMode_Oni' and "time" or "score";
+--local text = GAMESTATE:GetPlayMode() == 'PlayMode_Oni' and "time" or "score";
 
 local function DifficultyLabelText(pn)
 	local yNormal = SCREEN_CENTER_Y+175;
@@ -59,28 +59,4 @@ return Def.ActorFrame {
 	-- text on difficulty labels
 	DifficultyLabelText(PLAYER_1);
 	DifficultyLabelText(PLAYER_2);
-
-	-- text on bottom of score frames
-	Def.ActorFrame {
-		frame;
-		font .. {
-			Text = "player 1 " .. text;
-			InitCommand = cmd(horizalign,left;addy,16;addx,-80;zoom,0.75;shadowlength,0);
-		};
-		InitCommand = cmd(x,SCREEN_LEFT-100;y,SCREEN_CENTER_Y+202);
-		OnCommand = cmd(linear,1;x,SCREEN_CENTER_X-208);
-		OffCommand = cmd(linear,1;x,SCREEN_LEFT-100);
-		Condition=GAMESTATE:IsSideJoined(PLAYER_1) == true or GAMESTATE:GetPlayMode() == 'PlayMode_Rave';
-	};
-	Def.ActorFrame {
-		frame;
-		font .. {
-			Text = "player 2 " .. text;
-			InitCommand = cmd(horizalign,right;addy,16;addx,80;zoom,0.75;shadowlength,0);
-		};
-		InitCommand = cmd(x,SCREEN_RIGHT+100;y,SCREEN_CENTER_Y+202);
-		OnCommand = cmd(linear,1;x,SCREEN_CENTER_X+210);
-		OffCommand = cmd(linear,1;x,SCREEN_RIGHT+100);
-		Condition=GAMESTATE:IsSideJoined(PLAYER_2) == true or GAMESTATE:GetPlayMode() == 'PlayMode_Rave';
-	}
 };
--- stepmania/Themes/default/BGAnimations/ScreenLogo background/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenLogo background/default.lua
@@ -1,3 +1,20 @@
-return LoadActor( "bg.png" ) .. {
-	InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;scale_or_crop_background);
+return Def.ActorFrame {
+	LoadActor( "bg sky" ) .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
+	};
+	LoadActor( "bg ring" ) .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
+	};
+	LoadActor( "bg rainbow" ) .. {
+		InitCommand=cmd(x,SCREEN_LEFT;y,SCREEN_BOTTOM;horizalign,left;vertalign,bottom;);
+	};
+	LoadActor( "cursor frame" ) .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;);
+	};
+	LoadActor( "help pane" ) .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X+200;y,SCREEN_BOTTOM;vertalign,bottom;);
+	};
+	LoadActor( "settings pane" ) .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X-200;y,SCREEN_TOP;vertalign,top;);
+	};
 }
