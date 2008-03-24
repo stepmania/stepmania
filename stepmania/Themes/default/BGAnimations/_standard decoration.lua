f6b692823dbc0879d3628143fa8ca10f11af4f2c
Chris Danford <chrisdanford@users.sourceforge.net>
2008-03-24 04:07:46 +0000

    remove DifficultyIcon, use DifficultyDisplay in gameplay, DifficultyListRow -> DifficultyDisplayListRow to make inheritance clear

--- stepmania/Themes/default/BGAnimations/_standard decoration.lua
+++ stepmania/Themes/default/BGAnimations/_standard decoration.lua
@@ -1,6 +1,11 @@
 local MetricsName, FileName = ...;
 return LoadActor( THEME:GetPathG(Var "LoadingScreen",FileName) ) .. {
-	InitCommand=cmd(x,THEME:GetMetric(Var "LoadingScreen",MetricsName .. "X");y,THEME:GetMetric(Var "LoadingScreen",MetricsName .. "Y"););
+	InitCommand=function(self)
+			local x1 = THEME:GetMetric(Var "LoadingScreen",MetricsName .. "X");
+			if x1 then self:x(x1); end
+			local y1 = THEME:GetMetric(Var "LoadingScreen",MetricsName .. "Y");
+			if y1 then self:y(y1); end
+		end;
 	OnCommand=THEME:GetMetric(Var "LoadingScreen",MetricsName .. "OnCommand");
 	OffCommand=THEME:GetMetric(Var "LoadingScreen",MetricsName .. "OffCommand");
 };
