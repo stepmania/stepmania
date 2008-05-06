9f3d57ed32aa860327e4917d7c73c90c16b1ab43
Glenn Maynard <gmaynard@users.sourceforge.net>
2008-05-06 06:04:58 +0000

    gcc binary backwards-compatibility stub

---
+++ stepmania/src/archutils/Unix/StackCheck.cpp
@@ -0,0 +1,33 @@
+#include "global.h"
+
+/* Newer g++ versions (around 4.2) output __stack_chk_fail calls.  This is grossly
+ * backwards-incompatible.  It can be disabled, but you'd have to disable it in any
+ * statically linked libraries: all Ubuntu static libraries have this enabled.  Work
+ * around it by declaring the symbol manually. */
+extern "C" void __stack_chk_fail (void) __attribute__ ((__weak__)) __attribute__ ((__noreturn__));
+extern "C" void __stack_chk_fail (void) { abort(); }
+
+/*
+ * (c) 2008 Glenn Maynard
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
