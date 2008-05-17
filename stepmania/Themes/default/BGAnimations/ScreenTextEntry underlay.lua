18654fb89a9c7b5f1a64885a1f9c174b32d0584f
Josh Allen <axlecrusher@users.sourceforge.net>
2008-05-17 16:40:03 +0000

    Replace text entry quad with sprite.

--- stepmania/Themes/default/BGAnimations/ScreenTextEntry underlay.lua
+++ stepmania/Themes/default/BGAnimations/ScreenTextEntry underlay.lua
@@ -1,3 +1,7 @@
-return Def.Quad {
-	InitCommand=cmd(diffuse,0.5,0.5,1,0.7;zoomtowidth,300;zoomtoheight,24;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+20);
-};
+return Def.ActorFrame
+{
+	LoadActor( THEME:GetPathG("", "OptionRowSimple Frame (doubleres)") ) ..
+	{
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+20);
+	};
+}
