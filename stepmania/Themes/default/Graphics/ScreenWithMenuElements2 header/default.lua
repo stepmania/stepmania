d6d44e2927378470ef939da1bdd1fa7e0db50761
Josh Allen <axlecrusher@users.sourceforge.net>
2008-05-24 01:12:04 +0000

    Make the header texts listen to an update message

--- stepmania/Themes/default/Graphics/ScreenWithMenuElements2 header/default.lua
+++ stepmania/Themes/default/Graphics/ScreenWithMenuElements2 header/default.lua
@@ -9,14 +9,22 @@ return Def.ActorFrame {
 		};
 		LoadFont( "_sf sports night ns upright 52" ) .. {
 			InitCommand=cmd(x,64;y,-7;horizalign,left;shadowlength,0;settext,ScreenString("HeaderText");skewx,-0.15);
+			Name="HeaderText";
 		};
 		LoadFont( "_venacti 24" ) .. {
 			InitCommand=cmd(x,64;y,12;horizalign,left;shadowlength,0;settext,ScreenString("HeaderSubText"););
+			Name="HeaderSubText";
 		};
 		LoadActor( "arrow" ) .. {
 		};
 		LoadActor( "ring" ) .. {
 			InitCommand=cmd(x,-1.5);
 		};
+		UpdateScreenHeaderMessageCommand=function(self,params)
+			local header = self:GetChild("HeaderText");
+			local subHeader = self:GetChild("HeaderSubText");
+			header:settext( params.Header );
+			subHeader:settext( params.Subheader );
+		end
 	};
 };
