7227081c394907c649f77a9bdced45c37cee7c0b
Colby Klein <shakesoda@gmail.com>
2008-07-01 02:56:41 +0000

    Highlight the receptors when pressed.

--- stepmania/NoteSkins/kb7/default/Down Receptor.lua
+++ stepmania/NoteSkins/kb7/default/Down Receptor.lua
@@ -1,1 +1,13 @@
-return LoadActor( NOTESKIN:GetPath("", "_Tap Receptor"), NOTESKIN:LoadActor( Var "Button", "Go Receptor" ) );
+local t = Def.ActorFrame {
+	LoadActor( "Down Go Receptor" )..{
+		InitCommand=NOTESKIN:GetMetricA('ReceptorArrow', 'InitCommand');
+		NoneCommand=NOTESKIN:GetMetricA('ReceptorArrow', 'NoneCommand');
+	};
+	LoadActor( "Down Go Receptor" )..{
+		InitCommand=NOTESKIN:GetMetricA('ReceptorOverlay', 'InitCommand');
+		PressCommand=NOTESKIN:GetMetricA('ReceptorOverlay', 'PressCommand');
+		LiftCommand=NOTESKIN:GetMetricA('ReceptorOverlay', 'LiftCommand');
+		NoneCommand=NOTESKIN:GetMetricA('ReceptorArrow', 'NoneCommand');
+	};
+};
+return t;
