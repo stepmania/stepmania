96389ba367099817a84a5eaab99034987c31683c
Chris Danford <chrisdanford@users.sourceforge.net>
2008-07-21 08:05:06 +0000

    fixup

---
+++ stepmania/Themes/default/BGAnimations/ScreenStage decorations/default.lua
@@ -0,0 +1,23 @@
+local t = {};
+t[#t+1] = Def.ActorFrame {
+	LoadActor("horizon") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;);
+	};
+	LoadActor("sun") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;);
+	};
+};
+local InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;);
+local OnCommand=cmd(cropbottom,1;linear,0.5;cropbottom,0;sleep,10;);
+local function AddChild( i )
+	t[#t+1] = LoadActor( i ) .. {
+		InitCommand=InitCommand;
+		OnCommand=OnCommand;
+		Condition=GAMESTATE:GetCurrentStage() == i;
+	};
+end
+
+for i in ivalues(Stage) do
+	AddChild( i );
+end
+return t;
