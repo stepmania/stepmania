f787cf32904074fa47e654345ead8bea7ad60868
Chris Danford <chrisdanford@users.sourceforge.net>
2008-08-17 14:36:06 +0000

    eval/score cleanup

--- stepmania/Themes/default/BGAnimations/ScreenEvaluationStage decorations/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenEvaluationStage decorations/default.lua
@@ -41,38 +41,28 @@ if ShowStandardDecoration("ComboGraph") then
 	end
 end
 
-t[#t+1] = ComboGraph(PLAYER_1) .. {
-	InitCommand = cmd(x,SCREEN_CENTER_X-222;y,SCREEN_CENTER_Y+10;draworder,1;);
-};
-t[#t+1] = ComboGraph(PLAYER_2) .. {
-	InitCommand = cmd(x,SCREEN_CENTER_X+222;y,SCREEN_CENTER_Y+10;draworder,1;);
-};
-
 t[#t+1] = StandardDecorationFromFile( "TimingDifficultyFrame", "TimingDifficultyFrame" );
 t[#t+1] = LoadFont( Var "LoadingScreen", "TimingDifficultyNumber" ) .. {
 	InitCommand=function(self) self:name("TimingDifficultyNumber"); self:settext(GetTimingDifficulty()); ActorUtil.LoadAllCommandsAndSetXY(self,Var "LoadingScreen"); end;
 };
 
-t[#t+1] = Def.ModIconRow {
-		InitCommand=cmd(x,SCREEN_CENTER_X-316;y,SCREEN_CENTER_Y-130;draworder,1;Load,"ModIconRowEvaluationP1",PLAYER_1,player,PLAYER_1);
-		OnCommand=cmd(zoomy,0;linear,0.5;zoomy,1);
-		OffCommand=cmd(linear,0.5;zoomy,0);
-	};
-t[#t+1] = Def.ModIconRow {
-		InitCommand=cmd(x,SCREEN_CENTER_X+316;y,SCREEN_CENTER_Y-130;draworder,1;Load,"ModIconRowEvaluationP2",PLAYER_2;player,PLAYER_2);
-		OnCommand=cmd(zoomy,0;linear,0.5;zoomy,1);
-		OffCommand=cmd(linear,0.5;zoomy,0);
-	};
 
-t[#t+1] = Def.StepsDisplay {
-		InitCommand=cmd(x,SCREEN_CENTER_X-230;y,SCREEN_CENTER_Y+158;Load,"StepsDisplayEvaluation",PLAYER_1;SetFromGameState,PLAYER_1;player,PLAYER_1;);
-		OnCommand=cmd(zoomy,0;linear,0.5;zoomy,1);
-		OffCommand=cmd(linear,0.5;zoomy,0);
-	};
-t[#t+1] = Def.StepsDisplay {
-		InitCommand=cmd(x,SCREEN_CENTER_X+230;y,SCREEN_CENTER_Y+158;Load,"StepsDisplayEvaluation",PLAYER_2;SetFromGameState,PLAYER_2;player,PLAYER_2;);
-		OnCommand=cmd(zoomy,0;linear,0.5;zoomy,1);
-		OffCommand=cmd(linear,0.5;zoomy,0);
-	};
+if ShowStandardDecoration("ModIconRows") then
+	for pn in ivalues(PlayerNumber) do
+		local t2 = Def.ModIconRow {
+				InitCommand=cmd(Load,"ModIconRowEvaluation"..PlayerNumber:ToString()[pn],pn);
+			};	
+		t[#t+1] = StandardDecorationFromTable( "ModIconRow" .. PlayerNumber:ToString()[pn], t2 );
+	end
+end
+
+if ShowStandardDecoration("StepsDisplay") then
+	for pn in ivalues(PlayerNumber) do
+		local t2 = Def.StepsDisplay {
+				InitCommand=cmd(Load,"StepsDisplayEvaluation",pn;SetFromGameState,pn;);
+			};	
+		t[#t+1] = StandardDecorationFromTable( "StepsDisplay" .. PlayerNumber:ToString()[pn], t2 );
+	end
+end
 
 return t;
