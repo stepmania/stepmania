b498da5683e00e1f6dbcda1d36521cdaee4de4bd
Chris Danford <chrisdanford@users.sourceforge.net>
2008-11-27 02:55:52 +0000

    don't make command name lowercase; remove 'don't force lowercase' hack

--- stepmania/src/Command.cpp
+++ stepmania/src/Command.cpp
@@ -12,12 +12,6 @@ RString Command::GetName() const
 		return RString();
 	RString s = m_vsArgs[0];
 	Trim( s );
-
-	/* Transition hack: if a command begins with !, don't force it to lowercase. */
-	if( s.Left(1) == "!" )
-		s.erase( 0, 1 );
-	else
-		s.MakeLower();
 	return s;
 }
 
