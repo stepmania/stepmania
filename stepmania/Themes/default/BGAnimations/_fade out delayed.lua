1c87ce4db18097ab3d8939f2aec8ced322d62ad0
Rob Campbell <grim@darkskystudios.net>
2009-01-05 07:06:40 +0000

    transition work part II, and some other small touchups

--- stepmania/Themes/default/BGAnimations/_fade out delayed.lua
+++ stepmania/Themes/default/BGAnimations/_fade out delayed.lua
@@ -1,5 +1,5 @@
-local t = Def.Quad {
+local delay = ScreenMetric("OutDelay");
+return Def.Quad {
 	InitCommand=cmd(diffuse,color("#000000");stretchto,SCREEN_LEFT,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM);
-	StartTransitioningCommand=cmd(hibernate,.8;diffusealpha,0;linear,0.3;diffusealpha,1);
+	StartTransitioningCommand=cmd(hibernate,delay;diffusealpha,0;linear,0.3;diffusealpha,1);
 };
-return t;
--- stepmania/Themes/default/Graphics/ScreenTitleMenu scroll.lua
+++ stepmania/Themes/default/Graphics/ScreenTitleMenu scroll.lua
@@ -3,7 +3,7 @@ local s = THEME:GetString( 'ScreenTitleMenu', gc:GetText() );
 local t = Def.ActorFrame {
 	LoadFont("_venacti bold 24px") ..{
 		InitCommand=cmd(uppercase,true;settext,s;horizalign,center;shadowlengthx,0;shadowlengthy,2;strokecolor,color("#00000044"););
-		OffCommand=cmd(sleep,(7-gc:GetIndex())/12;linear,.1;addy,300);
+		OffCommand=cmd(sleep,(7-gc:GetIndex())/20;linear,.08;addy,280);
 		GainFocusCommand=cmd(stoptweening;diffuseshift;effectperiod,0.5;effectcolor1,0.5,1,0.5,1;effectcolor2,0.25,0.5,0.25,1;);
 		LoseFocusCommand=cmd(stoptweening;stopeffect);
 		DisabledCommand=cmd(diffuse,0.5,0.5,0.5,1);
