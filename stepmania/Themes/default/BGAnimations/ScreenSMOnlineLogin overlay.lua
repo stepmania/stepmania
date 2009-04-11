5fb7c98009a951a09d64f542178d5e6171903907
AJ Kelly <aj187@ajworld.net>
2009-04-11 01:41:45 +0000

    some SMO-related changes

--- stepmania/Themes/default/BGAnimations/ScreenSMOnlineLogin overlay.lua
+++ stepmania/Themes/default/BGAnimations/ScreenSMOnlineLogin overlay.lua
@@ -1,21 +1,11 @@
+local FontName = "_venacti Bold 15px";
 local OffCommand = cmd(accelerate,0.3;addx,SCREEN_WIDTH);
+
 local t = Def.ActorFrame {
-	LoadFont( "_zeroesthree" ) .. {
-		Text = "Depending on the server you're connected to,";
-		InitCommand=cmd(zoom,.75;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+90);
+	LoadFont( FontName ) .. {
+		Text = ScreenString("Depending on the server you're connected to, you may have to register for an account on their website.");
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+70;shadowlength,1;wrapwidthpixels,SCREEN_WIDTH*0.75);
 		OnCommand=cmd(cropright,1;linear,.5;cropright,0);
-		OffCommand=OffCommand;
-	};
-	LoadFont( "_zeroesthree" ) .. {
-		Text="you may have to register for an account";
-		InitCommand=cmd(zoom,.75;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+107);
-		OnCommand=cmd(cropright,1;sleep,.5;linear,.38;cropright,0);
-		OffCommand=OffCommand;
-	};
-	LoadFont( "_zeroesthree" ) .. {
-		Text="on their website.";
-		InitCommand=cmd(zoom,.75;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+124);
-		OnCommand=cmd(cropright,1;sleep,.9;linear,.25;cropright,0);
 		OffCommand=OffCommand;
 	};
 }
