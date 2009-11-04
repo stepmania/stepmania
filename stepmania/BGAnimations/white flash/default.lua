65b3a24080b6d4792a020927e8b75d3c0da38ab2
Chris Danford <chrisdanford@users.sourceforge.net>
2009-11-04 02:32:49 +0000

    add flashes

---
+++ stepmania/BGAnimations/white flash/default.lua
@@ -0,0 +1,5 @@
+return Def.Quad
+{
+	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH;zoomtoheight,SCREEN_HEIGHT);
+	GainFocusCommand=cmd(diffusealpha,1.0;accelerate,0.6;diffusealpha,0);
+}
