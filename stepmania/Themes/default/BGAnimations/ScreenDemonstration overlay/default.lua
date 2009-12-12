f1208f3c93fd13a555527ff99fd29c2894f0aefa
AJ Kelly <aj187@ajworld.net>
2009-12-12 18:02:14 +0000

    remove Demonstration graphic in favor of a BitmapText

--- stepmania/Themes/default/BGAnimations/ScreenDemonstration overlay/default.lua
+++ stepmania/Themes/default/BGAnimations/ScreenDemonstration overlay/default.lua
@@ -1,9 +1,9 @@
 local t = Def.ActorFrame {
-	LoadActor("../_black.png") .. {
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH;zoomtoheight,120;diffusealpha,0.7);
+	Def.Quad{
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomto,SCREEN_WIDTH,120;diffuse,color("0,0,0,0.7"));
 	};
-	LoadActor("demonstration.png") .. {
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;diffuseblink;effectperiod,1);
+	LoadFont("_terminator two 40px") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;shadowlength,0;strokecolor,color("#00006688");settext,"DEMONSTRATION";diffuseshift;effectperiod,1;effectcolor1,color("#A38B00");effectcolor2,color("#F3CB00"););
 	};
 };
 
