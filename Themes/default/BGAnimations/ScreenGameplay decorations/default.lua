af956dd4e7df7179cb64473799abd5349007cbfe
AJ Kelly <aj187@ajworld.net>
2010-01-28 13:18:42 -0600

    Theme changes: * Disable Left+Right sorting on ScreenSelectCourse [AJ] * more fun stopbar [Midiman]

--- Themes/default/BGAnimations/ScreenGameplay decorations/default.lua
+++ Themes/default/BGAnimations/ScreenGameplay decorations/default.lua
@@ -27,25 +27,50 @@ local function CreateStops()
 				local secs = data[2];
 				local beatTime = song:GetElapsedTimeFromBeat(beat);
 
-				stopFrame[#stopFrame+1] = Def.Quad {
-					InitCommand=function(self)
-						--self:diffuse(HSVA(192,1,0.8,0.8));
-						self:shadowlength(0);
-						self:shadowcolor( color("#FFFFFF77") );
-						-- set width
-						self:zoomto( math.max((secs/songLen)*fFrameWidth, 1), fFrameHeight );
-						-- find location
-						self:x( ( scale(beatTime, firstBeatSecs,lastBeatSecs, -fFrameWidth/2,fFrameWidth/2) ) );
-					end;
-					OnCommand=function(self)
-						self:diffuse(Color("White"));
-						self:sleep(beatTime-5);
-						self:linear(1);
-						self:diffuse(Color("Orange"));
-						self:sleep(4);
-						self:linear(2);
-						self:diffusealpha(0);
-					end;
+				stopFrame[#stopFrame+1] = Def.ActorFrame {
+					Def.Quad {
+						InitCommand=function(self)
+							--self:diffuse(HSVA(192,1,0.8,0.8));
+							self:shadowlength(0);
+							self:shadowcolor( color("#FFFFFF77") );
+							-- set width
+							self:zoomto( math.max((secs/songLen)*fFrameWidth, 1), fFrameHeight );
+							-- find location
+							self:x( ( scale(beatTime, firstBeatSecs,lastBeatSecs, -fFrameWidth/2,fFrameWidth/2) ) );
+						end;
+						OnCommand=function(self)
+							self:diffuse(Color("White"));
+							self:sleep(beatTime+1);
+							self:linear(2);
+							self:diffusealpha(0);
+						end;
+					};
+					Def.Quad {
+						InitCommand=function(self)
+							--self:diffuse(HSVA(192,1,0.8,0.8));
+							self:shadowlength(0);
+							self:shadowcolor( color("#FFFFFF77") );
+							-- set width
+							self:zoomto( math.max((secs/songLen)*fFrameWidth, 1), fFrameHeight );
+							-- find location
+							self:x( ( scale(beatTime, firstBeatSecs,lastBeatSecs, -fFrameWidth/2,fFrameWidth/2) ) );
+						end;
+						OnCommand=function(self)
+							self:diffusealpha(1);
+							self:diffuseshift();
+							self:effectcolor1(Color("Orange"));
+							self:effectcolor2(Color("Red"));
+							self:effectclock('beat');
+							self:effectperiod(1/8);
+							--
+							self:diffusealpha(0);
+							self:sleep(beatTime+1);
+	-- 						self:linear(1);
+							self:diffusealpha(1);
+							self:linear(4);
+							self:diffusealpha(0);
+						end;
+					};
 					--OnCommand=cmd(diffuse,Color("White");sleep,math.min(0.00001+(secs-5),0.00001);linear,1;diffuse,Color("Orange");sleep,4;linear,2;diffusealpha,0);
 					--OnCommand=cmd(diffuse,Color("White");linear,1;diffuse,Color("Orange"););
 				};
@@ -71,14 +96,14 @@ for pn in ivalues(PlayerNumber) do
 				ActorUtil.LoadAllCommandsAndSetXY(self,MetricsName); 
 			end;
 		};
-		CreateStops();
 		Def.SongMeterDisplay {
 			StreamWidth=THEME:GetMetric( MetricsName, 'StreamWidth' );
 			Stream=LoadActor( THEME:GetPathG( 'SongMeterDisplay', 'stream ' .. PlayerNumberToString(pn) ) )..{
-				InitCommand=cmd(diffusealpha,0.5;blend,Blend.Add;);
+				InitCommand=cmd(diffuse,PlayerColor(pn);diffusealpha,0.5;blend,Blend.Add;);
 			};
-			Tip=LoadActor( THEME:GetPathG( 'SongMeterDisplay', 'tip ' .. PlayerNumberToString(pn) ) );
+			Tip=LoadActor( THEME:GetPathG( 'SongMeterDisplay', 'tip ' .. PlayerNumberToString(pn) ) ) .. { InitCommand=cmd(visible,false); };
 		};
+		CreateStops();
 	};
 end
 
