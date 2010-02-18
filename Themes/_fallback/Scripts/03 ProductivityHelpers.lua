f9bfc80fa99910fbe56cf6c2f0b6f4570a02f37f
AJ Kelly <aj187@ajworld.net>
2010-02-17 20:48:30 -0600

    restore some stuff I had nuked; whoops. sorry wolfman.

--- Themes/_fallback/Scripts/03 ProductivityHelpers.lua
+++ Themes/_fallback/Scripts/03 ProductivityHelpers.lua
@@ -124,7 +124,7 @@ function pname(pn) return ToEnumShortString(pn); end;
 
 -- from http://ardoris.wordpress.com/2008/11/07/rounding-to-a-certain-number-of-decimal-places-in-lua/
 function round(what, precision)
-   return math.floor(what*math.pow(10,precision)+0.5) / math.pow(10,precision)
+	return math.floor(what*math.pow(10,precision)+0.5) / math.pow(10,precision)
 end
 
 --[[ end helper functions ]]
@@ -167,4 +167,121 @@ function ComboMaintain()
 		para = "TapNoteScore_W4",
 	};
 	return Maintain[sGame]
+end;
+
+function ComboPerRow()
+	sGame = GAMESTATE:GetCurrentGame():GetName();
+	if sGame == "pump" then
+		return true;
+	elseif GAMESTATE:GetPlayMode() == "PlayMode_Oni" then
+		return true;
+	else return false;
+	end;
+end;
+
+function HitCombo()
+	sGame = GAMESTATE:GetCurrentGame():GetName();
+	local Combo = {
+		dance = 2,
+		pump = 4,
+		beat = 2,
+		kb7 = 2,
+		para = 2,
+	};
+	return Combo[sGame]
+end;
+
+function MissCombo()
+	sGame = GAMESTATE:GetCurrentGame():GetName();
+	local Combo = {
+		dance = 2,
+		pump = 4,
+		beat = 0,
+		kb7 = 0,
+		para = 0,
+	};
+	return Combo[sGame]
+end;
+
+function FailCombo() -- The combo that causes game failure.
+	sGame = GAMESTATE:GetCurrentGame():GetName();
+	local Combo = {
+		dance = 30, -- ITG/Pump Pro does it this way.
+		pump = 51,
+		beat = -1,
+		kb7 = -1,
+		para = -1,
+	};
+	return Combo[sGame]
+end;
+
+-- todo: use tables for some of these -aj
+function HoldTiming()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return 0;
+	else return PREFSMAN:GetPreference("TimingWindowSecondsHold");
+	end;
+end;
+
+function HoldJudgmentFail()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return cmd();
+	else return cmd(finishtweening;shadowlength,0;diffusealpha,1;zoom,1;y,-10;linear,0.8;y,10;sleep,0.5;linear,0.1;zoomy,0.5;zoomx,2;diffusealpha,0);
+	end;
+end;
+
+function HoldJudgmentPass()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return cmd();
+	else return cmd(finishtweening;shadowlength,0;diffusealpha,1;zoom,1.25;linear,0.3;zoomx,1;zoomy,1;sleep,0.5;linear,0.1;zoomy,0.5;zoomx,2;diffusealpha,0);
+	end;
+end;
+
+function HoldHeadStep()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return false;
+	else return true;
+	end;
+end;
+
+function InitialHoldLife()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return 0.05;
+	else return 1;
+	end;
+end;
+
+function MaxHoldLife()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return 0.05;
+	else return 1;
+	end;
+end;
+
+function ImmediateHoldLetGo()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return false;
+	else return true;
+	end;
+end;
+
+function RollBodyIncrementsCombo()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return false;
+	else return true;
+	end;
+end;
+
+function CheckpointsTapsSeparateJudgment()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return false;
+	else return true;
+	end;
+end;
+
+function ScoreMissedHoldsAndRolls()
+	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
+		return false;
+	else return true;
+	end;
 end;
