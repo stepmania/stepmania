74fb6e58c03107df07cf0085ec46f0073ee41e32
AJ Kelly <aj187@ajworld.net>
2010-04-21 20:15:29 -0500

    Fix sizing of DeviceList and InputList

--- Themes/_fallback/BGAnimations/ScreenTestInput underlay.lua
+++ Themes/_fallback/BGAnimations/ScreenTestInput underlay.lua
@@ -4,11 +4,11 @@ return Def.ActorFrame {
 	--};
 	Def.DeviceList {
 		Font="Common normal";
-		InitCommand=cmd(x,SCREEN_LEFT+20;y,SCREEN_TOP+80;zoom,1.4;horizalign,left);
+		InitCommand=cmd(x,SCREEN_LEFT+20;y,SCREEN_TOP+80;zoom,0.8;halign,0);
 	};
 
 	Def.InputList {
 		Font="Common normal";
-		InitCommand=cmd(x,SCREEN_CENTER_X-250;y,SCREEN_CENTER_Y;zoom,1.4;horizalign,left;vertspacing,8);
+		InitCommand=cmd(x,SCREEN_CENTER_X-250;y,SCREEN_CENTER_Y;zoom,1;halign,0;vertspacing,8);
 	};
 };
