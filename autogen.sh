11ec325bb986d25b901cf65b8649bc7e2f02bc70
AJ Kelly <aj187@ajworld.net>
2010-05-23 10:39:28 -0500

    I hope you guys realize that if the linux version fucks up again I am going to get a heart attack and quit sm-ssc

--- autogen.sh
+++ autogen.sh
@@ -39,7 +39,7 @@ DIE=0
 }
 
 # Try automake-1.7 and up.
-if automake-1.11 --versions > /dev/null 2>&1; then
+if automake-1.11 --version > /dev/null 2>&1; then
 	ACLOCAL=aclocal-1.11
 	AUTOMAKE=automake-1.11
 elif automake-1.10 --version > /dev/null 2>&1; then
@@ -100,3 +100,4 @@ $AUTOCONF
 $AUTOHEADER
 $AUTOMAKE $AUTOMAKE_OPTIONS
 
+<p style="color:red">SYSTEM WARNING: Cannot modify header information - headers already sent by (output started at /home3/ajworldn/public_html/ssc/sm-ssc/bugtracker/file_download.php:172)</p>
