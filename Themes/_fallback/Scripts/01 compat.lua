d463d4e4f52bb1263f9bb5c27932babc8eeed839
AJ Kelly <aj187@ajworld.net>
2010-05-26 00:21:38 -0500

    script update;

--- Themes/_fallback/Scripts/01 compat.lua
+++ Themes/_fallback/Scripts/01 compat.lua
@@ -1,6 +1,6 @@
 --[[ sm-ssc compatibility helpers
 sm-ssc changes quite a few things which would make various SM4 content break.
-Also, certain things are deprecated/removed from sm-ssc.
+Also, certain things are deprecated/removed from sm-ssc (and sometimes SM4 too).
 --]]
 
 --[[ Actor ]]
@@ -8,6 +8,27 @@ function Actor:hidden(bHide)
 	Warn("hidden is deprecated, use visible instead. (used on ".. self:GetName() ..")");
 	self:visible(not bHide);
 end;
+
+-- for when horizalign and vertalign get killed:
+--[[
+function Actor:horizalign(v)
+	local values = {
+		left = 0;
+		center = 0.5;
+		right = 1;
+	};
+	self:halign(values[v]);
+end;
+
+function Actor:vertalign(v)
+	local values = {
+		top = 0;
+		middle = 0.5;
+		bottom = 1;
+	};
+	self:valign(values[v]);
+end;
+--]]
 
 --[[ ActorScroller: all of these got renamed, so alias the lowercase ones if
 things are going to look for them. ]]
@@ -38,3 +59,23 @@ end;
 -- renaming various StepMania functions to sm-ssc ones:
 if ScreenString then ScreenString = Screen.String; end;
 if ScreenMetric then ScreenMetric = Screen.Metric; end;
+
+-- SM4SVN r28330: "add IsFullCombo bindings" - Chris Danford
+-- Unfortunately we already bound FullComboOfScore(tns), meaning we're not
+-- going to go back and redefine bindings that match it. Alias them instead
+-- for any themes expecting to use them:
+function PlayerStageStats:IsFullComboW1()
+	return self:FullComboOfScore('TapNoteScore_W1');
+end;
+
+function PlayerStageStats:IsFullComboW2()
+	return self:FullComboOfScore('TapNoteScore_W2');
+end;
+
+function PlayerStageStats:IsFullComboW3()
+	return self:FullComboOfScore('TapNoteScore_W3');
+end;
+
+function PlayerStageStats:IsFullComboW4()
+	return self:FullComboOfScore('TapNoteScore_W4');
+end;
--- Themes/_fallback/Scripts/03 Gameplay.lua
+++ Themes/_fallback/Scripts/03 Gameplay.lua
@@ -123,18 +123,8 @@ function HoldTiming()
 	end;
 end;
 
-function HoldJudgmentLetGo()
-	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
-		return cmd(visible,false);
-	else return cmd(finishtweening;shadowlength,0;diffusealpha,1;zoom,1;y,-10;linear,0.8;y,10;sleep,0.5;linear,0.1;zoomy,0.5;zoomx,2;diffusealpha,0);
-	end;
-end;
-
-function HoldJudgmentHeld()
-	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
-		return cmd(visible,false);
-	else return cmd(finishtweening;shadowlength,0;diffusealpha,1;zoom,1.25;linear,0.3;zoomx,1;zoomy,1;sleep,0.5;linear,0.1;zoomy,0.5;zoomx,2;diffusealpha,0);
-	end;
+function ShowHoldJudgments()
+	return not GAMESTATE:GetCurrentGame():GetName() == "pump";
 end;
 
 function HoldHeadStep()
