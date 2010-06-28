270f7a8ac6a25cbbff490bf379c392ca5dbe1288
AJ Kelly <aj187@ajworld.net>
2010-06-27 20:37:48 -0500

    noteskin fix hopefully

---
+++ NoteSkins/kb7/default/Key Receptor.lua
@@ -0,0 +1,13 @@
+local t = Def.ActorFrame {
+	LoadActor( "Key Go Receptor" )..{
+		InitCommand=NOTESKIN:GetMetricA('ReceptorArrow', 'InitCommand');
+		NoneCommand=NOTESKIN:GetMetricA('ReceptorArrow', 'NoneCommand');
+	};
+	LoadActor( "Key Go Receptor" )..{
+		InitCommand=NOTESKIN:GetMetricA('ReceptorOverlay', 'InitCommand');
+		PressCommand=NOTESKIN:GetMetricA('ReceptorOverlay', 'PressCommand');
+		LiftCommand=NOTESKIN:GetMetricA('ReceptorOverlay', 'LiftCommand');
+		NoneCommand=NOTESKIN:GetMetricA('ReceptorArrow', 'NoneCommand');
+	};
+};
+return t;
