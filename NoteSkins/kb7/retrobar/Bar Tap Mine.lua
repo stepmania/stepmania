8f1f52d494eea34e6c990cf6f74b3611f6fa9156
AJ Kelly <aj187@ajworld.net>
2010-06-28 20:05:29 -0500

    add retrobar for dance and kb7, remove kb7's default-p2

---
+++ NoteSkins/kb7/retrobar/Bar Tap Mine.lua
@@ -0,0 +1,15 @@
+return Def.Sprite {
+	Texture=NOTESKIN:GetPath( '_bar', 'mine' );
+	Frame0000=0;
+	Delay0000=1;
+	Frame0001=1;
+	Delay0001=1;
+	Frame0002=2;
+	Delay0002=1;
+	Frame0003=3;
+	Delay0003=1;
+	Frame0004=2;
+	Delay0004=1;
+	Frame0005=1;
+	Delay0005=1;
+};
---
+++ NoteSkins/kb7/retrobar/Bar Tap Note.lua
@@ -0,0 +1,4 @@
+return Def.Sprite {
+	Texture=NOTESKIN:GetPath( '_bar', 'tap note' );
+	Frames = Sprite.LinearFrames( 4, 1 );
+};
