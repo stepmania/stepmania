7ae0b828166c91c2ee365bf11f2df081f6534460
AJ Kelly <aj187@ajworld.net>
2010-07-18 19:05:54 -0500

    ScreenMiniMenu changes in default theme

--- Themes/default/BGAnimations/ScreenMiniMenu underlay/default.lua
+++ Themes/default/BGAnimations/ScreenMiniMenu underlay/default.lua
@@ -1,5 +1,5 @@
 return Def.ActorFrame {
 	Def.Quad{
-		InitCommand=cmd(FullScreen;diffuse,color("0,0,0,0.5"));
+		InitCommand=cmd(scaletocover,-SCREEN_WIDTH*2,SCREEN_TOP,SCREEN_WIDTH*2,SCREEN_BOTTOM;diffuse,color("0,0,0,0.5"));
 	};
 };
