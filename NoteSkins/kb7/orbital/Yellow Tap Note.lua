f098406b39857b5ffb71224dc60be8e5aae4fc07
AJ Kelly <aj187@ajworld.net>
2010-07-21 21:09:57 -0500

    shakesoda's orbital noteskin for kb7

---
+++ NoteSkins/kb7/orbital/Yellow Tap Note.lua
@@ -0,0 +1,6 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('','model/note-wide.txt');
+	Materials=NOTESKIN:GetPath('','model/yellow.txt');
+	Bones=NOTESKIN:GetPath('','model/note-wide.txt');
+};
+return t;
