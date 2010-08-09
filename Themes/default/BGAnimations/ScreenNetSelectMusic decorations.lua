7d84442f8549ebb9c486c4cdb15ac9bf5ccecabd
AJ Kelly <aj187@ajworld.net>
2010-08-09 18:28:32 -0500

    more online related changes from midi

--- Themes/default/BGAnimations/ScreenNetSelectMusic decorations.lua
+++ Themes/default/BGAnimations/ScreenNetSelectMusic decorations.lua
@@ -1,5 +1,5 @@
 local t = LoadFallbackB();
 
 t[#t+1] = StandardDecorationFromFileOptional("BPMLabel","BPMLabel");
-
+t[#t+1] = StandardDecorationFromFileOptional("DifficultyDisplay","DifficultyDisplay");
 return t;
