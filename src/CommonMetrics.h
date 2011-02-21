b095069d7f252b8a27175c5497fe03c8f2935e1c
AJ Kelly <aj187@ajworld.net>
2011-02-21 03:50:52 -0600

    doxygen

--- src/CommonMetrics.h
+++ src/CommonMetrics.h
@@ -45,23 +45,26 @@ private:
 /** @brief The common metrics that are used throughout. */
 namespace CommonMetrics
 {
-	extern ThemeMetric<RString>			FIRST_ATTRACT_SCREEN;
-	extern ThemeMetric<RString>			DEFAULT_MODIFIERS;
+	extern ThemeMetric<RString>		FIRST_ATTRACT_SCREEN;
+	extern ThemeMetric<RString>		DEFAULT_MODIFIERS;
 	/** @brief the phrase that appears on the title bar. */
 	extern LocalizedString				WINDOW_TITLE;
-	extern ThemeMetric<int>				MAX_COURSE_ENTRIES_BEFORE_VARIOUS;
+	/** @brief How many entries should be shown before showing "Various" instead. */
+	extern ThemeMetric<int>			MAX_COURSE_ENTRIES_BEFORE_VARIOUS;
+	/** @brief Adjusts the assist tick sound's playback time. */
 	extern ThemeMetric<float>			TICK_EARLY_SECONDS;
 	/** @brief the name of the default noteskin. */
-	extern ThemeMetric<RString>			DEFAULT_NOTESKIN_NAME;
+	extern ThemeMetric<RString>		DEFAULT_NOTESKIN_NAME;
 	/** @brief Which difficulties are to be shown? */
-	extern ThemeMetricDifficultiesToShow		DIFFICULTIES_TO_SHOW;
+	extern ThemeMetricDifficultiesToShow	DIFFICULTIES_TO_SHOW;
 	/** @brief Which course difficulties are to be shown? */
 	extern ThemeMetricCourseDifficultiesToShow	COURSE_DIFFICULTIES_TO_SHOW;
 	/** @brief Which step types are to be shown? */
-	extern ThemeMetricStepsTypesToShow		STEPS_TYPES_TO_SHOW;
+	extern ThemeMetricStepsTypesToShow	STEPS_TYPES_TO_SHOW;
+	/** @brief Does the player need to explicitly set a style? */
 	extern ThemeMetric<bool>			AUTO_SET_STYLE;
 	/** @brief How many decimal places are used? */
-	extern ThemeMetric<int>				PERCENT_SCORE_DECIMAL_PLACES;
+	extern ThemeMetric<int>			PERCENT_SCORE_DECIMAL_PLACES;
 
 	RString LocalizeOptionItem( const RString &s, bool bOptional );
 };
