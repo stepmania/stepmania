2a12c21daa5b9a79f0b5bfd932f66f2fed3e4cf0
AJ Kelly <aj187@ajworld.net>
2011-03-02 16:57:34 -0600

    apply Tatsh's patch from http://ssc.ajworld.net/sm-ssc/bugtracker/view.php?id=218

--- src/arch/MovieTexture/MovieTexture_FFMpeg.cpp
+++ src/arch/MovieTexture/MovieTexture_FFMpeg.cpp
@@ -685,7 +685,7 @@ int URLRageFile_read( avcodec::URLContext *h, unsigned char *buf, int size )
 	return f->Read( buf, size );
 }
 
-int URLRageFile_write( avcodec::URLContext *h, unsigned char *buf, int size )
+int URLRageFile_write( avcodec::URLContext *h, const unsigned char *buf, int size )
 {
 	RageFileBasic *f = (RageFileBasic *) h->priv_data;
 	return f->Write( buf, size );
