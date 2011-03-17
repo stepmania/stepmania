341c451fb7394d2661a95d8408146d1f68b3c44c
Jason Felds <jafelds@gmail.com>
2011-03-16 20:52:02 -0400

    Don't forget the enum!

--- src/SongOptions.h
+++ src/SongOptions.h
@@ -34,17 +34,17 @@ public:
 		NUM_AUTOSYNC_TYPES
 	};
 	AutosyncType m_AutosyncType;
-	SoundEffectType m_SoundEffectType;
-	bool m_bStaticBackground;
-	bool m_bRandomBGOnly;
-	bool m_bSaveScore;
-	bool m_bSaveReplay;
 	enum SoundEffectType {
 		SOUNDEFFECT_OFF,
 		SOUNDEFFECT_SPEED,
 		SOUNDEFFECT_PITCH,
 		NUM_SOUNDEFFECT
 	};
+	SoundEffectType m_SoundEffectType;
+	bool m_bStaticBackground;
+	bool m_bRandomBGOnly;
+	bool m_bSaveScore;
+	bool m_bSaveReplay;
 
 	/**
 	 * @brief Set up the SongOptions with reasonable defaults.
