16f46584a34f4449ef33dce8f2fde82990ddeb66
Jonathan Payne <PhosphorusGL@gmail.com>
2011-04-04 13:14:45 -0700

    3D Midi-note by Jousway. StreamHead fix, CourseFormat

---
+++ NoteSkins/dance/midi-note-3d/Down Roll Head Inactive.lua
@@ -0,0 +1,4 @@
+return Def.Sprite {
+	Texture=NOTESKIN:GetPath('Down','Roll Head Active');
+	InitCommand=cmd(diffuse,color("0.5,0.5,0.5,1"));
+};
---
+++ NoteSkins/dance/midi-note-3d/Down Tap Note.lua
@@ -0,0 +1,7 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('_down','tap note meshes');
+	Materials=NOTESKIN:GetPath('_down','tap note materials');
+	Bones=NOTESKIN:GetPath('_down','tap bones');
+};
+
+return t;
