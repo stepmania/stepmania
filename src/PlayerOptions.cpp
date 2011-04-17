445f9a5e321c5e31a4b568d19f662db6b0915c95
AJ Kelly <aj187@ajworld.net>
2011-04-16 21:10:44 -0500

    * [PlayerOptions] Added UsingReverse() Lua binding (automatically tests GetReverse() == 1) * [PlayerState] Added GetCurrentPlayerOptions() Lua binding

--- src/PlayerOptions.cpp
+++ src/PlayerOptions.cpp
@@ -908,6 +908,7 @@ public:
 
 	// Scroll
 	DEFINE_METHOD( GetReverse, m_fScrolls[PlayerOptions::SCROLL_REVERSE] )
+	DEFINE_METHOD( UsingReverse, m_fScrolls[PlayerOptions::SCROLL_REVERSE] == 1 )
 	static int GetReversePercentForColumn( T *p, lua_State *L )
 	{
 		// todo: make sure IArg is within boundaries -aj
@@ -1021,6 +1022,7 @@ public:
 
 		// Scroll
 		ADD_METHOD( GetReverse );
+		ADD_METHOD( UsingReverse );
 		ADD_METHOD( GetReversePercentForColumn );
 		ADD_METHOD( GetSplit );
 		ADD_METHOD( GetAlternate );
