0f1e2c59ed5696af568c4673ccdc7c77e1faea19
Jason Felds <jafelds@gmail.com>
2011-05-23 11:26:53 -0400

    Updated comment/TODO.

--- src/ScreenGameplay.cpp
+++ src/ScreenGameplay.cpp
@@ -1386,6 +1386,9 @@ void ScreenGameplay::PauseGame( bool bPause, GameController gc )
 // play assist ticks
 void ScreenGameplay::PlayTicks()
 {
+	/* TODO: Allow all players to have ticks. Not as simple as it looks.
+	 * If a loop takes place, it could make one player's ticks come later
+	 * than intended. Any help here would be appreciated. -Wolfman2000 */
 	Player &player = *m_vPlayerInfo[0].m_pPlayer;
 	const NoteData &nd = player.GetNoteData();
 	m_GameplayAssist.PlayTicks( nd, player.GetPlayerState() );
