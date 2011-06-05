c3e1d439c5f1088eb7ab6525aaba589a22503f6f
AJ Kelly <aj187@ajworld.net>
2011-06-05 03:51:16 -0500

    slight version info tweaks on ScreenTitleMenu

--- Themes/default/Graphics/ScreenTitleMenu VersionInfo.lua
+++ Themes/default/Graphics/ScreenTitleMenu VersionInfo.lua
@@ -1,17 +1,17 @@
 return Def.ActorFrame {
 	LoadActor(THEME:GetPathG("ScreenTitleMenu","PreferenceFrame")) .. {
-		OnCommand=cmd(diffuse,Color("Orange");diffusetopedge,Color("Yellow");diffusealpha,0.25);
+		OnCommand=cmd(diffuse,color("#f7941d");diffusetopedge,color("#fff200");diffusealpha,0.25);
 	};
 	LoadFont("Common Normal") .. {
 		Text=ProductID();
 		AltText="";
-		InitCommand=cmd(y,-3;zoom,0.75);
+		InitCommand=cmd(y,-5;zoom,0.6);
 		OnCommand=cmd(shadowlength,1);
 	};
 	LoadFont("Common Normal") .. {
 		Text=ProductVersion() .. " (".. VersionDate() ..")";
 		AltText="";
-		InitCommand=cmd(y,13;zoom,0.5);
+		InitCommand=cmd(y,8;zoom,0.45);
 		OnCommand=cmd(shadowlength,1;skewx,-0.125);
 	};
 };
