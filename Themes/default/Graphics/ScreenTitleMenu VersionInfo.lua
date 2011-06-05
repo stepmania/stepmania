7dbedc8dff100377ca154844805e87e1e18773f7
AJ Kelly <aj187@ajworld.net>
2011-06-05 03:48:07 -0500

    fix up version info on title menu

--- Themes/default/Graphics/ScreenTitleMenu VersionInfo.lua
+++ Themes/default/Graphics/ScreenTitleMenu VersionInfo.lua
@@ -5,16 +5,13 @@ return Def.ActorFrame {
 	LoadFont("Common Normal") .. {
 		Text=ProductID();
 		AltText="";
-		InitCommand=cmd(x,-72;y,-1;horizalign,left;zoom,0.75);
+		InitCommand=cmd(y,-3;zoom,0.75);
 		OnCommand=cmd(shadowlength,1);
 	};
 	LoadFont("Common Normal") .. {
-		Text=ProductVersion() .. "\n" .. VersionDate(); 
+		Text=ProductVersion() .. " (".. VersionDate() ..")";
 		AltText="";
-		InitCommand=cmd(x,72*0.35;zoom,0.5);
+		InitCommand=cmd(y,13;zoom,0.5);
 		OnCommand=cmd(shadowlength,1;skewx,-0.125);
 	};
---[[ 	LoadFont("Common Normal") .. {
-		OnCommand=cmd(settext,"You're using " ..ProductID().." "..ProductVersion().."\nBuilt on "..VersionDate().." at "..VersionTime();horizalign,right;vertalign,bottom;zoom,0.5;);
-	}; --]]
 };
