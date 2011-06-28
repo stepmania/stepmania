ce89d3c43a5807acbb5612421f49367573e2b31f
Jonathan Payne <PhosphorusGL@gmail.com>
2011-06-28 13:47:42 -0700

    [DEFAULT] Various global fixes, default songs ( see rest of log ) i317 - Installed Songs, Courses On Gameplay text i355 - midi-note, midi-solo Hold Heads fixed MEW  - 2 New Songs, Courtesy of Kommisar!! NEW  - Old SongMeterDisplay implemented for Split Timing FIX  - Remove non-working failing logic FIX  - Misc Fixes

--- Themes/default/BGAnimations/ScreenTitleMenu underlay.lua
+++ Themes/default/BGAnimations/ScreenTitleMenu underlay.lua
@@ -8,5 +8,25 @@ t[#t+1] = StandardDecorationFromFileOptional("LifeDifficulty","LifeDifficulty");
 t[#t+1] = StandardDecorationFromFileOptional("TimingDifficulty","TimingDifficulty");
 t[#t+1] = StandardDecorationFromFileOptional("NetworkStatus","NetworkStatus");
 t[#t+1] = StandardDecorationFromFileOptional("SystemDirection","SystemDirection");
+--
+t[#t+1] = StandardDecorationFromFileOptional("NumSongs","NumSongs") .. {
+	SetCommand=function(self)
+		local InstalledSongs, AdditionalSongs, InstalledCourses, AdditionalCourses, Groups, Unlocked = 0;
+		if SONGMAN:GetRandomSong() then
+			InstalledSongs, AdditionalSongs, InstalledCourses, AdditionalCourses, Groups, Unlocked =
+				SONGMAN:GetNumSongs(),
+				SONGMAN:GetNumAdditionalSongs(),
+				SONGMAN:GetNumCourses(),
+				SONGMAN:GetNumAdditionalCourses(),
+				SONGMAN:GetNumSongGroups(),
+				SONGMAN:GetNumUnlockedSongs();
+		else
+			return
+		end
+		
+		self:settextf("%i Songs (%i Groups), %i Courses", InstalledSongs, Groups, InstalledCourses);
+-- 		self:settextf("%i (+%i) Songs (%i Groups), %i (+%i) Courses", InstalledSongs, AdditionalSongs, Groups, InstalledCourses, AdditionalCourses);
+	end;
+};
 
 return t
