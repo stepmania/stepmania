92bab49a16f9f3bb3c15d6be3f4564660c9a643a
Jason Felds <jafelds@gmail.com>
2011-07-17 22:21:16 -0400

    Hurp-a-durp.
    ...never let me say that again. Cache increased.

--- src/Song.cpp
+++ src/Song.cpp
@@ -41,7 +41,7 @@
  * @brief The internal version of the cache for StepMania.
  *
  * Increment this value to invalidate the current cache. */
-const int FILE_CACHE_VERSION = 193;
+const int FILE_CACHE_VERSION = 194;
 
 /** @brief How long does a song sample last by default? */
 const float DEFAULT_MUSIC_SAMPLE_LENGTH = 12.f;
