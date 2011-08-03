5dfddcb9d528596d002913ad79907381b7dbb0fb
luizsan <luizsanq@gmail.com>
2011-08-03 19:23:10 -0300

    |:

---
+++ NoteSkins/pump/delta-routine-p2/NoteSkin.lua
@@ -0,0 +1,11 @@
+local Noteskin = ... or {}
+
+Noteskin.PartsToRotate = {
+	--["elemenu"] = true|false;
+	["Roll Head Active"] = false;
+	["Roll Head Inactive"] = false;
+	--
+	["Tap Note"] = false;
+}
+
+return Noteskin
