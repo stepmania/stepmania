4fa91adc36d87b5304b9e56272c8f1e778527cd7
AJ Kelly <aj187@ajworld.net>
2011-08-07 18:29:40 -0500

    no really fix errors this time

--- Themes/default/Graphics/ScreenEvaluation MachineRecord.lua
+++ Themes/default/Graphics/ScreenEvaluation MachineRecord.lua
@@ -7,7 +7,7 @@ local hasMachineRecord = record ~= -1
 return LoadFont("Common normal")..{
 	InitCommand=cmd(zoom,0.55;shadowlength,1;NoStroke;glowshift;effectcolor1,color("1,1,1,0");effectcolor2,color("1,1,1,0.25"));
 	BeginCommand=function(self)
-		self:visible(hasMachineRecord;);
+		self:visible(hasMachineRecord);
 		local text = string.format(THEME:GetString("ScreenEvaluation", "MachineRecord"), record+1)
 		self:settext(text);
 	end;
