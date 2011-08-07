5129c6f39682c90134a9ce6cbfefe173f9a14569
AJ Kelly <aj187@ajworld.net>
2011-08-07 00:54:21 -0500

    fix error in ScreenEvaluation MachineRecord

--- Themes/default/Graphics/ScreenEvaluation MachineRecord.lua
+++ Themes/default/Graphics/ScreenEvaluation MachineRecord.lua
@@ -5,7 +5,7 @@ local record = stats:GetMachineHighScoreIndex()
 local hasMachineRecord = record ~= -1
 
 return LoadFont("Common normal")..{
-	Text=string.format(THEME:GetString("ScreenEvaluation", "MachineRecord"), record+1)string.format(THEME:GetString( "ScreenEvaluation", "Machine Record" ), record+1)
+	Text=string.format(THEME:GetString("ScreenEvaluation", "MachineRecord"), record+1);
 	InitCommand=cmd(zoom,0.55;shadowlength,1;NoStroke;glowshift;effectcolor1,color("1,1,1,0");effectcolor2,color("1,1,1,0.25"));
 	BeginCommand=cmd(visible,hasMachineRecord;);
 };
