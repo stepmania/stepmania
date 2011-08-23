a0baaeeeb26fc44b36cd193491d40031a655c30a
Mark Cannon <vyhdycokio@gmail.com>
2011-08-23 13:21:53 -0700

    [br:LuaDriver] pre-emptive fix for messages without params causing segfault

--- src/arch/LuaDriver/LuaDriver_Peripheral.cpp
+++ src/arch/LuaDriver/LuaDriver_Peripheral.cpp
@@ -102,7 +102,14 @@ void LuaDriver_Peripheral::HandleMessage( const Message &msg )
 
 	pFunc->PushSelf( L ); // function
 	m_pDriver->PushSelf( L ); // argument 1
-	msg.GetParamTable().PushSelf( L ); // argument 2
+
+	const LuaReference *params  = msg.GetParamTable();
+
+	// argument 2
+	if( params )
+		params->PushSelf( L );
+	else
+		lua_pushnil( L );
 
 	RString sError;
 	LuaHelpers::RunScriptOnStack(L, sError, 2, 0); // 2 args, 0 results
