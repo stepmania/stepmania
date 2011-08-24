c238cfe6986f0d22b2ce8af6cc17b33301827acf
AJ Kelly <aj187@ajworld.net>
2011-08-24 13:07:15 -0500

    fix some theme issues

--- Themes/default/Graphics/ScreenGameplay DeltaSeconds.lua
+++ Themes/default/Graphics/ScreenGameplay DeltaSeconds.lua
@@ -6,10 +6,11 @@ local t = LoadFont("ScreenGameplay","RemainingTime") .. {
 	Text="";
 	JudgmentMessageCommand=function(self,params)
 		if params.Player == PlayerNumber then
-			if params.TapNoteScore then 
+			if params.TapNoteScore then
+				local tns = ToEnumShortString(params.TapNoteScore)
 				self:playcommand( "GainSeconds" );
-				self:playcommand( ToEnumShortString( params.TapNoteScore ) );
-				self:settextf( "%+1.1fs", PREFSMAN:GetPreference( string.format("TimeMeterSecondsChange%s", ToEnumShortString( params.TapNoteScore ) ) ) );
+				self:playcommand( tns );
+				self:settextf( "%+1.1fs", PREFSMAN:GetPreference( string.format("TimeMeterSecondsChange%s", tns) ) );
 			else
 				return
 			end
