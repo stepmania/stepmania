8a490a81b1cbe08edf0f5f33492e6b5fdd1cd6b6
AJ Kelly <aj187@ajworld.net>
2011-09-07 22:04:17 -0500

    update logo

--- Themes/default/Graphics/ScreenTitleMenu logo/default.lua
+++ Themes/default/Graphics/ScreenTitleMenu logo/default.lua
@@ -1,8 +1,15 @@
--- comments show how this compares to a StepMania 3.9 BGAnimation.ini.
--- It should be noted that additional commands on the ActorFrame as a whole for
--- this file are in metrics.ini ([ScreenTitleMenu] ShowLogo, etc.)
-local t = Def.ActorFrame{ -- [BGAnimation]
-	LoadActor("_logo"); -- [Layer1] Type=0 File=_ball.png
+local t = Def.ActorFrame{
+	InitCommand=cmd(fov,70);
+	LoadActor("_arrow")..{
+		InitCommand=cmd(x,225;);
+		OnCommand=cmd(wag;effectmagnitude,0,0,16;effectperiod,2.5;);
+	};
+	LoadActor("_text");
+	LoadActor("_text")..{
+		Name="TextGlow";
+		InitCommand=cmd(blend,Blend.Add;diffusealpha,0.05;);
+		OnCommand=cmd(glowshift;effectperiod,2.5;effectcolor1,color("1,1,1,0.25");effectcolor2,color("1,1,1,1"););
+	};
 };
 
 return t;
