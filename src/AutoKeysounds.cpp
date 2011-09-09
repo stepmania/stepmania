stepmania/stepmania

commit d17c21e091556aa30ccc3436e586b0e0f5861bb9

Moved the unused RageSoundReader_Silence from RageSound.cpp to RageSoundReader.h and use it in AutoKeysounds.cpp. Hopefully this should fix Bug 0000500, but I don't know, can't reproduce.

src/AutoKeysounds.cpp
@@ -19,6 +19,7 @@
 #include "AutoKeysounds.h"
 #include "GameState.h"
 #include "Song.h"
+#include "RageSoundReader.h"
 #include "RageSoundReader_Chain.h"
 #include "RageSoundReader_ChannelSplit.h"
 #include "RageSoundReader_Extend.h"
@@ -276,6 +277,8 @@ void AutoKeysounds::FinishLoading()
 		else
 		{
 			delete pChain;
+			RageSoundReader_Silence *pSilence = new RageSoundReader_Silence;
+			m_pSharedSound = pSilence;
 		}
 	}
 	ASSERT_M( m_pSharedSound, ssprintf("No keysounds were loaded for the song %s!", pSong->m_sMainTitle.c_str() ));

src/RageSound.cpp
@@ -133,21 +133,6 @@ bool RageSound::IsLoaded() const
 	return m_pSource != NULL;
 }
 
-class RageSoundReader_Silence: public RageSoundReader
-{
-public:
-	int GetLength() const { return 0; }
-	int GetLength_Fast() const { return 0; }
-	int SetPosition( int iFrame )  { return 1; }
-	int Read( float *pBuf, int iFrames ) { return RageSoundReader::END_OF_FILE; }
-	RageSoundReader *Copy() const { return new RageSoundReader_Silence; }
-	int GetSampleRate() const { return 44100; }
-	unsigned GetNumChannels() const { return 1; }
-	int GetNextSourceFrame() const { return 0; }
-	float GetStreamToSourceRatio() const { return 1.0f; }
-	RString GetError() const { return ""; }
-};
-
 
 bool RageSound::Load( RString sSoundFilePath )
 {

src/RageSoundReader.h
@@ -43,6 +43,22 @@ public:
 	int RetriedRead( float *pBuffer, int iFrames, int *iSourceFrame = NULL, float *fRate = NULL );
 };
 
+
+class RageSoundReader_Silence: public RageSoundReader
+{
+public:
+	int GetLength() const { return 0; }
+	int GetLength_Fast() const { return 0; }
+	int SetPosition( int iFrame )  { return 1; }
+	int Read( float *pBuf, int iFrames ) { return RageSoundReader::END_OF_FILE; }
+	RageSoundReader *Copy() const { return new RageSoundReader_Silence; }
+	int GetSampleRate() const { return 44100; }
+	unsigned GetNumChannels() const { return 1; }
+	int GetNextSourceFrame() const { return 0; }
+	float GetStreamToSourceRatio() const { return 1.0f; }
+	RString GetError() const { return ""; }
+};
+
 #endif
 
 /*

commit e56de5a09aa334d20e9f028ff17b1fe66e7dcacf

undo regression introduced in d17c21e where any songs without keysounds were clobbered by RageSoundReader_Silence. please test your code before committing it :|

src/AutoKeysounds.cpp
@@ -19,7 +19,6 @@
 #include "AutoKeysounds.h"
 #include "GameState.h"
 #include "Song.h"
-#include "RageSoundReader.h"
 #include "RageSoundReader_Chain.h"
 #include "RageSoundReader_ChannelSplit.h"
 #include "RageSoundReader_Extend.h"
@@ -277,8 +276,6 @@ void AutoKeysounds::FinishLoading()
 		else
 		{
 			delete pChain;
-			RageSoundReader_Silence *pSilence = new RageSoundReader_Silence;
-			m_pSharedSound = pSilence;
 		}
 	}
 	ASSERT_M( m_pSharedSound, ssprintf("No keysounds were loaded for the song %s!", pSong->m_sMainTitle.c_str() ));

src/RageSound.cpp
@@ -133,6 +133,21 @@ bool RageSound::IsLoaded() const
 	return m_pSource != NULL;
 }
 
+class RageSoundReader_Silence: public RageSoundReader
+{
+public:
+	int GetLength() const { return 0; }
+	int GetLength_Fast() const { return 0; }
+	int SetPosition( int iFrame )  { return 1; }
+	int Read( float *pBuf, int iFrames ) { return RageSoundReader::END_OF_FILE; }
+	RageSoundReader *Copy() const { return new RageSoundReader_Silence; }
+	int GetSampleRate() const { return 44100; }
+	unsigned GetNumChannels() const { return 1; }
+	int GetNextSourceFrame() const { return 0; }
+	float GetStreamToSourceRatio() const { return 1.0f; }
+	RString GetError() const { return ""; }
+};
+
 
 bool RageSound::Load( RString sSoundFilePath )
 {

src/RageSoundReader.h
@@ -43,22 +43,6 @@ public:
 	int RetriedRead( float *pBuffer, int iFrames, int *iSourceFrame = NULL, float *fRate = NULL );
 };
 
-
-class RageSoundReader_Silence: public RageSoundReader
-{
-public:
-	int GetLength() const { return 0; }
-	int GetLength_Fast() const { return 0; }
-	int SetPosition( int iFrame )  { return 1; }
-	int Read( float *pBuf, int iFrames ) { return RageSoundReader::END_OF_FILE; }
-	RageSoundReader *Copy() const { return new RageSoundReader_Silence; }
-	int GetSampleRate() const { return 44100; }
-	unsigned GetNumChannels() const { return 1; }
-	int GetNextSourceFrame() const { return 0; }
-	float GetStreamToSourceRatio() const { return 1.0f; }
-	RString GetError() const { return ""; }
-};
-
 #endif
 
 /*