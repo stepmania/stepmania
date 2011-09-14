d285c80a72e6403a3a89255e3be32dc12722493e
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2011-09-14 23:48:24 +0700

    Some optimizations with the scroll segments.
    Scroll segments seem to cause the lag when there's a lot of segment
    in the steps, say, 165. On my Mac, it drops the frame-rate down by about
    30 FPS in the end.
    
    This is because the way the the game tries to determine the exact position
    of the note, it has to go through all the scroll segments from the
    beginning until it finds the currently active scroll segment and keep
    track of the beat to display instead.
    
    And each frame, the game needs to determine the position for each note,
    and also when figuring out the first beat and last beat to display. This
    makes the game slow even with just 165 scroll segments.
    
    The approach is that at every game update, we create an optimized data
    structure that can calculate the displayed beat from real song beat
    in O(log n) time. So in this approach we iterate through EVERY scroll
    segments each frame instead of iterating through the scroll segments
    for each note for each frame. This eliminates the lag on this side.
    
    The data structure is recomputed each update because I assume that the
    timing segments can change at any time, but not during draws.
    
    A similar problem is: when using Cmods, GetElapsedTimeFromBeat()
    is also called very often (say, for each note and also in FindFirst/Last
    DisplayedBeat), we may as well have to make these queries faster for songs
    that has hundreds of BPM changes or stops or delays to prevent the game
    from lagging.

--- src/Player.cpp
+++ src/Player.cpp
@@ -751,6 +751,27 @@ void Player::SendComboMessages( int iOldCombo, int iOldMissCombo )
 	}
 }
 
+static void GenerateCacheDataStructure(PlayerState *pPlayerState, NoteData &notes) {
+	
+	pPlayerState->m_CacheDisplayedBeat.clear();
+	
+	const vector<TimingSegment *> *segs = pPlayerState->GetDisplayedTiming().m_avpTimingSegments;
+	
+	float displayedBeat = 0.0f;
+	float lastRealBeat = 0.0f;
+	float lastRatio = 1.0f;
+	for ( unsigned i = 0; i < segs[SEGMENT_SCROLL].size(); i++ )
+	{
+		ScrollSegment *seg = static_cast<ScrollSegment *>(segs[SEGMENT_SCROLL][i]);
+		displayedBeat += ( seg->GetBeat() - lastRealBeat ) * lastRatio;
+		lastRealBeat = seg->GetBeat();
+		lastRatio = seg->GetRatio();
+		CacheDisplayedBeat c = { seg->GetBeat(), displayedBeat, seg->GetRatio() };
+		pPlayerState->m_CacheDisplayedBeat.push_back( c );
+	}
+	
+}
+
 void Player::Update( float fDeltaTime )
 {
 	const RageTimer now;
@@ -879,6 +900,9 @@ void Player::Update( float fDeltaTime )
 	// during pause.
 	if( m_bPaused )
 		return;
+
+	// Generate some cache data structure.
+	GenerateCacheDataStructure(m_pPlayerState, m_NoteData);
 
 	// Check for a strum miss
 	if( m_pPlayerState->m_fLastStrumMusicSeconds != -1  &&
