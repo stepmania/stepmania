594eaa08073a44272a0c571265fd3b3043dea451
Jousway <Zettbou@gmail.com>
2011-11-27 21:25:42 +0100

    And adding the new one, Also adding Midi-Vivid-3d. One little note tough, Midiman said he is making a new midi-note, So I might update these with Midiman's edits in the future.

---
+++ NoteSkins/dance/midi-note-3d/Down Tap Note.lua
@@ -0,0 +1,7 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('_Down','Tap Note');
+	Materials=NOTESKIN:GetPath('_Down','Tap Note');
+	Bones=NOTESKIN:GetPath('_Down','Tap Note');
+};
+
+return t;
---
+++ NoteSkins/dance/midi-note-3d/NoteSkin.lua
@@ -0,0 +1,138 @@
+-- Haggen Daze
+
+local ret = ... or {};
+
+ret.RedirTable =
+{
+	Up = "Down",
+	Down = "Down",
+	Left = "Down",
+	Right = "Down",
+	UpLeft = "Down",
+	UpRight = "Down",
+};
+
+local OldRedir = ret.Redir;
+ret.Redir = function(sButton, sElement)
+	-- sButton, sElement = OldRedir(sButton, sElement);
+
+	-- Instead of separate hold heads, use the tap note graphics.
+	if sElement == "Hold Head Active" or
+	   sElement == "Roll Head Active"
+	then
+		sElement = 
+			(GAMESTATE:GetCurrentStyle():GetStyleType() == "StyleType_TwoPlayersSharedSides")
+			and "Tap Note" or "Hold Head Active";
+	end
+	
+	-- Test
+	if sElement == "Hold Head Inactive" or
+	   sElement == "Roll Head Inactive"
+	then
+		sElement =
+			(GAMESTATE:GetCurrentStyle():GetStyleType() == "StyleType_TwoPlayersSharedSides")
+			and "Tap Note" or "Hold Head Inactive";
+	end
+	
+	if sElement == "Tap Fake"
+	then
+		sElement = "Tap Note";
+	end
+	
+	sButton = ret.RedirTable[sButton];
+
+	return sButton, sElement;
+end
+
+-- local OldRedir = ret.Redir;
+-- ret.Redir = function(sButton, sElement)
+	-- sButton = ret.RedirTable[sButton];
+	-- return sButton, sElement;
+-- end
+
+-- To have separate graphics for each hold part:
+local OldRedir = ret.Redir;
+ret.Redir = function(sButton, sElement)
+	-- Redirect non-hold, non-roll parts.
+	if string.find(sElement, "hold") or string.find(sElement, "roll") then
+		return sButton, sElement;
+	end
+	return OldRedir(sButton, sElement);
+end
+
+--[[
+local OldFunc = ret.Load;
+function ret.Load()
+	local t = OldFunc();
+
+	-- The main "Explosion" part just loads other actors; don't rotate
+	-- it.  The "Hold Explosion" part should not be rotated.
+	if Var "Element" == "Explosion" or
+	   Var "Element" == "Roll Explosion" then
+		t.BaseRotationZ = nil;
+	end
+	return t;
+end
+]]
+
+local OldFunc = ret.Load;
+function ret.Load()
+	local t = OldFunc();
+
+	--Explosion should not be rotated; it calls other actors.
+	if Var "Element" == "Explosion"	then
+		t.BaseRotationZ = nil;
+	end
+	return t;
+end
+
+ret.PartsToRotate =
+{
+	["Receptor"] = true,
+	["Go Receptor"] = true,
+	["Ready Receptor"] = true,
+	["Tap Explosion Bright"] = true,
+	["Tap Explosion Dim"] = true,
+	["Tap Note"] = true,
+	["Tap Fake"] = true,
+	["Tap Lift"] = true,
+	["Hold Head Active"] = true,
+	["Hold Head Inactive"] = true,
+	["Roll Head Active"] = true,
+	["Roll Head Inactive"] = true,
+	["Hold Explosion"] = true,
+	["Roll Explosion"] = true,
+};
+ret.Rotate =
+{
+	Up = 180,
+	Down = 0,
+	Left = 90,
+	Right = -90,
+	UpLeft = 135,
+	UpRight = 225,
+};
+
+--
+-- If a derived skin wants to have separate UpLeft graphics,
+-- use this:
+--
+-- ret.RedirTable.UpLeft = "UpLeft";
+-- ret.RedirTable.UpRight = "UpLeft";
+-- ret.Rotate.UpLeft = 0;
+-- ret.Rotate.UpRight = 90;
+--
+ret.Blank =
+{
+	["Hold Topcap Active"] = true,
+	["Hold Topcap Inactive"] = true,
+	["Roll Topcap Active"] = true,
+	["Roll Topcap Inactive"] = true,
+	["Hold Tail Active"] = true,
+	["Hold Tail Inactive"] = true,
+	["Roll Tail Active"] = true,
+	["Roll Tail Inactive"] = true,
+	["Roll Explosion"] = true,
+};
+
+return ret;
