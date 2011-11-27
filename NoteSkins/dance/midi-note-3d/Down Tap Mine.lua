cd4cd80184e6070806093cb9b54dded5863daec9
Jousway <Zettbou@gmail.com>
2011-11-27 21:23:09 +0100

    removing the old midi-note-3d

--- NoteSkins/dance/midi-note-3d/Down Tap Mine.lua
+++
@@ -1,9 +0,0 @@
-local t = Def.ActorFrame {
-	Def.Sprite {
-		Texture=NOTESKIN:GetPath( '_down', 'tap mine' );
-		Frame0000=0;
-		Delay0000=1;
-		InitCommand=cmd(spin;effectclock,'beat';effectmagnitude,0,0,-180);
-	};
-};
-return t;
--- NoteSkins/dance/midi-note-3d/Up Roll BottomCap Inactive.lua
+++
@@ -1,4 +0,0 @@
-return Def.Sprite {
-	Texture=NOTESKIN:GetPath('Up','Roll BottomCap Active');
-	InitCommand=cmd(diffuse,color("0.5,0.5,0.5,0.5"));
-};
