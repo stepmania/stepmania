907e3922d1d9351fd4bfb440e4b6f1f6f809fee1
AJ Kelly <aj187@ajworld.net>
2011-12-27 20:47:04 -0600

    move widescreenhelpers stuff into utilities

--- Themes/_fallback/Scripts/02 Utilities.lua
+++ Themes/_fallback/Scripts/02 Utilities.lua
@@ -319,6 +319,21 @@ function ThemeManager:GetAbsolutePath(sPath)
   return sFinPath
 end
 
+-- supported aspect ratios
+AspectRatios = {
+	ThreeFour   = 0.75,		-- (576x760 at 1024x768; meant for rotated monitors?)
+	OneOne      = 1.0,		-- 480x480  (uses Y value of specified resolution)
+	FiveFour    = 1.25,		-- 600x480 (1280x1024 is a real use case)
+	FourThree   = 1.33333,	-- 640x480 (common)
+	SixteenTen  = 1.6,		-- 720x480 (common)
+	SixteenNine = 1.77778,	-- 853x480 (common)
+	EightThree  = 2.66666	-- 1280x480 (two monitors)
+}
+
+function WideScale(AR4_3, AR16_9)
+	return scale( SCREEN_WIDTH, 640, 854, AR4_3, AR16_9 )
+end
+
 -- (c) 2005-2011 Glenn Maynard, Chris Danford, SSC
 -- All rights reserved.
 -- 
