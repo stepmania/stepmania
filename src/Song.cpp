dd51b200a9710de5af188cf8417603e342838e38
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-01-09 22:09:11 +0700

    if( !duringCache ) should be if( !fromCache ). My bad. :(

--- src/Song.cpp
+++ src/Song.cpp
@@ -589,7 +589,7 @@ void Song::TidyUpData( bool fromCache, bool duringCache )
 
 	CHECKPOINT_M( "Looking for images..." );
     
-    if( !duringCache )
+    if( !fromCache )
     {
 
         // First, check the file name for hints.
@@ -804,7 +804,7 @@ void Song::TidyUpData( bool fromCache, bool duringCache )
 
 	// If no BGChanges are specified and there are movies in the song directory, then assume
 	// they are DWI style where the movie begins at beat 0.
-	if( !HasBGChanges() && !duringCache )
+	if( !HasBGChanges() && !fromCache )
 	{
 		vector<RString> arrayPossibleMovies;
 		GetDirListing( m_sSongDir + RString("*.ogv"), arrayPossibleMovies );
@@ -826,7 +826,7 @@ void Song::TidyUpData( bool fromCache, bool duringCache )
 	/* Don't allow multiple Steps of the same StepsType and Difficulty (except
 	 * for edits). We should be able to use difficulty names as unique
 	 * identifiers for steps. */
-	if( !duringCache )
+	if( !fromCache )
     {
         SongUtil::AdjustDuplicateSteps( this );
     }
