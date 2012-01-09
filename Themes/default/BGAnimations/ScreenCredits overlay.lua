a3e72468425ea5ffe3500ba06d3a9b07c99af446
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2012-01-09 04:28:09 +0000

    pendantic

--- Themes/default/BGAnimations/ScreenCredits overlay.lua
+++ Themes/default/BGAnimations/ScreenCredits overlay.lua
@@ -92,7 +92,7 @@ local shoutout = {
 	--Image(), -- we should have some logos probably to look super pro
 	"#KBO",
 	"Celestia Radio", -- LOVE AND TOLERANCE
-	"You showed us... your ultimate dance",
+	"You showed us... your ultimate dance!",
 }
 
 local copyright = {
