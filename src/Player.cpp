516d01037832149a993533fe734f23fd8180e94f
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2012-01-24 03:26:11 +0000

    god awful ugly commit

--- src/Player.cpp
+++ src/Player.cpp
@@ -2234,7 +2234,14 @@ void Player::StepStrumHopo( int col, int row, const RageTimer &tm, bool bHeld, b
 				if( !bRelease && ( REQUIRE_STEP_ON_MINES == !bHeld ) && 
 				   fSecondsFromExact <= GetWindowSeconds(TW_Mine) &&
 				   m_Timing->IsJudgableAtRow(iSongRow))
+      {
 					score = TNS_HitMine;
+          if( m_pPlayerStageStats )
+	{
+		SetCombo( m_pPlayerStageStats->m_iCurCombo, m_pPlayerStageStats->m_iCurMissCombo );
+		m_pPlayerStageStats->UpdateComboList( STATSMAN->m_CurStageStats.m_fStepsSeconds, false );
+	}
+}   
 				break;
 
 			case TapNote::attack:
