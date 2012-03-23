f690fee5433fffb5d3df1610e88baec550960194
AJ Kelly <aj187@ajworld.net>
2012-03-23 14:51:06 -0500

    add a graphic for items that load webpages to the Hello World reference page scroller. (the tango icons are in the public domain)

--- Themes/HelloWorld/Graphics/ScreenGuideMain scroll.lua
+++ Themes/HelloWorld/Graphics/ScreenGuideMain scroll.lua
@@ -23,14 +23,14 @@ local t = Def.ActorFrame{
 		LoadFont("_frutiger roman 24px")..{
 			Name="SectionName";
 			Text=THEME:GetString("ScreenGuideMain",itemName);
-			InitCommand=cmd(x,-250;y,-6;halign,0;diffuse,color("#222222FF"));
+			InitCommand=cmd(x,-246;y,-6;halign,0;diffuse,color("#222222FF"));
 			GainFocusCommand=cmd(decelerate,0.25;diffusealpha,1);
 			LoseFocusCommand=cmd(stoptweening;decelerate,0.25;diffusealpha,0.5);
 		};
 		LoadFont("_frutiger roman 24px")..{
 			Name="SectionDesc";
 			Text=THEME:GetString("ScreenGuideMain",itemName.."Desc");
-			InitCommand=cmd(x,-248;y,10;align,0,0;diffuse,color("#222222FF");zoom,0.5);
+			InitCommand=cmd(x,-244;y,10;align,0,0;diffuse,color("#222222FF");zoom,0.5);
 			GainFocusCommand=cmd(decelerate,0.25;diffusealpha,1);
 			LoseFocusCommand=cmd(stoptweening;decelerate,0.25;diffusealpha,0.5);
 		};
--- Themes/HelloWorld/Graphics/ScreenReferenceMain scroll.lua
+++ Themes/HelloWorld/Graphics/ScreenReferenceMain scroll.lua
@@ -1,5 +1,6 @@
 local gc = Var("GameCommand")
 local itemName = gc:GetName()
+local url = gc:GetUrl()
 
 local t = Def.ActorFrame{
 	-- focused frame
@@ -23,14 +24,20 @@ local t = Def.ActorFrame{
 		LoadFont("_frutiger roman 24px")..{
 			Name="SectionName";
 			Text=THEME:GetString("ScreenReferenceMain",itemName);
-			InitCommand=cmd(x,-250;y,-6;halign,0;diffuse,color("#222222FF"));
+			InitCommand=cmd(x,-246;y,-6;halign,0;diffuse,color("#222222FF"));
 			GainFocusCommand=cmd(decelerate,0.25;diffusealpha,1);
 			LoseFocusCommand=cmd(stoptweening;decelerate,0.25;diffusealpha,0.5);
 		};
 		LoadFont("_frutiger roman 24px")..{
 			Name="SectionDesc";
 			Text=THEME:GetString("ScreenReferenceMain",itemName.."Desc");
-			InitCommand=cmd(x,-248;y,10;align,0,0;diffuse,color("#222222FF");zoom,0.5);
+			InitCommand=cmd(x,-244;y,10;align,0,0;diffuse,color("#222222FF");zoom,0.5);
+			GainFocusCommand=cmd(decelerate,0.25;diffusealpha,1);
+			LoseFocusCommand=cmd(stoptweening;decelerate,0.25;diffusealpha,0.5);
+		};
+		LoadActor(THEME:GetPathG("_tango","webbrowser"))..{
+			Name="UrlIcon";
+			InitCommand=cmd(x,224;y,4;zoom,0.75;visible,url~="");
 			GainFocusCommand=cmd(decelerate,0.25;diffusealpha,1);
 			LoseFocusCommand=cmd(stoptweening;decelerate,0.25;diffusealpha,0.5);
 		};
