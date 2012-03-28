e71832b14e2186762b23e5d4f360b944c9e36277
AJ Kelly <aj187@ajworld.net>
2012-03-28 18:54:20 -0500

    fix up Rave gameplay out not displaying the proper results; see issue 695

--- Themes/default/BGAnimations/ScreenGameplay out.lua
+++ Themes/default/BGAnimations/ScreenGameplay out.lua
@@ -1,27 +1,50 @@
+local raveChildren
+
 local bg = Def.ActorFrame{
 	Def.Quad{
 		InitCommand=cmd(FullScreen;diffuse,color("0,0,0,0"));
 		OnCommand=cmd(linear,5;diffusealpha,1);
 	};
-};
 
-if GAMESTATE:GetPlayMode() == 'PlayMode_Rave' then
-	-- find out who won the round
-	local raveWin, raveFile;
-	if GAMESTATE:IsWinner(PLAYER_1) then
-		raveFile = THEME:GetPathG("_rave result","P1")
-	elseif GAMESTATE:IsWinner(PLAYER_2) then
-		raveFile = THEME:GetPathG("_rave result","P2")
-	else
-		raveFile = THEME:GetPathG("_rave result","draw")
-	end
+	Def.ActorFrame{
+		Name="RaveMessages";
+		InitCommand=function(self)
+			raveChildren = self:GetChildren()
+			self:visible(GAMESTATE:GetPlayMode() == 'PlayMode_Rave')
 
-	raveWin = LoadActor(raveFile)..{
-		InitCommand=cmd(Center;cropbottom,1;fadebottom,1;);
-		OnCommand=cmd(sleep,2;linear,0.5;cropbottom,0;fadebottom,0;sleep,1.75;linear,0.25;diffusealpha,0);
+			raveChildren.P1Win:visible(false)
+			raveChildren.P2Win:visible(false)
+			raveChildren.Draw:visible(false)
+		end;
+		OffCommand=function(self)
+			local p1Win = GAMESTATE:IsWinner(PLAYER_1)
+			local p2Win = GAMESTATE:IsWinner(PLAYER_2)
+
+			if GAMESTATE:IsWinner(PLAYER_1) then
+				raveChildren.P1Win:visible(true)
+			elseif GAMESTATE:IsWinner(PLAYER_2) then
+				raveChildren.P2Win:visible(true)
+			else
+				raveChildren.Draw:visible(true)
+			end
+		end;
+
+		LoadActor(THEME:GetPathG("_rave result","P1"))..{
+			Name="P1Win";
+			InitCommand=cmd(Center;cropbottom,1;fadebottom,1;);
+			OnCommand=cmd(sleep,2;linear,0.5;cropbottom,0;fadebottom,0;sleep,1.75;linear,0.25;diffusealpha,0);
+		};
+		LoadActor(THEME:GetPathG("_rave result","P2"))..{
+			Name="P2Win";
+			InitCommand=cmd(Center;cropbottom,1;fadebottom,1;);
+			OnCommand=cmd(sleep,2;linear,0.5;cropbottom,0;fadebottom,0;sleep,1.75;linear,0.25;diffusealpha,0);
+		};
+		LoadActor(THEME:GetPathG("_rave result","draw"))..{
+			Name="Draw";
+			InitCommand=cmd(Center;cropbottom,1;fadebottom,1;);
+			OnCommand=cmd(sleep,2;linear,0.5;cropbottom,0;fadebottom,0;sleep,1.75;linear,0.25;diffusealpha,0);
+		};
 	};
-
-	table.insert(bg,raveWin)
-end
+};
 
 return bg
