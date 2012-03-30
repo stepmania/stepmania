c0b4389846b8c489d1e345fce111f664569f1a9e
AJ Kelly <aj187@ajworld.net>
2012-03-30 15:31:11 -0500

    Gameplay StageDisplay now shows final stage properly

--- Themes/default/Graphics/ScreenGameplay StageDisplay.lua
+++ Themes/default/Graphics/ScreenGameplay StageDisplay.lua
@@ -1,6 +1,17 @@
 local curScreen = Var "LoadingScreen";
 local curStage = GAMESTATE:GetCurrentStage();
 local curStageIndex = GAMESTATE:GetCurrentStageIndex();
+local tRemap = {
+	Stage_1st		= 1,
+	Stage_2nd		= 2,
+	Stage_3rd		= 3,
+	Stage_4th		= 4,
+	Stage_5th		= 5,
+	Stage_6th		= 6,
+};
+if tRemap[curStage] == PREFSMAN:GetPreference("SongsPerPlay") then
+	curStage = "Stage_Final";
+end
 
 local t = Def.ActorFrame {};
 
@@ -8,23 +19,22 @@ t[#t+1] = Def.ActorFrame {
 	LoadActor(THEME:GetPathB("_frame","3x3"),"rounded black",64,16);
 	LoadFont("Common Normal") .. {
 		InitCommand=cmd(y,-1;shadowlength,1;playcommand,"Set");
-        SetCommand=function(self)
-          if GAMESTATE:GetCurrentCourse() then
-            self:settext( GAMESTATE:GetCurrentStageIndex()+1 .. " / " .. GAMESTATE:GetCurrentCourse():GetEstimatedNumStages() );
-          elseif GAMESTATE:IsEventMode() then
-            self:settextf("Stage %s", curStageIndex+1);
-          else
-            if THEME:GetMetric(curScreen,"StageDisplayUseShortString") then
-              self:settextf("%s", ToEnumShortString(curStage));
-            else
-              self:settextf("%s Stage", ToEnumShortString(curStage));
-            end;
-          end;
-          --
-          self:zoom(0.675);
-          self:diffuse(StageToColor(curStage));
-          self:diffusetopedge(ColorLightTone(StageToColor(curStage)));
-        end;
+		SetCommand=function(self)
+			if GAMESTATE:GetCurrentCourse() then
+				self:settext( GAMESTATE:GetCurrentStageIndex()+1 .. " / " .. GAMESTATE:GetCurrentCourse():GetEstimatedNumStages() );
+			elseif GAMESTATE:IsEventMode() then
+				self:settextf("Stage %s", curStageIndex+1);
+			else
+				if THEME:GetMetric(curScreen,"StageDisplayUseShortString") then
+					self:settextf("%s", ToEnumShortString(curStage));
+				else
+					self:settextf("%s Stage", ToEnumShortString(curStage));
+				end;
+			end;
+			self:zoom(0.675);
+			self:diffuse(StageToColor(curStage));
+			self:diffusetopedge(ColorLightTone(StageToColor(curStage)));
+		end;
 	};
 };
 return t
