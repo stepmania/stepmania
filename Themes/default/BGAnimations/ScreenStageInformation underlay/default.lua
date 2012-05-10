a8eca9c49c2d93c1aea763e48aff257e85338529
Jason Felds <jafelds@gmail.com>
2012-05-10 02:02:16 -0400

    The bigger issue, of course...
    ...is that InitScoringModes doesn't exist.
    
    Comment out the block of code for now.

--- Themes/default/BGAnimations/ScreenStageInformation underlay/default.lua
+++ Themes/default/BGAnimations/ScreenStageInformation underlay/default.lua
@@ -79,10 +79,12 @@ t[#t+1] = Def.ActorFrame {
 
 --Get scoring ready.
 if not GAMESTATE:IsCourseMode() then
+--[[
 	InitScoringModes(
-    (GAMESTATE:IsSideJoined(PLAYER_1)) and GetUserPref("UserPrefScoringMode"), 
-    (GAMESTATE:IsSideJoined(PLAYER_2)) and GetUserPref("UserPrefScoringMode")
-  )
+		GAMESTATE:IsSideJoined(PLAYER_1) and GetUserPref("UserPrefScoringMode"), 
+		GAMESTATE:IsSideJoined(PLAYER_2) and GetUserPref("UserPrefScoringMode")
+	)
+]]
 end
 
 return t
