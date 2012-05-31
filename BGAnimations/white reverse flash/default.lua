f89dd5e96e6a4e30aee7ce3720e9896ad86f9f13
AJ Kelly <aj187@ajworld.net>
2012-05-31 02:43:17 -0500

    add white reverse flash bganimation

---
+++ BGAnimations/white reverse flash/default.lua
@@ -0,0 +1,4 @@
+return Def.Quad {    
+    InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;scaletoclipped,SCREEN_WIDTH*2,SCREEN_HEIGHT*2);
+    GainFocusCommand=cmd(finishtweening;diffusealpha,0;accelerate,0.6;diffusealpha,1);
+};
