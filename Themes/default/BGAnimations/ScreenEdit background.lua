185643aa269e728e9618680c0cd42e23914bd662
Jonathan Payne <PhosphorusGL@gmail.com>
2012-07-06 11:56:41 -0700

    Fix editor yellow syndrome, remove EditModifiers metric

--- Themes/default/BGAnimations/ScreenEdit background.lua
+++ Themes/default/BGAnimations/ScreenEdit background.lua
@@ -3,10 +3,6 @@ local t = Def.ActorFrame {
 };
 t[#t+1] = Def.ActorFrame {
   InitCommand=cmd(Center);
-	Def.Quad {
-		InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT);
-		OnCommand=cmd(diffuse,color("#ffcb05"));
-	};
 	LoadActor( THEME:GetPathB("ScreenWithMenuElements","background/_bg top") ) .. {
 		InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT);
 	};
