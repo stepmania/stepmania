4427ea0d915aa1a04da30a3d22f111f09a8c4588
Jason Felds <jafelds@gmail.com>
2013-04-27 13:08:15 -0400

    Remove unused maros.
    Also, white space hell.

--- src/Foreach.h
+++ src/Foreach.h
@@ -22,9 +22,6 @@ for( set<elemType>::iterator var = (vect).begin(); var != (vect).end(); ++var )
 #define FOREACHS_CONST( elemType, vect, var ) 	\
 for( set<elemType>::const_iterator var = (vect).begin(); var != (vect).end(); ++var )
 
-/** @brief General foreach loop iterating over a list. */
-#define FOREACHL( elemType, vect, var ) 			\
-for( list<elemType>::iterator var = (vect).begin(); var != (vect).end(); ++var )
 /** @brief General foreach loop iterating over a list, using a constant iterator. */
 #define FOREACHL_CONST( elemType, vect, var ) 			\
 for( list<elemType>::const_iterator var = (vect).begin(); var != (vect).end(); ++var )
@@ -35,14 +32,6 @@ for( map<keyType, valType>::iterator var = (vect).begin(); var != (vect).end(); 
 /** @brief General foreach loop iterating over a map, using a constant iterator. */
 #define FOREACHM_CONST( keyType, valType, vect, var ) 			\
 for( map<keyType, valType>::const_iterator var = (vect).begin(); var != (vect).end(); ++var )
-
-/** @brief General foreach loop iterating over a multimap. */
-#define FOREACHMM( keyType, valType, vect, var ) 			\
-for( multimap<keyType, valType>::iterator var = (vect).begin(); var != (vect).end(); ++var )
-
-/** @brief General foreach loop iterating over a multimap, using a constant iterator. */
-#define FOREACHMM_CONST( keyType, valType, vect, var ) \
-for( multimap<keyType, valType>::const_iterator var = (vect).begin(); var != (vect).end(); ++var )
 
 #endif
 
