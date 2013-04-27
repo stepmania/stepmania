b6f2bf317c686e7774c7b2330804296c5dd13008
Jason Felds <jafelds@gmail.com>
2013-04-27 15:20:30 -0400

    Where's my hg commit --amend command?

--- src/SongManager.cpp
+++ src/SongManager.cpp
@@ -563,7 +563,7 @@ RString SongManager::GetCourseGroupBannerPath( const RString &sCourseGroup ) con
 
 void SongManager::GetCourseGroupNames( vector<RString> &AddTo ) const
 {
-	for (std::pair<RString const &, CourseGroupInfo &> const &iter : m_mapCourseGroupToInfo)
+	for (std::pair<RString const, CourseGroupInfo> const &iter : m_mapCourseGroupToInfo)
 		AddTo.push_back( iter.first );
 }
 
@@ -575,7 +575,7 @@ bool SongManager::DoesCourseGroupExist( const RString &sCourseGroup ) const
 RageColor SongManager::GetCourseGroupColor( const RString &sCourseGroup ) const
 {
 	int iIndex = 0;
-	for (std::pair<RString const &, CourseGroupInfo &> const &iter : m_mapCourseGroupToInfo)
+	for (std::pair<RString const, CourseGroupInfo> const &iter : m_mapCourseGroupToInfo)
 	{
 		if( iter.first == sCourseGroup )
 			return SONG_GROUP_COLOR.GetValue( iIndex%NUM_SONG_GROUP_COLORS );
