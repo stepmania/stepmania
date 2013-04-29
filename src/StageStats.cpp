9311913f4a10b5aa426594ce24217530eb4890ba
Jason Felds <jafelds@gmail.com>
2013-04-28 23:27:05 -0400

    Don't feel like a goodie here.

--- src/StageStats.cpp
+++ src/StageStats.cpp
@@ -86,10 +86,10 @@ int StageStats::GetAverageMeter( PlayerNumber pn ) const
 void StageStats::AddStats( const StageStats& other )
 {
 	ASSERT( !other.m_vpPlayedSongs.empty() );
-	FOREACH_CONST( Song*, other.m_vpPlayedSongs, s )
-		m_vpPlayedSongs.push_back( *s );
-	FOREACH_CONST( Song*, other.m_vpPossibleSongs, s )
-		m_vpPossibleSongs.push_back( *s );
+	for (Song *s : other.m_vpPlayedSongs)
+		m_vpPlayedSongs.push_back( s );
+	for (Song *s : other.m_vpPossibleSongs)
+		m_vpPossibleSongs.push_back( s );
 	m_Stage = Stage_Invalid; // meaningless
 	m_iStageIndex = -1; // meaningless
 
@@ -122,8 +122,8 @@ bool StageStats::AllFailed() const
 float StageStats::GetTotalPossibleStepsSeconds() const
 {
 	float fSecs = 0;
-	FOREACH_CONST( Song*, m_vpPossibleSongs, s )
-		fSecs += (*s)->GetStepsSeconds();
+	for (Song const *s : m_vpPlayedSongs)
+		fSecs += s->GetStepsSeconds();
 	return fSecs / m_fMusicRate;
 }
 
