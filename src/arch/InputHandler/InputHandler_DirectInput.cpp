438d7a5305e070777cc962d1f2a5716e95ee2b16
Jason Felds <jafelds@gmail.com>
2013-04-30 23:03:45 -0400

    Nested loops were simple.

--- src/arch/InputHandler/InputHandler_DirectInput.cpp
+++ src/arch/InputHandler/InputHandler_DirectInput.cpp
@@ -846,14 +846,14 @@ wchar_t InputHandler_DInput::DeviceButtonToChar( DeviceButton button, bool bUseC
 		return '\0';
 	}
 
-	FOREACH_CONST( DIDevice, Devices, d )
+	for (DIDevice const &d : Devices)
 	{
-		if( d->type != DIDevice::KEYBOARD )
+		if( d.type != DIDevice::KEYBOARD )
 			continue;
 
-		FOREACH_CONST( input_t, d->Inputs, i )
+		for (input_t const &i : d.Inputs)
 		{
-			if( button != i->num )
+			if( button != i.num )
 				continue;
 
 			unsigned char keys[256];
@@ -861,7 +861,7 @@ wchar_t InputHandler_DInput::DeviceButtonToChar( DeviceButton button, bool bUseC
 			if( bUseCurrentKeyModifiers )
 				GetKeyboardState(keys);
 			// todo: handle Caps Lock -freem
-			wchar_t c = ScancodeAndKeysToChar( i->ofs, keys );
+			wchar_t c = ScancodeAndKeysToChar( i.ofs, keys );
 			if( c )
 				return c;
 		}
