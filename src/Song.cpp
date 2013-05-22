27940bc57257c728d4078b2aa0ec0e9039778b01
Devin J. Pohly <djpohly@gmail.com>
2013-05-22 13:29:02 -0700

    Bump cachever from -bpm fix, and fix some EOLs

--- src/Song.cpp
+++ src/Song.cpp
@@ -42,7 +42,7 @@
  * @brief The internal version of the cache for StepMania.
  *
  * Increment this value to invalidate the current cache. */
-const int FILE_CACHE_VERSION = 210;
+const int FILE_CACHE_VERSION = 211;
 
 /** @brief How long does a song sample last by default? */
 const float DEFAULT_MUSIC_SAMPLE_LENGTH = 12.f;
