da657870d49a3dd36f8d4e6aa31ad0d3bef02bb9
Jason Felds <jafelds@gmail.com>
2013-07-18 17:41:10 -0400

    Revert "More lua."
    This reverts commit 2330c875010b36b53cea00b8f60ba5fbfe11f2b4.

--- BGAnimations/white reverse flash/default.lua
+++ BGAnimations/white reverse flash/default.lua
@@ -1,13 +1,4 @@
 return Def.Quad {    
-    InitCommand=function(self)
-		self:x(SCREEN_CENTER_X);
-		self:y(SCREEN_CENTER_Y);
-		self:scaletoclipped(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2);
-	end;
-    GainFocusCommand=function(self)
-		self:finishtweening();
-		self:diffusealpha(0);
-		self:accelerate(0.6);
-		self:diffusealpha(1);
-	end;
+    InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;scaletoclipped,SCREEN_WIDTH*2,SCREEN_HEIGHT*2);
+    GainFocusCommand=cmd(finishtweening;diffusealpha,0;accelerate,0.6;diffusealpha,1);
 };
