ff12dc332faff70fcc3ec31a55bda7aec232f397
AJ Kelly <aj187@ajworld.net>
2013-10-07 15:05:34 -0500

    small changes to roulette item
    (Sourced from shakesoda/stepmania.)

--- Themes/dimension-tripper/Graphics/MusicWheelItem Roulette NormalPart.lua
+++ Themes/dimension-tripper/Graphics/MusicWheelItem Roulette NormalPart.lua
@@ -40,4 +40,11 @@ t[#t].InitCommand = function(self)
 	self:texcoordvelocity(-0, 0.5)
 end
 
+-- blu blockers
+t[#t+1] = Def.Quad {}
+t[#t].InitCommand = function(self)
+	self:setsize(width, height * 0.8)
+	self:diffuse({0, 0, 0, 0.65})
+end
+
 return t
