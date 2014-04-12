d54c9f6a0199d65a82ceb25ff99158950bc2b168
Jason Felds <jafelds@gmail.com>
2014-04-12 00:03:15 -0400

    All additional dependencies set.
    Now...the unresolved errors.

--- src/RageSoundReader_Vorbisfile.cpp
+++ src/RageSoundReader_Vorbisfile.cpp
@@ -11,9 +11,9 @@
 #endif
 
 #if defined(_MSC_VER)
-#pragma comment(lib, OGG_LIB_DIR "ogg_static.lib")
-#pragma comment(lib, OGG_LIB_DIR "vorbis_static.lib")
-#pragma comment(lib, OGG_LIB_DIR "vorbisfile_static.lib")
+#pragma comment(lib, "ogg_static.lib")
+#pragma comment(lib, "vorbis_static.lib")
+#pragma comment(lib, "vorbisfile_static.lib")
 #endif // _MSC_VER
 
 #include <cstring>
