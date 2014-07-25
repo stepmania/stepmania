ea74aa74bc6f160c54a5c1ff118a8745c13bcc9c
freem <aj187@ajworld.net>
2014-07-25 12:59:47 -0500

    [Themes/default] hack for SWME StageDisplay to show correct stage on Normal Mode Evaluation

--- Themes/default/Graphics/ScreenWithMenuElements StageDisplay.lua
+++ Themes/default/Graphics/ScreenWithMenuElements StageDisplay.lua
@@ -22,12 +22,19 @@ t[#t+1] = Def.ActorFrame {
 			elseif GAMESTATE:IsEventMode() then
 				self:settextf("Stage %s", curStageIndex);
 			else
+				-- hack: ScreenEvaluation shows the current stage, but it needs to
+				-- show the last stage instead. PlayedStageStats has the right one.
+				if string.find(curScreen,"Evaluation") then
+					local stageStats = STATSMAN:GetPlayedStageStats(1)
+					curStage = stageStats:GetStage()
+				end
+
 				if THEME:GetMetric(curScreen,"StageDisplayUseShortString") then
-				  self:settextf("%s", ToEnumShortString(curStage));
-				  self:zoom(0.75);
+					self:settextf("%s", ToEnumShortString(curStage));
+					self:zoom(0.75);
 				else
-				  self:settextf("%s Stage", ToEnumShortString(curStage));
-				  self:zoom(1);
+					self:settextf("%s Stage", ToEnumShortString(curStage));
+					self:zoom(1);
 				end;
 			end;
 			-- StepMania is being stupid so we have to do this here;
