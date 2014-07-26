7f305664fa8b8563ac8a8f868b1b60e9476c6c6f
freem <aj187@ajworld.net>
2014-07-26 15:48:36 -0500

    update ScreenGameOver with a fancier "game over" graphic and bigger text

--- Themes/default/BGAnimations/ScreenGameOver underlay/default.lua
+++ Themes/default/BGAnimations/ScreenGameOver underlay/default.lua
@@ -1,11 +1,10 @@
 return Def.ActorFrame {
-	InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
-	LoadFont("Common Normal") .. {
-		Text=ScreenString("GAME OVER");
-		InitCommand=cmd(y,-4;shadowlength,1;diffuse,Color("Red"));
-	};
-	LoadFont("Common Normal") .. {
-		Text=ScreenString("Play again soon!");
-		InitCommand=cmd(y,16;shadowlength,1;zoom,0.5;);
-	};
-};
+	InitCommand=cmd(Center),
+	LoadActor(THEME:GetPathG("ScreenGameOver","gameover"))..{
+		InitCommand=cmd(y,-16;shadowlength,2)
+	},
+	LoadFont("Common Normal")..{
+		Text=ScreenString("Play again soon!"),
+		InitCommand=cmd(y,36;shadowlength,1)
+	}
+}
