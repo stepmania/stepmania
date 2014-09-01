b6ff29fdd8c6e09cd0103eefff8f095d4cb533fa
Jonathan Payne <potentj@gmail.com>
2014-08-31 23:26:23 -0700

    Make TwoPartSelection dialog hide on events

--- Themes/default/BGAnimations/ScreenSelectMusic overlay.lua
+++ Themes/default/BGAnimations/ScreenSelectMusic overlay.lua
@@ -53,9 +53,15 @@ for i=1,#num_players do
 		};
 	};
 end
+-- Lock input for half a second so that players don't accidentally start a song
+t[#t+1] = Def.Actor { 
+	StartSelectingStepsMessageCommand=function() SCREENMAN:GetTopScreen():lockinput(0.5); end;
+};
 
 --
-t.InitCommand=cmd(Center;x,SCREEN_CENTER_X*1.5;visible,false;diffusealpha,0);
-t.StartSelectingStepsMessageCommand=cmd(linear,0.2;visible,true;diffusealpha,1);
-t.StartSelectingSongMessageCommand=cmd(linear,0.2;visible,true;diffusealpha,0);
+t.InitCommand=cmd(Center;x,SCREEN_CENTER_X*1.5;diffusealpha,0);
+t.StartSelectingStepsMessageCommand=cmd(linear,0.2;diffusealpha,1);
+t.SongUnchosenMessageCommand=cmd(linear,0.2;diffusealpha,0);
+
+
 return t;
