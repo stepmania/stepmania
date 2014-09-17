ccd8cfc5a7f6e1ccf5ea153d1ac3c9222e11abe5
freem <aj187@ajworld.net>
2014-09-17 02:09:10 -0500

    fix another small error in ScreenNameEntryTraditional underlay

--- Themes/_fallback/BGAnimations/ScreenNameEntryTraditional underlay/default.lua
+++ Themes/_fallback/BGAnimations/ScreenNameEntryTraditional underlay/default.lua
@@ -26,7 +26,7 @@ t[#t+1] = Def.ActorFrame {
 --
 t[#t+1] = Def.Actor {
 	MenuTimerExpiredMessageCommand = function(self, param)
-		for pn in ivalues(Players) do
+		for pn in ivalues(PlayerNumber) do
 			SCREENMAN:GetTopScreen():Finish(pn);
 		end
 	end;
