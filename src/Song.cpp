fc8da6b485db3a4184f7cb85d4b4baf1045b06d9
Kyzentun <kyzentun@gmail.com>
2015-03-02 19:01:17 -0700

    Update file cache version.

--- src/Song.cpp
+++ src/Song.cpp
@@ -43,7 +43,7 @@
  * @brief The internal version of the cache for StepMania.
  *
  * Increment this value to invalidate the current cache. */
-const int FILE_CACHE_VERSION = 222;
+const int FILE_CACHE_VERSION = 223;
 
 /** @brief How long does a song sample last by default? */
 const float DEFAULT_MUSIC_SAMPLE_LENGTH = 12.f;
