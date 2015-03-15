006d6634cb54086abfe112d3a02247b2de5db30c
Jason Felds <jafelds@gmail.com>
2015-03-14 21:18:07 -0400

    Xrandr is package'd.

--- CMake/Modules/FindXrandr.cmake
+++ CMake/Modules/FindXrandr.cmake
@@ -27,24 +27,18 @@
 # limitations under the License.
 #=============================================================================
 
-find_path(XRANDR_INCLUDE_DIR NAMES X11/extensions/Xrandr.h
+find_path(XRANDR_INCLUDE_DIRS NAMES X11/extensions/Xrandr.h
   PATH_SUFFIXES X11/extensions
   PATHS /opt/X11/include
   DOC "The XRANDR include directory"
 )
 
-find_library(XRANDR_LIBRARY NAMES Xrandr
+find_library(XRANDR_LIBRARIES NAMES Xrandr
   PATHS /opt/X11/lib
   DOC "The XRANDR library"
 )
 
 include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(XRANDR DEFAULT_MSG XRANDR_LIBRARIES XRANDR_INCLUDE_DIRS)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(XRANDR DEFAULT_MSG XRANDR_LIBRARY XRANDR_INCLUDE_DIR)
-
-if(XRANDR_FOUND)
-  set( XRANDR_LIBRARIES ${XRANDR_LIBRARY} )
-  set( XRANDR_INCLUDE_DIRS ${XRANDR_INCLUDE_DIR} )
-endif()
-
-mark_as_advanced(XRANDR_INCLUDE_DIR XRANDR_LIBRARY)
+mark_as_advanced(XRANDR_INCLUDE_DIRS XRANDR_LIBRARIES)
