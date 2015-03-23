0a1f43dcbea35ce69ef2b48bcde3845d30318417
Jason Felds <jafelds@gmail.com>
2015-03-23 18:54:58 -0400

    Fix compilation for cmake-less 2013 users.
    This should compliment 7ac547a5.

--- src/ver.h
+++ src/ver.h
@@ -1,7 +1,11 @@
 #ifndef STEPMANIA_VER_H
 #define STEPMANIA_VER_H
 
+#if !defined(CMAKE_POWERED) && ( defined(_MSC_VER) || defined(__MACOSX__) )
+#define product_version "5.0-UNKNOWN"
+#else
 extern const char *const product_version;
+#endif
 
 // XXX: These names are misnomers. This is actually the BUILD number, time and date.
 // NOTE: In GNU toolchain these are defined in ver.cpp. In MSVC these are defined in archutils/Win32/verinc.c I think. Why? I don't know and I don't have MSVC. --root
