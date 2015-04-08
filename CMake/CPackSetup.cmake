361df3a49636e9034e6657aa3f78016a1a06e64c
Jason Felds <jafelds@gmail.com>
2015-04-08 18:18:06 -0400

    Provide initial cpack support. Mainly for devs.
    There are two targets you may not be familiar with, `install` and `package`.
    * For Linux, `install` now defaults to `/usr/local`, but you can set it back to `/opt` with `cmake -DCMAKE_INSTALL_PREFIX=/opt` if you wish.
    * For Windows, `install` may not work, but this is not a target you would want usually.
    * For Linux, `package` has not been tested yet. It will likely create an archive file of what is installed.
    * For Windows, `package` will create an NSIS package that can be used to install StepMania. One minor change as a result is that the binary and companion dlls will be placed in `bin` instead of `Program` now, but it makes no difference code-wise.
    
    Mac OS X users: for now, use the `dmg` target instead.
    
    This commit does *not* deprecate the original nsi script yet. The Texture Font Generator program has not been addressed yet.

---
+++ CMake/CPackSetup.cmake
@@ -0,0 +1,27 @@
+set(CPACK_PACKAGE_NAME "${SM_EXE_NAME}")
+set(CPACK_PACKAGE_VENDOR "StepMania")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Rhythm Game Simulator")
+set(CPACK_PACKAGE_VERSION_MAJOR "${SM_VERSION_MAJOR}")
+set(CPACK_PACKAGE_VERSION_MINOR "${SM_VERSION_MINOR}")
+set(CPACK_PACKAGE_VERSION_PATCH "${SM_VERSION_PATCH}")
+set(CPACK_PACKAGE_VERSION "${SM_VERSION_TRADITIONAL}")
+set(CPACK_NSIS_HELP_LINK "https://github.com/stepmania/stepmania/issues")
+set(CPACK_NSIS_URL_INFO_ABOUT "http://www.stepmania.com/")
+set(CPACK_RESOURCE_FILE_README "${SM_ROOT_DIR}/README.md")
+set(CPACK_RESOURCE_FILE_LICENSE "${SM_BUILD_DIR}/license_install.txt")
+set(CPACK_PACKAGE_EXECUTABLES
+  "${SM_EXE_NAME}" "StepMania ${SM_VERSION_MAJOR}" 
+)
+
+if(WIN32)
+
+  # By setting these install keys manually,
+  # The default directory of "StepMania major.minor.patch" is lost.
+  # This is currently done to maintain backwards compatibility.
+  # However, removing these two will allow for multiple versions of StepMania
+  # to be installed relatively cleanly.
+  set(CPACK_PACKAGE_INSTALL_DIRECTORY "StepMania ${SM_VERSION_MAJOR}")
+  set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "StepMania ${SM_VERSION_MAJOR}")
+endif()
+
+include(CPack)
---
+++ CMake/DefineOptions.cmake
@@ -0,0 +1,38 @@
+# Prep options that are needed for each platform.
+option(WITH_NETWORKING "Build with networking support." ON)
+
+# This option quiets warnings that are a part of external projects.
+option(WITH_EXTERNAL_WARNINGS "Build with warnings for all components, not just StepMania." OFF)
+
+# This option is not yet working, but will likely default to ON in the future.
+option(WITH_LTO "Build with Link Time Optimization (LTO)/Whole Program Optimization." OFF)
+
+# This option handles if we use SSE2 processing.
+option(WITH_SSE2 "Build with SSE2 Optimizations." ON)
+
+# This option may go away in the future: if it does, JPEG will always be required.
+option(WITH_JPEG "Build with JPEG Image Support." ON)
+
+# Turn this on to set this to a specific release mode.
+option(WITH_FULL_RELEASE "Build as a proper, full release." OFF)
+
+# Turn this option off to not use the GPL exclusive components.
+option(WITH_GPL_LIBS "Build with GPL libraries." ON)
+
+if(WIN32)
+  option(WITH_MINIMAID "Build with Mimimaid Lights Support." OFF)
+else()
+  if(LINUX)
+    option(WITH_FFMPEG "Build with FFMPEG." ON)
+    # Builder beware: later versions of ffmpeg may break!
+    option(WITH_SYSTEM_FFMPEG "Build with the system's FFMPEG." OFF)
+    option(WITH_TTY "Build with Linux TTY Input Support." OFF)
+    option(WITH_PROFILING "Build with Profiling Support." OFF)
+    option(WITH_GLES2 "Build with OpenGL ES 2.0 Support." ON)
+    option(WITH_GTK2 "Build with GTK2 Support." ON)
+    option(WITH_OGG "Build with OGG/Vorbis Support." ON)
+    option(WITH_MP3 "Build with MP3 Support." ON)
+    option(WITH_PARALLEL_PORT "Build with Parallel Lights I/O Support." OFF)
+    option(WITH_CRASH_HANDLER "Build with Crash Handler Support." ON)
+  endif()
+endif()
