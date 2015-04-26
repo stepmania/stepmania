156f8c6e495ba7dc873f194769b8496483f38d16
Jonathan Payne <jonathanjpayne@gmail.com>
2015-04-25 20:02:11 -0700

    Header, Footer style improvements

--- Themes/default/Graphics/ScreenWithMenuElements Footer.lua
+++ Themes/default/Graphics/ScreenWithMenuElements Footer.lua
@@ -1,12 +1,7 @@
 local t = Def.ActorFrame {};
 
 t[#t+1] = Def.Quad {
-	InitCommand=cmd(vertalign,bottom;zoomto,SCREEN_WIDTH,34;diffuse,color("#161616"));
-};
-
-t[#t+1] = LoadActor(THEME:GetPathG("ScreenWithMenuElements","header/Header")) .. {
-	InitCommand=cmd(y,-48;vertalign,bottom;zoomtowidth,SCREEN_WIDTH);
-	OnCommand=cmd(zoomy,-1;diffuse,color("#ffd400"));
+	InitCommand=cmd(vertalign,bottom;zoomto,SCREEN_WIDTH,32;diffuse,color("#161616"));
 };
 
 return t;
