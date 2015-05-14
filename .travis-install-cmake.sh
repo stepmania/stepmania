e2a185a17c5802ddffe8688526f2a9a08a65299f
Jason Felds <jafelds@gmail.com>
2015-05-14 18:44:49 -0400

    Try limiting travis to 2 jobs only.

--- .travis-install-cmake.sh
+++ .travis-install-cmake.sh
@@ -2,5 +2,5 @@
 set -ex
 wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
 tar -xzf cmake-3.2.2.tar.gz
-cd cmake-3.2.2 && ./configure && make -j && sudo make install
+cd cmake-3.2.2 && ./configure && make -j2 && sudo make install
 
