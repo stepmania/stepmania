b03fcc0a195b9b3166cf091539b127df6f7db82b
Prcuvu <prcuvu@gmail.com>
2015-07-16 22:28:18 +0800

    Update glew to 1.12.0.

--- extern/CMakeProject-glew.cmake
+++ extern/CMakeProject-glew.cmake
@@ -1,16 +1,23 @@
-set(GLEW_SRC "glew-1.5.8/src/glew.c")
+set(GLEW_SRC "glew-1.12.0/src/glew.c")
 
-source_group("" FILES ${GLEW_SRC})
+set(GLEW_HPP "glew-1.12.0/include/GL/glew.h")
 
-add_library("glew" ${GLEW_SRC})
+if(WIN32)
+  list(APPEND GLEW_HPP "glew-1.12.0/include/GL/wglew.h")
+elseif(LINUX)
+  list(APPEND GLEW_HPP "glew-1.12.0/include/GL/glxew.h")
+endif(WIN32)
+
+source_group("" FILES ${GLEW_SRC} ${GLEW_HPP})
+
+add_library("glew" ${GLEW_SRC} ${GLEW_HPP})
 
 set_property(TARGET "glew" PROPERTY FOLDER "External Libraries")
 
-target_include_directories("glew" PUBLIC "glew-1.5.8/include")
+target_include_directories("glew" PUBLIC "glew-1.12.0/include")
 
 sm_add_compile_definition("glew" GLEW_STATIC)
 
 if(MSVC)
   sm_add_compile_definition("glew" _MBCS)
 endif(MSVC)
-
