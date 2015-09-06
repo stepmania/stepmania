02a443460e297bd7808d18028f5b8cf8f797fe9d
Jason Felds <jafelds@gmail.com>
2015-09-06 08:33:30 -0400

    Switch to travis's container infrastructure.
    Alledgedly, we'll get faster builds and performance. Considering how complex this repository is, it's worth trying. Note that sudo calls are not allowed with this container format at this time.

--- .install-cmake.sh
+++ .install-cmake.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-set -ex
-wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
-tar -xzvf cmake-3.2.2.tar.gz
-cd cmake-3.2.2 && ./configure && make && sudo make install
+set -e
+# See if the cmake folder is empty.
+if [! -d "$HOME/cmake/lib"]; then
+	wget http://www.cmake.org/files/v3.3/cmake-3.3.1.tar.gz
+	tar -xzf cmake-3.3.1.tar.gz
+	cd cmake-3.3.1 && ./configure --prefix=$HOME/cmake && make && make install
+else
+	echo "Using cached directory."
+fi
 
