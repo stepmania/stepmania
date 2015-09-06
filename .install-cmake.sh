e488674987e43b0aae5e6d6a5ca3760900c39280
Jason Felds <jafelds@gmail.com>
2015-09-06 08:40:37 -0400

    Whitespace fix.

--- .install-cmake.sh
+++ .install-cmake.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 # See if the cmake folder is empty.
-if [! -d "$HOME/cmake/lib"]; then
+if [ ! -d "$HOME/cmake/lib" ]; then
 	wget http://www.cmake.org/files/v3.3/cmake-3.3.1.tar.gz
 	tar -xzf cmake-3.3.1.tar.gz
 	cd cmake-3.3.1 && ./configure --prefix=$HOME/cmake && make && make install
