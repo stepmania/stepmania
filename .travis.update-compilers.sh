3741de4eb32045c91265bf30cd09229837c4cafb
Jason Felds <jafelds@gmail.com>
2015-09-22 19:59:09 -0400

    Change to home directory. Try without setting CXX.

--- .travis.update-compilers.sh
+++ .travis.update-compilers.sh
@@ -2,6 +2,7 @@
 set -e
 if [ "$CXX" == "g++" ]; then
 	export ORIG_BUILD_DIR=$(pwd)
+	cd ~
 	wget https://github.com/Viq111/travis-container-packets/releases/download/gcc-4.9.2/gcc.tar.bz2
 	tar -xjf gcc.tar.bz2
 	rm gcc.tar.bz2
@@ -10,8 +11,6 @@ if [ "$CXX" == "g++" ]; then
 	export LD_LIBRARY_PATH=$(pwd)/gcc/lib64:$LD_LIBRARY_PATH
 	export CPLUS_INCLUDE_PATH=$(pwd)/gcc/include/c++/4.9.2:$CPLUS_INCLUDE_PATH
 	export CXXFLAGS="--std=c++11"
-	export CXX="g++-4.9.2"
-	export CC="gcc-4.9.2"
 	cd $ORIG_BUILD_DIR
 	echo "New path for GCC: $PATH"
 	echo "New library path for GCC: $LIBRARY_PATH"
@@ -20,6 +19,7 @@ fi
 
 if [ "$CXX" == "clang++" ]; then
 	export ORIG_BUILD_DIR=$(pwd)
+	cd ~
 	wget https://github.com/Viq111/travis-container-packets/releases/download/clang%2Blibcxx-3.5.1/clang_libcxx.tar.bz2
 	tar -xjf clang_libcxx.tar.bz2
 	rm clang_libcxx.tar.bz2
@@ -29,8 +29,6 @@ if [ "$CXX" == "clang++" ]; then
 	export LD_LIBRARY_PATH=$(pwd)/clang/lib:$LD_LIBRARY_PATH
 	export CPLUS_INCLUDE_PATH=$(pwd)/clang/include/c++/v1:$CPLUS_INCLUDE_PATH
 	export CXXFLAGS="-std=c++11 -stdlib=libc++"
-	export CXX="clang++-3.5.1"
-	export CC="clang-3.5.1"
 	cd $ORIG_BUILD_DIR
 	echo "New path for clang: $PATH"
 	echo "New library path for clang: $LIBRARY_PATH"
