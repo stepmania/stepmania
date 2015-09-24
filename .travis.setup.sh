24f122dbe7ce0f311a04fe1c523d08704551ff0c
Jason Felds <jafelds@gmail.com>
2015-09-24 19:51:22 -0400

    Two fixes.
    * Case sensitive PWD within script evaluation.
    * Ignore travis_retry for now.
    * Attempt only 4 jobs instead of 6.

--- .travis.setup.sh
+++ .travis.setup.sh
@@ -3,14 +3,14 @@ set -e
 
 # Setup a directory for dependencies. Remember the original location.
 cd ~
-export DEPS_DIR="${pwd}/deps"
+export DEPS_DIR="${PWD}/deps"
 mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
 
 # There's a binary for CMake available: don't need to make it. Just use it.
 export CMAKE_URL=http://www.cmake.org/files/v3.3/cmake-3.3.1-Linux-x86_64.tar.gz
 mkdir cmake
-travis_retry wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-export PATH=${pwd}/cmake/bin:${PATH}
+wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+export PATH=${PWD}/cmake/bin:${PATH}
 
 # Ensure libc++ and libc++abi are available for clang.
 if [ "${COMPILER}" =~ clang\+\+-3.6 ]; then
@@ -20,9 +20,9 @@ if [ "${COMPILER}" =~ clang\+\+-3.6 ]; then
   export TAR_FMT="-xJ"
 
   mkdir -p llvm llvm/build llvm-projects/libcxx llvm/projects/libcxxabi
-  travis_retry wget --quiet -O - ${LLVM_URL} | tar --strip-components=1 ${TAR_FMT} -C llvm
-  travis_retry wget --quiet -O - ${LIBCXX_URL} | tar --strip-components=1 ${TAR_FMT} -C llvm/projects/libcxx
-  travis_retry wget --quiet -O - ${LIBCXXABI_URL} | tar --strip-components=1 ${TAR_FMT} -C llvm/projects/libcxxabi
+  wget --quiet -O - ${LLVM_URL} | tar --strip-components=1 ${TAR_FMT} -C llvm
+  wget --quiet -O - ${LIBCXX_URL} | tar --strip-components=1 ${TAR_FMT} -C llvm/projects/libcxx
+  wget --quiet -O - ${LIBCXXABI_URL} | tar --strip-components=1 ${TAR_FMT} -C llvm/projects/libcxxabi
   cd llvm/build
   cmake .. -DCMAKE_CXX_COMPILER=clang++
   make cxx -j2
