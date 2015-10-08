e0b32ad3db15234183306f87a80ad7fcc5bf5ca9
Jason Felds <jafelds@gmail.com>
2015-10-08 17:13:33 -0400

    Fix keysounds (and other WAV files).
    Bloody off by one error.

--- src/RageSoundReader_WAV.cpp
+++ src/RageSoundReader_WAV.cpp
@@ -454,8 +454,8 @@ RString ReadString( RageFileBasic &f, int iSize, RString &sError )
 	if( sError.size() != 0 )
 		return RString();
 
-	char *buf = new char[iSize];
-	std::fill(buf, buf + iSize, '\0');
+	char *buf = new char[iSize + 1];
+	std::fill(buf, buf + iSize + 1, '\0');
 	FileReading::ReadBytes( f, buf, iSize, sError );
 	RString ret(buf);
 	delete [] buf;
