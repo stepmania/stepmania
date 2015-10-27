e0290fc85675e1659831d2912b9e4c47d86f6801
Jason Felds <jafelds@gmail.com>
2015-10-27 18:11:37 -0400

    Compile the Texture Font Generator with cmake.
    This program is not built by default. Enable one of the following options to utilize it:
    
    * WITH_FULL_RELEASE
    * WITH_TEXTURE_GENERATOR
    
    It is heavily advised to make sure the MFC library is installed for Visual Studio and the computer is restarted afterwards.
    
    As a bonus, `zlib1.dll` can go away now.
    
    The old visual studio projects are still in play in case others still struggle with the code fixes.

--- CMake/DefineOptions.cmake
+++ CMake/DefineOptions.cmake
@@ -43,6 +43,9 @@ if(NOT MSVC)
   option(WITH_FFMPEG "Build with FFMPEG." ON)
   # Change this number to utilize a different number of jobs for building FFMPEG.
   option(WITH_FFMPEG_JOBS "Build FFMPEG with this many jobs." 2)
+else()
+  # Turn this option on to enable using the Texture Font Generator.
+  option(WITH_TEXTURE_GENERATOR "Build with the Texture Font Generator. Ensure the MFC library is installed." OFF)
 endif()
 
 if(WIN32)
