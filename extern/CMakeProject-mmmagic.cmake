385c1dd5255ac8102859790f4ed36084655e6950
Kyzentun Keeslala <kyzentun@gmail.com>
2016-02-14 22:58:57 -0700

    Forgot to add mmmagic cmake project file.

---
+++ extern/CMakeProject-mmmagic.cmake
@@ -0,0 +1,13 @@
+if(LINUX)
+  if(WITH_MINIMAID)
+    if(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
+      list(APPEND SMDATA_LINK_LIB
+        "${SM_EXTERN_DIR}/libmmmagic/linux-64bit/libmmmagic.a"
+      )
+    else()
+      list(APPEND SMDATA_LINK_LIB
+        "${SM_EXTERN_DIR}/libmmmagic/linux-32bit/libmmmagic.a"
+      )
+    endif()
+  endif()
+endif()
