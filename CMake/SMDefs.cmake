216547c093df33a4a1b74953389b96b3493ba658
Kyzentun Keeslala <kyzentun@gmail.com>
2016-03-30 10:08:58 -0600

    Bumped version to 5.0.11.

--- CMake/SMDefs.cmake
+++ CMake/SMDefs.cmake
@@ -1,7 +1,7 @@
 # Set up version numbers according to the new scheme.
 set(SM_VERSION_MAJOR 5)
 set(SM_VERSION_MINOR 0)
-set(SM_VERSION_PATCH 10)
+set(SM_VERSION_PATCH 11)
 set(SM_VERSION_TRADITIONAL "${SM_VERSION_MAJOR}.${SM_VERSION_MINOR}.${SM_VERSION_PATCH}")
 
 execute_process(COMMAND git rev-parse --short HEAD
