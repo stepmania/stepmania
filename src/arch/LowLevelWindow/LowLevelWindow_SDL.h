4e16c76bfc1ef77ba1ed833e6e829d749c1cf3ea
Andrew Barbarello <andrew.barbarello@outlook.com>
2016-06-18 21:05:49 -0400

    support multi-monitors + FSBW

--- src/arch/LowLevelWindow/LowLevelWindow_SDL.h
+++ src/arch/LowLevelWindow/LowLevelWindow_SDL.h
@@ -24,7 +24,7 @@ public:
     bool SupportsFullscreenBorderlessWindow() const;
 
 private:
-
+	int GetSDLDisplayNum( const std::string displayID ) const;
     ActualVideoModeParams CurrentParams;
 };
 
