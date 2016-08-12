db36520f3183c9924487f54c282a795049fa5083
xwidghet <xwidghet@yahoo.com>
2016-08-11 23:30:10 -0700

    Only check as far back in time as we need to in order to update judged rows (Miss catching). Makes a big difference on songs with a lot of arrows. Magic number of 1 frame's delta at 60fps is added to the current frame delta to ensure  we never miss an arrow. Open to suggestions of a way to find a mathematically perfect value.

--- src/Player.cpp
+++ src/Player.cpp
@@ -1093,7 +1093,7 @@ void Player::Update( float fDeltaTime )
 	}
 
 	// Check for completely judged rows.
-	UpdateJudgedRows();
+	UpdateJudgedRows(fDeltaTime);
 
 	// Check for TapNote misses
 	if (!GAMESTATE->m_bInStepEditor)
@@ -2478,10 +2478,12 @@ void Player::UpdateTapNotesMissedOlderThan( float fMissIfOlderThanSeconds )
 	}
 }
 
-void Player::UpdateJudgedRows()
+void Player::UpdateJudgedRows(float fDeltaTime)
 {
-	// Look ahead far enough to catch any rows judged early.
+	// Look into the past and future only as far as we need to
+	// catch misses and early hits
 	const int iEndRow = BeatToNoteRow( m_Timing->GetBeatFromElapsedTime( m_pPlayerState->m_Position.m_fMusicSeconds + GetMaxStepDistanceSeconds() ) );
+	const int iStartRow = BeatToNoteRow(m_Timing->GetBeatFromElapsedTime(m_pPlayerState->m_Position.m_fMusicSeconds - GetMaxStepDistanceSeconds() - fDeltaTime - 0.1666667f));
 	bool bAllJudged = true;
 	const bool bSeparately = GAMESTATE->GetCurrentGame()->m_bCountNotesSeparately;
 
@@ -2491,6 +2493,10 @@ void Player::UpdateJudgedRows()
 		for( ; !iter.IsAtEnd()  &&  iter.Row() <= iEndRow; ++iter )
 		{
 			int iRow = iter.Row();
+
+			// Do not go across rows which are too old
+			if (iStartRow > iRow)
+				continue;
 
 			// Do not judge arrows in WarpSegments or FakeSegments
 			if (!m_Timing->IsJudgableAtRow(iRow))
--- src/Player.h
+++ src/Player.h
@@ -120,7 +120,7 @@ public:
 
 protected:
 	void UpdateTapNotesMissedOlderThan( float fMissIfOlderThanThisBeat );
-	void UpdateJudgedRows();
+	void UpdateJudgedRows(float fDeltaTime);
 	void FlashGhostRow( int iRow );
 	void HandleTapRowScore( unsigned row );
 	void HandleHoldScore( const TapNote &tn );
