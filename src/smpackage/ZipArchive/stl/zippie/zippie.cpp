eeb117e6ba57b1158b04a7a89c7f46c4b9ece181
latot <felipematas@yahoo.com>
2016-08-24 14:48:41 -0300

    fix buff length

--- src/smpackage/ZipArchive/stl/zippie/zippie.cpp
+++ src/smpackage/ZipArchive/stl/zippie/zippie.cpp
@@ -886,7 +886,7 @@ int main(int argc, char* argv[])
 						lFiles.push_back(fh.GetFileName());
 					else
 					{
-						char buf[50];
+						char buf[54 + std::char_traits<char>::length(i)];
 						sprintf(buf, "There are troubles with getting info from file number %d", i);
 						lFiles.push_back(buf);
 					}
