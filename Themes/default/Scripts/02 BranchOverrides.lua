70ed6acec501bc524677e0d0e168802ee5ec5789
Kyzentun Keeslala <kyzentun@gmail.com>
2016-10-24 19:16:00 -0600

    Remove option restriction check in branch logic.  The lua that sets the menu items should handle it.

--- Themes/default/Scripts/02 BranchOverrides.lua
+++ Themes/default/Scripts/02 BranchOverrides.lua
@@ -1,14 +1,6 @@
 Branch.PlayerOptions= function()
-	local pm = GAMESTATE:GetPlayMode()
-	local restricted = { PlayMode_Oni= true, PlayMode_Rave= true,
-		--"PlayMode_Battle" -- ??
-	}
-	local optionsScreen = "ScreenNestyPlayerOptions"
-	if restricted[pm] then
-		optionsScreen = "ScreenNestyPlayerOptionsRestricted"
-	end
 	if SCREENMAN:GetTopScreen():GetGoToOptions() then
-		return optionsScreen
+		return "ScreenNestyPlayerOptions"
 	else
 		return "ScreenStageInformation"
 	end
