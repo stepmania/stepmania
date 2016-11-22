1a09897971bffbb2536a238d2f977862bffc9d4f
ARC <kirbyrulez@att.net>
2016-11-22 14:10:53 -0600

    Add PacDrive lights driver (#1329)

--- src/CMakeData-arch.cmake
+++ src/CMakeData-arch.cmake
@@ -290,9 +290,11 @@ if(NOT APPLE)
   if(WIN32)
     list(APPEND SMDATA_ARCH_LIGHTS_SRC
       "arch/Lights/LightsDriver_Win32Parallel.cpp"
+      "arch/Lights/LightsDriver_PacDrive.cpp"
     )
     list(APPEND SMDATA_ARCH_LIGHTS_HPP
       "arch/Lights/LightsDriver_Win32Parallel.h"
+      "arch/Lights/LightsDriver_PacDrive.h"
     )
     if (WITH_MINIMAID)
       list(APPEND SMDATA_ARCH_LIGHTS_SRC
