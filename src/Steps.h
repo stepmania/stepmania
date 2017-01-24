78d5946deabfdaa9974a85d268d0c92d56343395
Nickito123 <Nickito123@users.noreply.github.com>
2017-01-24 01:50:30 -0300

    Update Steps.h

--- src/Steps.h
+++ src/Steps.h
@@ -128,7 +128,7 @@ public:
 	using the notedata stored in game memory immediately after reading it than parsing it using lua. - Mina */
 	std::string GenerateChartKey(NoteData &nd, TimingData *td);
 	std::string GenerateChartKey();
-	std::string ChartKey = "Invalid";
+	std::string ChartKey;
 	std::string GetChartKey() const { return ChartKey; }
 	void SetChartKey(const std::string &k) { ChartKey = k; }
 
