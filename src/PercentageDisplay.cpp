a1bf8658b7a6c7acb8dffffaf88a04ed64ecfae4
Concubidated <tbrekke@gmail.com>
2017-06-06 21:54:52 -0700

    PercentageDisplay: Fix ShowDancePointsNotPercentage()
    The function was always showing dance points regardless if
    we were in a Endless Course or not when PercentageScoring
    was set to true. Now only Endless mode will show dance points.
    
    Fixes: #1473

--- src/PercentageDisplay.cpp
+++ src/PercentageDisplay.cpp
@@ -199,10 +199,10 @@ bool PercentageDisplay::ShowDancePointsNotPercentage() const
 	{
 		if( GAMESTATE->m_pCurCourse->m_fGoalSeconds > 0 )
 			return true;
-	}
 
-	if( PREFSMAN->m_bDancePointsForOni )
-		return true;
+		if( PREFSMAN->m_bDancePointsForOni )
+			return true;
+	}
 
 	return false;
 }
