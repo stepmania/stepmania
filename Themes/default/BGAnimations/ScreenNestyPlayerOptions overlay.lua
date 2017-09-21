32592ca0eaa6c38d11a905c630f4c28aa18c8aef
Kyzentun Keeslala <kyzentun@gmail.com>
2017-09-21 16:58:51 -0600

    Added notefield_mods menus to default theme.  Fix lifetype check on ScreenGameplay that activated defective mode.

--- Themes/default/BGAnimations/ScreenNestyPlayerOptions overlay.lua
+++ Themes/default/BGAnimations/ScreenNestyPlayerOptions overlay.lua
@@ -92,6 +92,8 @@ local menu_data= {
 	{"submenu", "gameplay_options", gameplay_options},
 	{"submenu", "song_options", song_options},
 	{"submenu", "life_options", life_options},
+	get_notefield_mods_toggle_menu(true, false),
+	get_notefield_mods_value_menu(true, false),
 }
 
 local player_ready= {}
