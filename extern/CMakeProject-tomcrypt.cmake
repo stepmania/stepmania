b3b51f0dc0f15a7e3a40c5e7e56fc4c01ae461d2
Tracy Ward <coderjoe69@yahoo.com>
2017-11-21 20:35:43 -0500

    Update tomcrypt to v1.18.0
    v1.16 had conflicts with latest Windows SDK headers.

--- extern/CMakeProject-tomcrypt.cmake
+++ extern/CMakeProject-tomcrypt.cmake
@@ -10,6 +10,7 @@ list(APPEND TOMCRYPT_MISC_CRYPT
   "${TOMDIR}/src/misc/crypt/crypt_argchk.c"
   "${TOMDIR}/src/misc/crypt/crypt_cipher_descriptor.c"
   "${TOMDIR}/src/misc/crypt/crypt_cipher_is_valid.c"
+  "${TOMDIR}/src/misc/crypt/crypt_constants.c"
   "${TOMDIR}/src/misc/crypt/crypt_find_cipher.c"
   "${TOMDIR}/src/misc/crypt/crypt_find_cipher_any.c"
   "${TOMDIR}/src/misc/crypt/crypt_find_cipher_id.c"
@@ -21,12 +22,18 @@ list(APPEND TOMCRYPT_MISC_CRYPT
   "${TOMDIR}/src/misc/crypt/crypt_fsa.c"
   "${TOMDIR}/src/misc/crypt/crypt_hash_descriptor.c"
   "${TOMDIR}/src/misc/crypt/crypt_hash_is_valid.c"
+  "${TOMDIR}/src/misc/crypt/crypt_inits.c"
   "${TOMDIR}/src/misc/crypt/crypt_ltc_mp_descriptor.c"
   "${TOMDIR}/src/misc/crypt/crypt_prng_descriptor.c"
   "${TOMDIR}/src/misc/crypt/crypt_prng_is_valid.c"
+  "${TOMDIR}/src/misc/crypt/crypt_prng_rng_descriptor.c"
+  "${TOMDIR}/src/misc/crypt/crypt_register_all_ciphers.c"
+  "${TOMDIR}/src/misc/crypt/crypt_register_all_hashes.c"
+  "${TOMDIR}/src/misc/crypt/crypt_register_all_prngs.c"
   "${TOMDIR}/src/misc/crypt/crypt_register_cipher.c"
   "${TOMDIR}/src/misc/crypt/crypt_register_hash.c"
   "${TOMDIR}/src/misc/crypt/crypt_register_prng.c"
+  "${TOMDIR}/src/misc/crypt/crypt_sizes.c"
   "${TOMDIR}/src/misc/crypt/crypt_unregister_cipher.c"
   "${TOMDIR}/src/misc/crypt/crypt_unregister_hash.c"
   "${TOMDIR}/src/misc/crypt/crypt_unregister_prng.c"
@@ -52,6 +59,9 @@ list(APPEND TOMCRYPT_MISC
   "${TOMDIR}/src/misc/burn_stack.c"
   "${TOMDIR}/src/misc/error_to_string.c"
   "${TOMDIR}/src/misc/zeromem.c"
+  "${TOMDIR}/src/misc/pk_get_oid.c"
+  "${TOMDIR}/src/misc/mem_neq.c"
+  "${TOMDIR}/src/misc/compare_testvector.c"
 )
 
 source_group("src\\\\misc" FILES ${TOMCRYPT_MISC})
@@ -188,43 +198,68 @@ list(APPEND TOMCRYPT_PK_DSA
 source_group("src\\\\pk\\\\dsa" FILES ${TOMCRYPT_PK_DSA})
 
 list(APPEND TOMCRYPT_PK_DER
+  # bit
   "${TOMDIR}/src/pk/asn1/der/bit/der_decode_bit_string.c"
+  "${TOMDIR}/src/pk/asn1/der/bit/der_decode_raw_bit_string.c"
   "${TOMDIR}/src/pk/asn1/der/bit/der_encode_bit_string.c"
+  "${TOMDIR}/src/pk/asn1/der/bit/der_encode_raw_bit_string.c"
   "${TOMDIR}/src/pk/asn1/der/bit/der_length_bit_string.c"
+  # boolean
   "${TOMDIR}/src/pk/asn1/der/boolean/der_decode_boolean.c"
   "${TOMDIR}/src/pk/asn1/der/boolean/der_encode_boolean.c"
   "${TOMDIR}/src/pk/asn1/der/boolean/der_length_boolean.c"
+  # choice
   "${TOMDIR}/src/pk/asn1/der/choice/der_decode_choice.c"
+  # generalizedtime
+  "${TOMDIR}/src/pk/asn1/der/generalizedtime/der_decode_generalizedtime.c"
+  "${TOMDIR}/src/pk/asn1/der/generalizedtime/der_encode_generalizedtime.c"
+  "${TOMDIR}/src/pk/asn1/der/generalizedtime/der_length_generalizedtime.c"
+  # ia5
   "${TOMDIR}/src/pk/asn1/der/ia5/der_decode_ia5_string.c"
   "${TOMDIR}/src/pk/asn1/der/ia5/der_encode_ia5_string.c"
   "${TOMDIR}/src/pk/asn1/der/ia5/der_length_ia5_string.c"
+  # integer
   "${TOMDIR}/src/pk/asn1/der/integer/der_decode_integer.c"
   "${TOMDIR}/src/pk/asn1/der/integer/der_encode_integer.c"
   "${TOMDIR}/src/pk/asn1/der/integer/der_length_integer.c"
+  # object_identifier
   "${TOMDIR}/src/pk/asn1/der/object_identifier/der_decode_object_identifier.c"
   "${TOMDIR}/src/pk/asn1/der/object_identifier/der_encode_object_identifier.c"
   "${TOMDIR}/src/pk/asn1/der/object_identifier/der_length_object_identifier.c"
+  # octect
   "${TOMDIR}/src/pk/asn1/der/octet/der_decode_octet_string.c"
   "${TOMDIR}/src/pk/asn1/der/octet/der_encode_octet_string.c"
   "${TOMDIR}/src/pk/asn1/der/octet/der_length_octet_string.c"
+  # printable_string
   "${TOMDIR}/src/pk/asn1/der/printable_string/der_decode_printable_string.c"
   "${TOMDIR}/src/pk/asn1/der/printable_string/der_encode_printable_string.c"
   "${TOMDIR}/src/pk/asn1/der/printable_string/der_length_printable_string.c"
+  # sequence
   "${TOMDIR}/src/pk/asn1/der/sequence/der_decode_sequence_ex.c"
   "${TOMDIR}/src/pk/asn1/der/sequence/der_decode_sequence_flexi.c"
   "${TOMDIR}/src/pk/asn1/der/sequence/der_decode_sequence_multi.c"
+  "${TOMDIR}/src/pk/asn1/der/sequence/der_decode_subject_public_key_info.c"
   "${TOMDIR}/src/pk/asn1/der/sequence/der_encode_sequence_ex.c"
   "${TOMDIR}/src/pk/asn1/der/sequence/der_encode_sequence_multi.c"
+  "${TOMDIR}/src/pk/asn1/der/sequence/der_encode_subject_public_key_info.c"
   "${TOMDIR}/src/pk/asn1/der/sequence/der_length_sequence.c"
   "${TOMDIR}/src/pk/asn1/der/sequence/der_sequence_free.c"
+  "${TOMDIR}/src/pk/asn1/der/sequence/der_sequence_shrink.c"
+  # set
   "${TOMDIR}/src/pk/asn1/der/set/der_encode_set.c"
   "${TOMDIR}/src/pk/asn1/der/set/der_encode_setof.c"
+  # short_integer
   "${TOMDIR}/src/pk/asn1/der/short_integer/der_decode_short_integer.c"
   "${TOMDIR}/src/pk/asn1/der/short_integer/der_encode_short_integer.c"
   "${TOMDIR}/src/pk/asn1/der/short_integer/der_length_short_integer.c"
+  # teletex_string
+  "${TOMDIR}/src/pk/asn1/der/teletex_string/der_decode_teletex_string.c"
+  "${TOMDIR}/src/pk/asn1/der/teletex_string/der_length_teletex_string.c"
+  # utctime
   "${TOMDIR}/src/pk/asn1/der/utctime/der_decode_utctime.c"
   "${TOMDIR}/src/pk/asn1/der/utctime/der_encode_utctime.c"
   "${TOMDIR}/src/pk/asn1/der/utctime/der_length_utctime.c"
+  # utf8
   "${TOMDIR}/src/pk/asn1/der/utf8/der_decode_utf8_string.c"
   "${TOMDIR}/src/pk/asn1/der/utf8/der_encode_utf8_string.c"
   "${TOMDIR}/src/pk/asn1/der/utf8/der_length_utf8_string.c"
@@ -281,30 +316,32 @@ sm_add_compile_definition("tomcrypt" LTC_SOURCE)
 sm_add_compile_definition("tomcrypt" LTM_DESC)
 
 # This was defined behind an always active block.
-sm_add_compile_definition("tomcrypt" DEVRANDOM)
+sm_add_compile_definition("tomcrypt" LTC_DEVRANDOM)
 
 # Common formulas used by our app.
-sm_add_compile_definition("tomcrypt" SHA1)
-sm_add_compile_definition("tomcrypt" MD5)
+sm_add_compile_definition("tomcrypt" LTC_SHA1)
+sm_add_compile_definition("tomcrypt" LTC_MD5)
 
 # Use the full AES encryption items.
-sm_add_compile_definition("tomcrypt" YARROW)
-sm_add_compile_definition("tomcrypt" YARROW_AES=3)
+sm_add_compile_definition("tomcrypt" LTC_YARROW)
+sm_add_compile_definition("tomcrypt" LTC_YARROW_AES=3)
 
 # Other definitions we used in the past, but whose meanings are not clear.
 sm_add_compile_definition("tomcrypt" LTC_NO_PKCS)
-sm_add_compile_definition("tomcrypt" PKCS_1)
+sm_add_compile_definition("tomcrypt" LTC_PKCS_1)
 sm_add_compile_definition("tomcrypt" LTC_DER)
 sm_add_compile_definition("tomcrypt" LTC_NO_MODES)
 sm_add_compile_definition("tomcrypt" LTC_ECB_MODE)
 sm_add_compile_definition("tomcrypt" LTC_CBC_MODE)
 sm_add_compile_definition("tomcrypt" LTC_CTR_MODE)
 sm_add_compile_definition("tomcrypt" LTC_NO_HASHES)
-sm_add_compile_definition("tomcrypt" LTC_NO_MACS) # Meaning is unclear: used with Mac OS X too.
+sm_add_compile_definition("tomcrypt" LTC_NO_MACS) # no MAC (message authentication code) support
 sm_add_compile_definition("tomcrypt" LTC_NO_PRNGS)
-sm_add_compile_definition("tomcrypt" TRY_URANDOM_FIRST)
+sm_add_compile_definition("tomcrypt" LTC_RNG_GET_BYTES)
+sm_add_compile_definition("tomcrypt" LTC_RNG_MAKE_PRNG)
+sm_add_compile_definition("tomcrypt" LTC_TRY_URANDOM_FIRST)
 sm_add_compile_definition("tomcrypt" LTC_NO_PK)
-sm_add_compile_definition("tomcrypt" MRSA)
+sm_add_compile_definition("tomcrypt" LTC_MRSA)
 sm_add_compile_definition("tomcrypt" LTC_NO_PROTOTYPES)
 
 if (WITH_PORTABLE_TOMCRYPT)
