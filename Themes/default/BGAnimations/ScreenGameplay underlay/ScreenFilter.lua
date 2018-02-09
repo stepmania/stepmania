6452449b6fd5f64f276336aa1f13eedaf5437dc8
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2018-02-09 12:22:54 -0600

    the ScreenFilter env will be nil sometimes, catch that

--- Themes/default/BGAnimations/ScreenGameplay underlay/ScreenFilter.lua
+++ Themes/default/BGAnimations/ScreenGameplay underlay/ScreenFilter.lua
@@ -23,7 +23,7 @@ local stepsType = style:GetStepsType()
 if numPlayers == 1 then
 	local player = GAMESTATE:GetMasterPlayerNumber()
 	local pNum = (player == PLAYER_1) and 1 or 2
-	filterAlphas[player] = tonumber(getenv("ScreenFilterP"..pNum));
+	filterAlphas[player] = tonumber(getenv("ScreenFilterP"..pNum)) or 0;
 
 	local pos;
 	-- [ScreenGameplay] PlayerP#Player*Side(s)X
@@ -54,7 +54,7 @@ else
 		-- otherwise we need two separate ones. to the pairsmobile!
 		for i, player in ipairs(PlayerNumber) do
 			local pNum = (player == PLAYER_1) and 1 or 2
-			filterAlphas[player] = tonumber(getenv("ScreenFilterP"..pNum));
+			filterAlphas[player] = tonumber(getenv("ScreenFilterP"..pNum)) or 0;
 			local metricName = string.format("PlayerP%i%sX",pNum,styleType)
 			local pos = THEME:GetMetric("ScreenGameplay",metricName)
 			t[#t+1] = Def.Quad{
