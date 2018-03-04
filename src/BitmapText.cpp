15149c8982e53878e41da3d931d2f9d0ecf8de57
Tobias Diedrich <ranma+stepmania@tdiedrich.de>
2018-03-04 13:37:25 +0100

    Improve DrawQuads batching in the font render.
    Currently only identical glyphs are merged into one DrawQuads call.
    This fixes the comparison so that all glyphs with the same texture can
    be merged, which greatly improves the font rendering performance.

--- src/BitmapText.cpp
+++ src/BitmapText.cpp
@@ -422,7 +422,7 @@ void BitmapText::DrawChars( bool bUseStrokeTexture )
 	for( int start = iStartGlyph; start < iEndGlyph; )
 	{
 		int end = start;
-		while( end < iEndGlyph  &&  m_vpFontPageTextures[end] == m_vpFontPageTextures[start] )
+		while( end < iEndGlyph  &&  *m_vpFontPageTextures[end] == *m_vpFontPageTextures[start] )
 			end++;
 
 		bool bHaveATexture = !bUseStrokeTexture  ||  (bUseStrokeTexture && m_vpFontPageTextures[start]->m_pTextureStroke);
