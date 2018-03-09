51e852bdc07befd39313066b23f473b3255c43d8
Nicolas <abramlujan@gmail.com>
2018-03-08 22:14:47 -0300

    Change smo timeout to 5

--- src/ezsockets.cpp
+++ src/ezsockets.cpp
@@ -174,7 +174,7 @@ bool EzSockets::create(EzSockets_Proto Protocol, int Type)
 
 	data->sock = socket(AF_INET, Type, realproto);
 	if (data->sock > SOCKET_NONE) {
-		struct timeval tv = { 2, 0 };
+		struct timeval tv = { 5, 0 };
 		setsockopt(data->sock, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(struct timeval));
 	}
 	lastCode = data->sock;
