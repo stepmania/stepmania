860d3482b1319daa5708c95a97288a794a3739df
Andrew Udvare <audvare@gmail.com>
2019-02-13 09:04:10 -0500

    Fix for Xcode libjpeg TRUE/FALSE

--- src/RageSurface_Save_JPEG.cpp
+++ src/RageSurface_Save_JPEG.cpp
@@ -16,6 +16,12 @@ namespace jpeg
 	}
 }
 
+// Only Xcode wants this. FIXME
+#ifdef PBBUILD
+#define TRUE jpeg::TRUE
+#define FALSE jpeg::FALSE
+#endif
+
 #define OUTPUT_BUFFER_SIZE	4096
 typedef struct
 {
@@ -149,7 +155,7 @@ bool RageSurfaceUtils::SaveJPEG( RageSurface *surface, RageFile &f, bool bHighQu
 /*
  * (c) 2004 Chris Danford
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -159,7 +165,7 @@ bool RageSurfaceUtils::SaveJPEG( RageSurface *surface, RageFile &f, bool bHighQu
  * copyright notice(s) and this permission notice appear in all copies of
  * the Software and that both the above copyright notice(s) and this
  * permission notice appear in supporting documentation.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
